5c9293f53658127b9cf1b8c38abb0bcc6f6f889b
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-01-16 15:59:06 +0200

    SQL Queries

---
+++ SQL Queries/Vss_deferment/001aa_students_addopenbal.sql
@@ -0,0 +1,6 @@
+﻿SELECT
+  X000_Students_curr.*,
+  X000_Tran_balopen_curr.BAL_OPEN
+FROM
+  X000_Students_curr
+  LEFT JOIN X000_Tran_balopen_curr ON X000_Tran_balopen_curr.STUDENT = X000_Students_curr.KSTUDBUSENTID
---
+++ SQL Queries/Vss_deferment/Test.sql
@@ -0,0 +1,4 @@
+﻿SELECT
+  Total(X001aa_Students.BALOPEN_AMOUNT) AS Total_BALOPEN_AMOUNT
+FROM
+  X001aa_Students
