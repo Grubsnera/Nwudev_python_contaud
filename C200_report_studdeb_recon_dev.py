9d5814d81860c608ceea95263715e141347658e8
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-01-24 09:48:18 +0200

    Update C200_report_studdeb_recon_dev.py
    Add calculation to group and export matched (vss and gl) bursary transactions

--- C200_report_studdeb_recon_dev.py
+++ C200_report_studdeb_recon_dev.py
@@ -54,45 +54,35 @@ funcfile.writelog("%t OPEN DATABASE: Kfs_vss_studdeb")
 # Development script ***********************************************************
 
 
+# BURSARY VSS GL RECON X010cc Group matched vss gl transactions on campus & month
 
-# Calculate vss balances per campus per month ******************************
-print("Calculate vss campus balances per month...")
-sr_file = "X002cb_vss_balmonth"
+print("Group matched vss and gl bursary transactions on campus and month...")
+sr_file = "X010cc_group_vss_gl_matched"    
 s_sql = "CREATE TABLE "+sr_file+" AS " + """
 SELECT
-  X002ab_vss_transort.CAMPUS_VSS,
-  X002ab_vss_transort.MONTH_VSS,
-  Total(X002ab_vss_transort.AMOUNT_DT) AS AMOUNT_DT,
-  Total(X002ab_vss_transort.AMOUNT_CR) AS AMOUNT_CT,
-  Total(X002ab_vss_transort.AMOUNT_VSS) AS AMOUNT
+  X010cb_join_vss_gl_matched.CAMPUS_VSS,
+  X010cb_join_vss_gl_matched.MONTH_VSS,
+  Total(X010cb_join_vss_gl_matched.AMOUNT_VSS) AS Total_AMOUNT_VSS
 FROM
-  X002ab_vss_transort
+  X010cb_join_vss_gl_matched
 GROUP BY
-  X002ab_vss_transort.CAMPUS_VSS,
-  X002ab_vss_transort.MONTH_VSS
+  X010cb_join_vss_gl_matched.CAMPUS_VSS,
+  X010cb_join_vss_gl_matched.MONTH_VSS
 ;"""
-so_curs.execute("DROP TABLE IF EXISTS "+sr_file)    
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
 so_curs.execute(s_sql)
 so_conn.commit()
 funcfile.writelog("%t BUILD TABLE: "+sr_file)
 # Export the data
-print("Export vss campus balances per transaction type...")
+print("Export grouped matched bursary transactions...")
 sr_filet = sr_file
-sx_path = re_path + funcdate.prev_year() + "/"
-sx_file = "Debtor_001_vsssummmonth_"
+sx_path = re_path + funcdate.cur_year() + "/"
+sx_file = "Debtor_010c_burs_matched_summmonth_"
 sx_filet = sx_file + funcdate.prev_monthendfile()
 s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
-funccsv.write_data(so_conn, "main", sr_file, sx_path, sx_file, s_head)
-funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)  
-
-
-
-
-
-
-
-
-
+funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
+#funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
+funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
 
 
 
