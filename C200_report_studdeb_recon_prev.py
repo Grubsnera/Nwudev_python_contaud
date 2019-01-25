ad7d38452e812a07be47d8420a55d1b637dd3085
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-01-25 12:23:23 +0200

    Update C200_report_studdeb_recon_prev.py
    Develope and add vss and gl monthly running totals.

--- C200_report_studdeb_recon_prev.py
+++ C200_report_studdeb_recon_prev.py
@@ -285,16 +285,6 @@ so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
 so_curs.execute(s_sql)
 so_conn.commit()
 funcfile.writelog("%t BUILD TABLE: "+sr_file)
-# Export the data
-print("Export gl student debtor summary per month...")
-sr_filet = sr_file
-sx_path = re_path + funcdate.prev_year() + "/"
-sx_file = "Debtor_001_glsummmonth_"
-sx_filet = sx_file + funcdate.prev_monthendfile()
-s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
-funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
-#funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
-funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)    
 
 # Build gl summary per transaction type ****************************************
 print("Build gl summary per transaction type...")
@@ -328,28 +318,6 @@ s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
 funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
 #funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
 funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
-
-#*** LIST GL TRANSACTIONS Identify new accounts not linked to a campus *********
-# See Add column gl business unit id where new account must be linked to campus
-print("Identify new accounts linked to a campus...")
-sr_file = "X001da_test_newaccount"
-s_sql = "CREATE TABLE "+sr_file+" AS " + """
-SELECT
-  X001ab_gl_transort.COST_STRING,
-  X001ab_gl_transort.TIME_POST,
-  X001ab_gl_transort.EDOC,
-  X001ab_gl_transort.DESC_FULL,
-  X001ab_gl_transort.AMOUNT,
-  Trim(X001ab_gl_transort.COST_STRING) || X001ab_gl_transort.TIME_POST || Trim(X001ab_gl_transort.EDOC) AS "ROWID"
-FROM
-  X001ab_gl_transort
-WHERE
-  X001ab_gl_transort.CAMPUS IS NULL
-;"""
-so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
-so_curs.execute(s_sql)
-so_conn.commit()
-funcfile.writelog("%t BUILD TABLE: "+sr_file)    
 
 # Determine gl post month **************************************************
 print("Determine gl post month...")
@@ -391,6 +359,59 @@ else:
     gl_month = p_month
     #print(gl_month)
     
+# Calculate the running gl balance ********************************************
+print("Calculate the running gl balance...")
+sr_file = "X001ce_gl_balmonth_calc_runbal"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  a.CAMPUS,
+  a.MONTH,
+  a.BALANCE,
+  TOTAL(b.BALANCE) RUNBAL
+FROM
+  X001cb_gl_balmonth a,
+  X001cb_gl_balmonth b
+WHERE
+  (a.CAMPUS = b.CAMPUS AND
+  a.MONTH >= b.MONTH)
+GROUP BY
+  a.CAMPUS,
+  a.MONTH
+ORDER BY
+  a.CAMPUS,
+  a.MONTH
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
+
+
+
+
+#*** LIST GL TRANSACTIONS Identify new accounts not linked to a campus *********
+# See Add column gl business unit id where new account must be linked to campus
+print("Identify new accounts linked to a campus...")
+sr_file = "X001da_test_newaccount"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  X001ab_gl_transort.COST_STRING,
+  X001ab_gl_transort.TIME_POST,
+  X001ab_gl_transort.EDOC,
+  X001ab_gl_transort.DESC_FULL,
+  X001ab_gl_transort.AMOUNT,
+  Trim(X001ab_gl_transort.COST_STRING) || X001ab_gl_transort.TIME_POST || Trim(X001ab_gl_transort.EDOC) AS "ROWID"
+FROM
+  X001ab_gl_transort
+WHERE
+  X001ab_gl_transort.CAMPUS IS NULL
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)    
+
+
 
 """*************************************************************************
 ***
@@ -629,15 +650,6 @@ so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
 so_curs.execute(s_sql)
 so_conn.commit()
 funcfile.writelog("%t BUILD TABLE: "+sr_file)
-# Export the data
-print("Export vss campus balances per transaction type...")
-sr_filet = sr_file
-sx_path = re_path + funcdate.prev_year() + "/"
-sx_file = "Debtor_001_vsssummmonth_"
-sx_filet = sx_file + funcdate.prev_monthendfile()
-s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
-funccsv.write_data(so_conn, "main", sr_file, sx_path, sx_file, s_head)
-funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)   
 
 # Calculate vss balances per campus per month per transaction type *********
 print("Calculate vss balances per transaction type...")
@@ -673,6 +685,35 @@ s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
 funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
 #funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
 funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)    
+
+# Calculate the running vss balance ********************************************
+print("Calculate the running vss balance...")
+sr_file = "X002ce_vss_balmonth_calc_runbal"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  a.CAMPUS_VSS,
+  a.MONTH_VSS,
+  a.AMOUNT_DT,
+  a.AMOUNT_CT,
+  a.AMOUNT,
+  TOTAL(b.AMOUNT) RUNBAL
+FROM
+  X002cb_vss_balmonth a,
+  X002cb_vss_balmonth b
+WHERE
+  (a.CAMPUS_VSS = b.CAMPUS_VSS AND
+  a.MONTH_VSS >= b.MONTH_VSS)
+GROUP BY
+  a.CAMPUS_VSS,
+  a.MONTH_VSS
+ORDER BY
+  a.CAMPUS_VSS,
+  a.MONTH_VSS
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
 
 # Sum vss student balances per campus ******************************************
 print("Sum vss student balances per campus...")
@@ -742,9 +783,6 @@ funcfile.writelog("%t BUILD TABLE: "+sr_file)
 
 
 
-
-
-
 """*************************************************************************
 ***
 *** JOIN VSS & GL MONTHLY TOTALS
@@ -760,26 +798,85 @@ print("Join vss and gl monthly totals...")
 sr_file = "X002ea_vss_gl_balance_month"
 s_sql = "CREATE TABLE "+sr_file+" AS " + """
 SELECT
-  UPPER(SUBSTR(X002cb_vss_balmonth.CAMPUS_VSS,1,3))||TRIM(X002cb_vss_balmonth.MONTH_VSS) AS ROWID,
+  UPPER(SUBSTR(X002ce_vss_balmonth_calc_runbal.CAMPUS_VSS,1,3))||TRIM(X002ce_vss_balmonth_calc_runbal.MONTH_VSS) AS ROWID,
   'NWU' AS ORG,
-  X002cb_vss_balmonth.CAMPUS_VSS AS CAMPUS,
-  X002cb_vss_balmonth.MONTH_VSS AS MONTH,
-  X002cb_vss_balmonth.AMOUNT_DT AS VSS_TRAN_DT,
-  X002cb_vss_balmonth.AMOUNT_CT AS VSS_TRAN_CT,
-  X002cb_vss_balmonth.AMOUNT AS VSS_TRAN,
-  X001cb_gl_balmonth.BALANCE AS GL_TRAN
+  X002ce_vss_balmonth_calc_runbal.CAMPUS_VSS AS CAMPUS,
+  X002ce_vss_balmonth_calc_runbal.MONTH_VSS AS MONTH,
+  X002ce_vss_balmonth_calc_runbal.AMOUNT_DT AS VSS_TRAN_DT,
+  X002ce_vss_balmonth_calc_runbal.AMOUNT_CT AS VSS_TRAN_CT,
+  X002ce_vss_balmonth_calc_runbal.AMOUNT AS VSS_TRAN,
+  X002ce_vss_balmonth_calc_runbal.RUNBAL AS VSS_RUNBAL,
+  X001ce_gl_balmonth_calc_runbal.BALANCE AS GL_TRAN,
+  X001ce_gl_balmonth_calc_runbal.RUNBAL AS GL_RUNBAL
 FROM
-  X002cb_vss_balmonth
-  LEFT JOIN X001cb_gl_balmonth ON X001cb_gl_balmonth.CAMPUS = X002cb_vss_balmonth.CAMPUS_VSS AND
-    X001cb_gl_balmonth.MONTH = X002cb_vss_balmonth.MONTH_VSS
+  X002ce_vss_balmonth_calc_runbal
+  LEFT JOIN X001ce_gl_balmonth_calc_runbal ON X001ce_gl_balmonth_calc_runbal.CAMPUS = X002ce_vss_balmonth_calc_runbal.CAMPUS_VSS AND
+    X001ce_gl_balmonth_calc_runbal.MONTH = X002ce_vss_balmonth_calc_runbal.MONTH_VSS
 WHERE
-  X002cb_vss_balmonth.MONTH_VSS <= '%PMONTH%'
+  X002ce_vss_balmonth_calc_runbal.MONTH_VSS <= '%PMONTH%'
 ;"""
 so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+gl_month = "13"
 s_sql = s_sql.replace("%PMONTH%",gl_month) 
 so_curs.execute(s_sql)
 so_conn.commit()
 funcfile.writelog("%t BUILD TABLE: "+sr_file)
+
+# Add columns ******************************************************************
+# Reconciling amount
+print("Add column vss gl difference...")
+so_curs.execute("ALTER TABLE X002ea_vss_gl_balance_month ADD COLUMN DIFF REAL;")
+so_curs.execute("UPDATE X002ea_vss_gl_balance_month SET DIFF = VSS_RUNBAL - GL_RUNBAL;")
+so_conn.commit()
+funcfile.writelog("%t ADD COLUMN: DIFF")
+
+# Calculate the running recon amount *******************************************
+print("Calculate the running recon amount...")
+sr_file = "X002ea_vss_gl_balance_month_move"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  a.ROWID,
+  a.ORG,
+  a.CAMPUS,
+  a.MONTH,
+  a.VSS_TRAN_DT,
+  a.VSS_TRAN_CT,
+  a.VSS_TRAN,
+  a.VSS_RUNBAL,
+  a.GL_TRAN,
+  a.GL_RUNBAL,
+  a.DIFF,
+  a.DIFF - b.DIFF AS MOVE
+FROM
+  X002ea_vss_gl_balance_month a,
+  X002ea_vss_gl_balance_month b
+WHERE
+  (a.CAMPUS = b.CAMPUS AND
+  a.MONTH > b.MONTH) OR
+  (a.CAMPUS = b.CAMPUS AND
+  b.MONTH = "00")
+  
+GROUP BY
+  a.CAMPUS,
+  a.MONTH
+ORDER BY
+  a.CAMPUS,
+  a.MONTH
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
+# Export the data
+print("Export vss campus balances per month...")
+sr_filet = sr_file
+sx_path = re_path + funcdate.prev_year() + "/"
+sx_file = "Debtor_000_vss_gl_summmonth_"
+sx_filet = sx_file + funcdate.prev_monthendfile()
+s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
+funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
+#funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
+funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file) 
 
 # Import the reporting officers ************************************************
 print("Import reporting officers from VSS.SQLITE...")
@@ -828,14 +925,18 @@ print("Add the reporting officer and supervisor...")
 sr_file = "X002ex_vss_gl_balance_month"
 s_sql = "CREATE TABLE " + sr_file + " AS " + """
 SELECT
-  X002ea_vss_gl_balance_month."ROWID",
-  X002ea_vss_gl_balance_month.ORG,
-  X002ea_vss_gl_balance_month.CAMPUS,
-  X002ea_vss_gl_balance_month.MONTH,
-  X002ea_vss_gl_balance_month.VSS_TRAN_DT,
-  X002ea_vss_gl_balance_month.VSS_TRAN_CT,
-  X002ea_vss_gl_balance_month.VSS_TRAN,
-  X002ea_vss_gl_balance_month.GL_TRAN,
+  X002ea_vss_gl_balance_month_move."ROWID",
+  X002ea_vss_gl_balance_month_move.ORG,
+  X002ea_vss_gl_balance_month_move.CAMPUS,
+  X002ea_vss_gl_balance_month_move.MONTH,
+  X002ea_vss_gl_balance_month_move.VSS_TRAN_DT,
+  X002ea_vss_gl_balance_month_move.VSS_TRAN_CT,
+  X002ea_vss_gl_balance_month_move.VSS_TRAN,
+  X002ea_vss_gl_balance_month_move.VSS_RUNBAL,
+  X002ea_vss_gl_balance_month_move.GL_TRAN,
+  X002ea_vss_gl_balance_month_move.GL_RUNBAL,
+  X002ea_vss_gl_balance_month_move.DIFF,
+  X002ea_vss_gl_balance_month_move.VSS_MOVE,
   CAMP_OFFICER.EMPLOYEE_NUMBER AS OFFICER_CAMP,
   CAMP_OFFICER.KNOWN_NAME AS OFFICER_NAME_CAMP,
   CAMP_OFFICER.EMAIL_ADDRESS AS OFFICER_MAIL_CAMP,
@@ -849,11 +950,11 @@ SELECT
   ORG_SUPERVISOR.KNOWN_NAME AS SUPERVISOR_NAME_ORG,
   ORG_SUPERVISOR.EMAIL_ADDRESS AS SUPERVISOR_MAIL_ORG
 FROM
-  X002ea_vss_gl_balance_month
-  LEFT JOIN X002eb_impo_report_officer CAMP_OFFICER ON CAMP_OFFICER.CAMPUS = X002ea_vss_gl_balance_month.CAMPUS
-  LEFT JOIN X002eb_impo_report_officer ORG_OFFICER ON ORG_OFFICER.CAMPUS = X002ea_vss_gl_balance_month.ORG
-  LEFT JOIN X002ec_impo_report_supervisor CAMP_SUPERVISOR ON CAMP_SUPERVISOR.CAMPUS = X002ea_vss_gl_balance_month.CAMPUS
-  LEFT JOIN X002ec_impo_report_supervisor ORG_SUPERVISOR ON ORG_SUPERVISOR.CAMPUS = X002ea_vss_gl_balance_month.ORG
+  X002ea_vss_gl_balance_month_move
+  LEFT JOIN X002eb_impo_report_officer CAMP_OFFICER ON CAMP_OFFICER.CAMPUS = X002ea_vss_gl_balance_month_move.CAMPUS
+  LEFT JOIN X002eb_impo_report_officer ORG_OFFICER ON ORG_OFFICER.CAMPUS = X002ea_vss_gl_balance_month_move.ORG
+  LEFT JOIN X002ec_impo_report_supervisor CAMP_SUPERVISOR ON CAMP_SUPERVISOR.CAMPUS = X002ea_vss_gl_balance_month_move.CAMPUS
+  LEFT JOIN X002ec_impo_report_supervisor ORG_SUPERVISOR ON ORG_SUPERVISOR.CAMPUS = X002ea_vss_gl_balance_month_move.ORG
 ;"""
 so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
 so_curs.execute(s_sql)
@@ -1611,7 +1712,7 @@ sx_file = "Debtor_002_vsssummtypeburs_"
 sx_filet = sx_file + funcdate.prev_monthendfile()
 s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
 funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
-funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
+#funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
 funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
 
 # *** BURSARY VSS GL RECON Extract the gl bursary transactions ******************************************
@@ -1690,7 +1791,7 @@ sx_file = "Debtor_001_glsummtypeburs_"
 sx_filet = sx_file + funcdate.prev_monthendfile()
 s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
 funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
-funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
+#funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
 funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
 
 # *** BURSARY VSS GL RECON Join vss bursary summary totals *********************
