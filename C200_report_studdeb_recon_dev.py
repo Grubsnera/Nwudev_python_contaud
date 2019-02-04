16645ad0595981d3bc18fe750fc605a9d6e72217
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-02-04 08:48:16 +0200

    Update C200_report_studdeb_recon_dev.py
    Add script to add transactions in the gl but not in vss to vss

--- C200_report_studdeb_recon_dev.py
+++ C200_report_studdeb_recon_dev.py
@@ -60,19 +60,21 @@ funcfile.writelog("%t OPEN MYSQL DATABASE: " + s_database)
 
 # Development script ***********************************************************
 
+
+
+# Transfer GL transactions to the VSS file *********************************
 # Open the SOURCE file to obtain column headings
-print("Transfer gl data to the vss database...")
-funcfile.writelog("%t GET COLUMN HEADINGS: X003aa_vss_gl_join")
-s_head = funcmysql.get_colnames_sqlite_text(so_curs,"X003aa_vss_gl_join","")
+print("Transfer english gl data to the vss table...")
+funcfile.writelog("%t GET COLUMN HEADINGS: X003aa_vss_gl_join_eng")
+s_head = funcmysql.get_colnames_sqlite_text(so_curs,"X003aa_vss_gl_join_eng","")
 s_head = "(" + s_head.rstrip(", ") + ")"
 #print(s_head)
-
 # Open the SOURCE file to obtain the data
-print("Insert gl data into vss table...")
+print("Insert english gl data into vss table...")
 #with sqlite3.connect(so_path+so_file) as rs_conn:
 #    rs_conn.row_factory = sqlite3.Row
 #rs_curs = rs_conn.cursor()
-so_curs.execute("SELECT * FROM X003aa_gl_vss_join")
+so_curs.execute("SELECT * FROM X003aa_gl_vss_join_eng")
 rows = so_curs.fetchall()
 i_tota = 0
 i_coun = 0
@@ -90,7 +92,7 @@ for row in rows:
             s_data = s_data + "'', "
     s_data = s_data.rstrip(", ") + ")"
     #print(s_data)
-    s_sql = "INSERT INTO `X003aa_vss_gl_join` " + s_head + " VALUES " + s_data + ";"
+    s_sql = "INSERT INTO `X003aa_vss_gl_join_eng` " + s_head + " VALUES " + s_data + ";"
     so_curs.execute(s_sql)
     i_tota = i_tota + 1
     i_coun = i_coun + 1
@@ -99,7 +101,47 @@ for row in rows:
         i_coun = 0
 so_conn.commit()        
 print("Inserted " + str(i_tota) + " rows...")
-funcfile.writelog("%t POPULATE TABLE: X003aa_vss_gl_join with " + str(i_tota) + " rows")
+funcfile.writelog("%t POPULATE TABLE: X003aa_vss_gl_join_eng with " + str(i_tota) + " rows")
+
+# Report on vss and gl transaction type join *******************************
+print("Report vss gl join transaction type...")
+sr_file = "X003ax_vss_gl_join_eng"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  X003aa_vss_gl_join_eng.CAMPUS_VSS AS CAMPUS,
+  X003aa_vss_gl_join_eng.MONTH_VSS AS MONTH,
+  X003aa_vss_gl_join_eng.TRANSCODE_VSS AS TRANCODE,
+  X003aa_vss_gl_join_eng.TEMP_DESC_E AS VSS_DESCRIPTION,
+  CAST(X003aa_vss_gl_join_eng.AMOUNT_VSS AS REAL) AS VSS_AMOUNT,
+  X003aa_vss_gl_join_eng.DESC_VSS AS GL_DESCRIPTION,
+  CAST(X003aa_vss_gl_join_eng.AMOUNT AS REAL) AS GL_AMOUNT,
+  X003aa_vss_gl_join_eng.DIFF,
+  X003aa_vss_gl_join_eng.MATCHED,
+  X003aa_vss_gl_join_eng.PERIOD
+FROM
+  X003aa_vss_gl_join_eng
+ORDER BY
+  CAMPUS,
+  MONTH
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)    
+print("Export vss gl recon...")
+sr_filet = sr_file
+sx_path = re_path + funcdate.cur_year() + "/"
+sx_file = "Debtor_003_vss_gl_recon_eng_"
+sx_filet = sx_file + funcdate.cur_monthendfile()
+s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
+funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
+funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
+funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
+
+
+
+
+
 
 
 # Close the table connection ***************************************************
