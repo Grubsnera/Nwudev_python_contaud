573438ff3e9823f278515e0a4da1025c01ac659e
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-02-05 15:42:55 +0200

    Update Report_vss_deferments - dev.py
    Adding balance after all credits

--- Report_vss_deferments - dev.py
+++ Report_vss_deferments - dev.py
@@ -46,28 +46,7 @@ funcfile.writelog("%t ATTACH DATABASE: Kfs_vss_studdeb.sqlite")
 
 # ADD THE BALANCES TO THE LIST OF REGISTERED STUDENTS **************************
 
-print("Add the calculated balances to the students list...")
-sr_file = "X001aa_Students"
-s_sql = "CREATE TABLE " + sr_file+ " AS" + """
-SELECT
-  X000_Students_curr.*,
-  X001aa_Trans_balopen.BAL_OPEN,
-  X001ad_Trans_balreg.BAL_REG,
-  X001ae_Trans_crebefreg.CRE_REG_BEFORE,
-  X001af_Trans_creaftreg.CRE_REG_AFTER,
-  X001ab_Trans_feereg.FEE_REG,
-  CAST(0 AS REAL) AS BAL_REG_CALC
-FROM
-  X000_Students_curr
-  LEFT JOIN X001ad_Trans_balreg ON X001ad_Trans_balreg.STUDENT_VSS = X000_Students_curr.KSTUDBUSENTID
-  LEFT JOIN X001aa_Trans_balopen ON X001aa_Trans_balopen.STUDENT_VSS = X000_Students_curr.KSTUDBUSENTID
-  LEFT JOIN X001ae_Trans_crebefreg ON X001ae_Trans_crebefreg.STUDENT_VSS = X000_Students_curr.KSTUDBUSENTID
-  LEFT JOIN X001af_Trans_creaftreg ON X001af_Trans_creaftreg.STUDENT_VSS = X000_Students_curr.KSTUDBUSENTID
-  LEFT JOIN X001ab_Trans_feereg ON X001ab_Trans_feereg.STUDENT_VSS = X000_Students_curr.KSTUDBUSENTID
-"""
-so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
-so_curs.execute(s_sql)
-funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
 # Calc indicator for non-entering and first-time students
 print("Add column bal_reg_calc...")
 so_curs.execute("UPDATE " + sr_file + """
