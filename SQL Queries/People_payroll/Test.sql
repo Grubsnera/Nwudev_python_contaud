92a65bcf589e8b7be9ff7464350ad8e72380d77e
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-02-07 11:59:29 +0200

    Development queries

--- SQL Queries/People_payroll/Test.sql
+++
@@ -1,7 +0,0 @@
-﻿SELECT
-  X001aa_element_package_prev.ASSIGNMENT_ID,
-  Count(X001aa_element_package_prev.INPUT_VALUE_ID) AS Count_INPUT_VALUE_ID
-FROM
-  X001aa_element_package_prev
-GROUP BY
-  X001aa_element_package_prev.ASSIGNMENT_ID
