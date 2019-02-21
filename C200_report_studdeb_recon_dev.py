37fd96196c3d9701237b9046be3703ffa68609c5
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-02-21 13:13:46 +0200

    Update C200_report_studdeb_recon_dev.py
    Development tests

--- C200_report_studdeb_recon_dev.py
+++ C200_report_studdeb_recon_dev.py
@@ -50,6 +50,8 @@ so_curs.execute("ATTACH DATABASE 'W:/Kfs/Kfs.sqlite' AS 'KFS'")
 funcfile.writelog("%t ATTACH DATABASE: KFS.SQLITE")
 so_curs.execute("ATTACH DATABASE 'W:/Vss/Vss.sqlite' AS 'VSS'")
 funcfile.writelog("%t ATTACH DATABASE: VSS.SQLITE")
+so_curs.execute("ATTACH DATABASE 'W:/Kfs_vss_studdeb/Kfs_vss_studdeb_prev.sqlite' AS 'PREV'")
+funcfile.writelog("%t ATTACH DATABASE: VSS.SQLITE")
 
 # Open the MYSQL DESTINATION table
 s_database = "Web_ia_nwu"
@@ -59,19 +61,108 @@ funcfile.writelog("%t OPEN MYSQL DATABASE: " + s_database)
 
 # Development script ***********************************************************
 
+# CALCULATE OPENING BALANCES ***************************************************
+print("Sum vss student opening balances per campus...")
+sr_file = "X002da_vss_student_balance_open"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  X002ab_vss_transort.CAMPUS_VSS AS CAMPUS,
+  X002ab_vss_transort.STUDENT_VSS AS STUDENT,  
+  0.00 AS BAL_DT,
+  0.00 AS BAL_CT,
+  Round(Total(X002ab_vss_transort.AMOUNT_VSS),2) AS BALANCE
+FROM
+  X002ab_vss_transort
+WHERE
+  X002ab_vss_transort.MONTH_VSS = '00'
+GROUP BY
+  X002ab_vss_transort.STUDENT_VSS,
+  X002ab_vss_transort.CAMPUS_VSS
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)    
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
+# Add column vss debit amount
+print("Add column vss dt amount...")
+so_curs.execute("UPDATE X002da_vss_student_balance_open " + """
+                SET BAL_DT = 
+                CASE
+                   WHEN BALANCE > 0 THEN BALANCE
+                   ELSE 0.00
+                END
+                ;""")
+so_conn.commit()
+funcfile.writelog("%t ADD COLUMN: Vss debit amount")
+# Add column vss credit amount
+print("Add column vss ct amount...")
+so_curs.execute("UPDATE X002da_vss_student_balance_open " + """
+                SET BAL_CT = 
+                CASE
+                   WHEN BALANCE < 0 THEN BALANCE
+                   ELSE 0.00
+                END
+                ;""")
+so_conn.commit()
+funcfile.writelog("%t ADD COLUMN: Vss credit amount")
 
+# JOIN PREVIOUS BALANCE AND CURRENT OPENING BALANCE ****************************
+print("Join previous balance and current opening balance...")
+sr_file = "X002dc_vss_prevbal_curopen"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  PREV.X002da_vss_student_balance.CAMPUS,
+  PREV.X002da_vss_student_balance.STUDENT,
+  round(PREV.X002da_vss_student_balance.BALANCE,2) AS BAL_PREV,
+  round(X002da_vss_student_balance_open.BALANCE,2) AS BAL_OPEN,
+  0.00 AS DIFF_BAL
+FROM
+  PREV.X002da_vss_student_balance
+  LEFT JOIN X002da_vss_student_balance_open ON X002da_vss_student_balance_open.CAMPUS =
+  PREV.X002da_vss_student_balance.CAMPUS AND X002da_vss_student_balance_open.STUDENT = PREV.X002da_vss_student_balance.STUDENT
+WHERE
+  PREV.X002da_vss_student_balance.BALANCE <> 0.00
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)    
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
+# Add column vss debit amount
+print("COrrect the open balance column id null...")
+so_curs.execute("UPDATE X002dc_vss_prevbal_curopen " + """
+                SET BAL_OPEN =
+                CASE
+                  WHEN BAL_OPEN IS NULL THEN 0.00
+                  ELSE BAL_OPEN
+                END
+                ;""")
+so_conn.commit()
+# Add column vss debit amount
+print("Add column vss dt amount...")
+so_curs.execute("UPDATE X002dc_vss_prevbal_curopen " + """
+                SET DIFF_BAL = round(BAL_OPEN - BAL_PREV,2)
+                ;""")
+so_conn.commit()
 
-
-
-
-
-
-
-
-
-
-
-
+# SELECT STUDENTS WHERE OPENING BALANCE DIFFER FROM CLOSING BALANCE ************
+print("Select students where closing and opening balances differ...")
+sr_file = "X002dd_vss_closing_open_differ"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  X002dc_vss_prevbal_curopen.CAMPUS,
+  X002dc_vss_prevbal_curopen.STUDENT,
+  X002dc_vss_prevbal_curopen.BAL_PREV,
+  X002dc_vss_prevbal_curopen.BAL_OPEN,
+  X002dc_vss_prevbal_curopen.DIFF_BAL
+FROM
+  X002dc_vss_prevbal_curopen
+WHERE
+  X002dc_vss_prevbal_curopen.DIFF_BAL <> 0
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)    
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
 
 # Close the table connection ***************************************************
 so_conn.commit()
