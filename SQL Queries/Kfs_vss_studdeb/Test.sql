9ee797f7198a1192f3ddebff40d2392f7bb2fbf3
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-02-21 13:14:16 +0200

    SQL Testing
    SQL Testing

---
+++ SQL Queries/Kfs_vss_studdeb/Test.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+  X002dc_vss_prevbal_curopen.CAMPUS,
+  X002dc_vss_prevbal_curopen.STUDENT,
+  X002dc_vss_prevbal_curopen.BAL_PREV,
+  X002dc_vss_prevbal_curopen.BAL_OPEN,
+  X002dc_vss_prevbal_curopen.DIFF_BAL
+FROM
+  X002dc_vss_prevbal_curopen
+WHERE
+  (X002dc_vss_prevbal_curopen.DIFF_BAL <> 0) OR
+  (X002dc_vss_prevbal_curopen.DIFF_BAL IS NULL AND
+  X002dc_vss_prevbal_curopen.BAL_PREV <> 0)
