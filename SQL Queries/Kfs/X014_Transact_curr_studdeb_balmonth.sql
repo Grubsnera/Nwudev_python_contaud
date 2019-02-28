4aa8728a9410a2fee41e159d083d0872b56aac9a
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-02-28 15:51:03 +0200

    Query development

--- SQL Queries/Kfs/X014_Transact_curr_studdeb_balmonth.sql
+++
@@ -1,9 +0,0 @@
-﻿SELECT
-  X010_Studdeb_tranlist.CAMPUS AS CAMPUS,
-  X010_Studdeb_tranlist.UNIV_FISCAL_PRD_CD AS MONTH,
-  Sum(X010_Studdeb_tranlist.CALC_AMOUNT) AS BALANCE
-FROM
-  X010_Studdeb_tranlist
-GROUP BY
-  X010_Studdeb_tranlist.CAMPUS,
-  X010_Studdeb_tranlist.UNIV_FISCAL_PRD_CD
