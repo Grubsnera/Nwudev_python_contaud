1527e7a77cfee933f0730129a8fb4b2760600f82
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-03-05 11:31:52 +0200

    Create C001_people_test_masterfile_xdev.py
    People mater file tests development area

---
+++ C001_people_test_masterfile_xdev.py
@@ -0,0 +1,94 @@
+""" Script to test PEOPLE master file data *************************************
+Created on: 1 Mar 2019
+Author: Albert J v Rensburg (NWU21162395)
+*****************************************************************************"""
+
+""" INDEX **********************************************************************
+ENVIRONMENT
+OPEN THE DATABASES
+BEGIN OF SCRIPT
+END OF SCRIPT
+*****************************************************************************"""
+
+"""*****************************************************************************
+ENVIRONMENT
+*****************************************************************************"""
+
+# IMPORT PYTHON MODULES
+import csv
+import datetime
+import sqlite3
+import sys
+
+# ADD OWN MODULE PATH
+sys.path.append('S:/_my_modules')
+
+# IMPORT OWN MODULES
+import funccsv
+import funcdate
+import funcfile
+import funcmail
+import funcmysql
+import funcpeople
+import funcstr
+import funcsys
+
+# OPEN THE SCRIPT LOG FILE
+print("-----------------")    
+print("B001_PEOPLE_LISTS")
+print("-----------------")
+funcfile.writelog("Now")
+funcfile.writelog("SCRIPT: B001_PEOPLE_LISTS_DEV")
+funcfile.writelog("-----------------------------")
+ilog_severity = 1
+
+# DECLARE VARIABLES
+so_path = "W:/People/" #Source database path
+re_path = "R:/People/" # Results path
+ed_path = "S:/_external_data/" #external data path
+so_file = "People_test_masterfile.sqlite" # Source database
+s_sql = "" # SQL statements
+l_export = False
+l_mail = True
+
+"""*****************************************************************************
+OPEN THE DATABASES
+*****************************************************************************"""
+print("OPEN THE DATABASES")
+funcfile.writelog("OPEN THE DATABASES")
+
+# OPEN THE WORKING DATABASE
+with sqlite3.connect(so_path+so_file) as so_conn:
+    so_curs = so_conn.cursor()
+funcfile.writelog("%t OPEN DATABASE: PEOPLE_TEST_MASTERFILE.SQLITE")
+
+# ATTACH DATA SOURCES
+so_curs.execute("ATTACH DATABASE 'W:/People/People.sqlite' AS 'PEOPLE'")
+funcfile.writelog("%t ATTACH DATABASE: PEOPLE.SQLITE")
+
+""" ****************************************************************************
+BEGIN OF SCRIPT
+*****************************************************************************"""
+print("BEGIN OF SCRIPT")
+funcfile.writelog("BEGIN OF SCRIPT")
+
+""" ****************************************************************************
+TEST ZA ID INVALID
+*****************************************************************************"""
+print("TEST ZA ID INVALID")
+funcfile.writelog("TEST ZA ID INVALID")
+
+
+
+""" ****************************************************************************
+END OF SCRIPT
+*****************************************************************************"""
+print("END OF SCRIPT")
+funcfile.writelog("END OF SCRIPT")
+
+# CLOSE THE DATABASE CONNECTION
+so_conn.close()
+
+# CLOSE THE LOG WRITER
+funcfile.writelog("--------------------------------------")
+funcfile.writelog("COMPLETED: C001_PEOPLE_TEST_MASTERFILE")
