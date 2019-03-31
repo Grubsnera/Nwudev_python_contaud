4db0c65974b52099b2541543fac271a1302ba148
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-03-31 18:50:05 +0200

    Update B001_people_lists.py
    Moved birthday list to master file tests

--- B001_people_lists.py
+++ B001_people_lists.py
@@ -1500,9 +1500,6 @@ def People_lists():
         funcfile.writelog("%t EXPORT DATA: " + sx_path + sx_filet)
 
 
-
-
-
     # Create MYSQL PEOPLE TO WEB table *****************************************
     print("Build mysql current people...")
     ms_curs.execute("DROP TABLE IF EXISTS ia_people")
@@ -1998,56 +1995,6 @@ def People_lists():
 
     if l_mail == True:
         funcmail.Mail("hr_people_organogram")
-
-    # BUILD PEOPLE BIRTHDAYS
-    print("Build people birthday...")
-    s_sql = "CREATE TABLE X003_PEOPLE_BIRT AS " + """
-    SELECT
-        X002_PEOPLE_CURR.EMPLOYEE_NUMBER,
-        X002_PEOPLE_CURR.NAME_LIST,
-        X002_PEOPLE_CURR.KNOWN_NAME,
-        X002_PEOPLE_CURR.DATE_OF_BIRTH,
-        X002_PEOPLE_CURR.AGE,
-        X002_PEOPLE_CURR.POSITION_FULL,
-        X002_PEOPLE_CURR.OE_CODE
-    FROM
-        X002_PEOPLE_CURR
-    WHERE
-        %WHERE%
-    ORDER BY
-        StrfTime('%m-%d', X002_PEOPLE_CURR.DATE_OF_BIRTH),
-        X002_PEOPLE_CURR.AGE DESC,
-        X002_PEOPLE_CURR.NAME_LIST
-    """
-    so_curs.execute("DROP TABLE IF EXISTS X003_PEOPLE_BIRT")
-    if funcdate.today_dayname() == "Fri":
-        s_sql = s_sql.replace("%WHERE%","StrfTime('%m-%d',X002_PEOPLE_CURR.DATE_OF_BIRTH)>=StrfTime('%m-%d','now') AND StrfTime('%m-%d',X002_PEOPLE_CURR.DATE_OF_BIRTH)<=StrfTime('%m-%d','now','+2 day')")
-    else:
-        s_sql = s_sql.replace("%WHERE%","StrfTime('%m-%d',X002_PEOPLE_CURR.DATE_OF_BIRTH)>=StrfTime('%m-%d','now') AND StrfTime('%m-%d',X002_PEOPLE_CURR.DATE_OF_BIRTH)<=StrfTime('%m-%d','now')")
-    #print(s_sql) # DEBUG
-    so_curs.execute(s_sql)
-    so_conn.commit()
-    funcfile.writelog("%t BUILD TABLE: X003_PEOPLE_BIRT")        
-    # Export the birthdays
-    """
-    if l_export == True:
-        sr_file = "X003_PEOPLE_BIRT"
-        sr_filet = sr_file
-        sx_path = re_path + funcdate.cur_year() + "/"
-        sx_file = "People_003_birthday_"
-        sx_filet = sx_file + funcdate.cur_month()
-        print("Export people birthday..." + sx_path + sx_filet)
-        # Read the header data
-        s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
-        # Write the data
-        funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
-        funcfile.writelog("%t EXPORT DATA: " + sx_path + sx_file)
-    """
-    # Mail the birthdays
-    """
-    if l_mail == True:
-        funcmail.Mail("hr_people_birthday")
-    """
 
     # Delete some unncessary files *************************************************
 
