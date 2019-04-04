1e8bcdcdd3302ebd7a48a5a35c26df511b37bf89
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-04-04 15:46:37 +0200

    Query development

---
+++ SQL Queries/Kfs/Prev_pay_docs.sql
@@ -0,0 +1,7 @@
+﻿Select
+    X001_Report_payments_prev.*,
+    X000_Documents.DOC_TYP_NM,
+    X000_Documents.LBL
+From
+    X001_Report_payments_prev Left Join
+    X000_Documents On X000_Documents.DOC_HDR_ID = X001_Report_payments_prev.CUST_PMT_DOC_NBR
---
+++ SQL Queries/Kfs/Test.sql
@@ -0,0 +1,9 @@
+﻿Select
+    X000_Vendor.PAYEE_ID_NBR,
+    X000_Vendor.PAYEE_ID_TYP_CD,
+    Count(X000_Vendor.BNK_ACCT_NBR) As Count_BNK_ACCT_NBR
+From
+    X000_Vendor
+Group By
+    X000_Vendor.PAYEE_ID_NBR,
+    X000_Vendor.PAYEE_ID_TYP_CD
