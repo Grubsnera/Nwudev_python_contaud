fbd201b3098f97e5b8c219234f62f764ec5b81ac
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-04-07 06:30:13 +0200

    Update B002_kfs_lists_xdev.py
    Vacuum kfs dev area

--- B002_kfs_lists_xdev.py
+++ B002_kfs_lists_xdev.py
@@ -1,56 +1,95 @@
-"""
-Script to build standard KFS lists
+""" Script to build standard KFS lists *****************************************
 Created on: 11 Mar 2018
-Copyright: Albert J v Rensburg
-"""
+Author: Albert J v Rensburg (NWU21162395)
+*************************************************************************** """
 
-# Import python modules
+""" INDEX **********************************************************************
+ENVIRONMENT
+OPEN THE DATABASES
+BEGIN OF SCRIPT
+END OF SCRIPT
+*****************************************************************************"""
+
+"""*****************************************************************************
+ENVIRONMENT
+*****************************************************************************"""
+
+# IMPORT PYTHON MODULES
+import csv
 import datetime
 import sqlite3
 import sys
 
-# Add own module path
-sys.path.append('S:\\_my_modules')
+# ADD OWN MODULE PATH
+sys.path.append('S:/_my_modules')
 
-# Import own modules
-import funcdate
+# IMPORT OWN MODULES
 import funccsv
+import funcdate
 import funcfile
+import funcmail
+import funcmysql
+import funcpeople
+import funcstr
+import funcsys
 
-# Open the script log file ******************************************************
-
+# OPEN THE SCRIPT LOG FILE
+print("------------------")    
+print("B002_KFS_LISTS_DEV")
+print("------------------")
 funcfile.writelog("Now")
-funcfile.writelog("SCRIPT: B002_KFS_LISTS")
-funcfile.writelog("----------------------")
-print("--------------")
-print("B002_KFS_LISTS")
-print("--------------")
+funcfile.writelog("SCRIPT: B002_KFS_LISTS_DEV")
+funcfile.writelog("--------------------------")
 ilog_severity = 1
 
-# Declare variables
+# DECLARE VARIABLES
 so_path = "W:/Kfs/" #Source database path
-so_file = "Kfs.sqlite" #Source database
-s_sql = "" #SQL statements
+re_path = "R:/Kfs/" # Results path
+ed_path = "S:/_external_data/" #external data path
+so_file = "Kfs.sqlite" # Source database
+s_sql = "" # SQL statements
+l_export = False
+l_mail = False
+l_record = False
 
-# Open the SOURCE file
+"""*****************************************************************************
+OPEN THE DATABASES
+*****************************************************************************"""
+print("OPEN THE DATABASES")
+funcfile.writelog("OPEN THE DATABASES")
+
+# OPEN THE WORKING DATABASE
 with sqlite3.connect(so_path+so_file) as so_conn:
     so_curs = so_conn.cursor()
+funcfile.writelog("%t OPEN DATABASE: " + so_file)
 
-funcfile.writelog("%t OPEN DATABASE: KFS")
+"""# ATTACH DATA SOURCES
+so_curs.execute("ATTACH DATABASE 'W:/People/People.sqlite' AS 'PEOPLE'")
+funcfile.writelog("%t ATTACH DATABASE: PEOPLE.SQLITE")"""
 
-"""*****************************************************************************
-BEGIN
+"""# OPEN THE MYSQL DESTINATION TABLE
+s_database = "Web_ia_nwu"
+ms_cnxn = funcmysql.mysql_open(s_database)
+ms_curs = ms_cnxn.cursor()
+funcfile.writelog("%t OPEN MYSQL DATABASE: " + s_database)"""
+
+""" ****************************************************************************
+BEGIN OF SCRIPT
 *****************************************************************************"""
+print("BEGIN OF SCRIPT")
+funcfile.writelog("BEGIN OF SCRIPT")
 
-  
 
-"""*****************************************************************************
-END
+
+""" ****************************************************************************
+END OF SCRIPT
 *****************************************************************************"""
+print("END OF SCRIPT")
+funcfile.writelog("END OF SCRIPT")
 
-# Close the table connection ***************************************************
+# CLOSE THE DATABASE CONNECTION
 so_conn.close()
 
-# Close the log writer *********************************************************
-funcfile.writelog("-------------------------")
-funcfile.writelog("COMPLETED: B002_KFS_LISTS")
+# CLOSE THE LOG WRITER
+funcfile.writelog("-----------------------------")
+funcfile.writelog("COMPLETED: B002_KFS_LISTS_DEV")
