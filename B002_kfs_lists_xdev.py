4e23450d4b4ca0a2f1b0c41cff7e5b09d958d4b6
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-04-10 16:16:38 +0200

    Update B002_kfs_lists_xdev.py
    Vacuum kfs lists dev area

--- B002_kfs_lists_xdev.py
+++ B002_kfs_lists_xdev.py
@@ -69,51 +69,7 @@ BEGIN OF SCRIPT
 print("BEGIN OF SCRIPT")
 funcfile.writelog("BEGIN OF SCRIPT")
 
-# BUILD VENDOR TABLE
-print("Build vendor master file...")
-sr_file = "X000_VENDOR_MASTER"
-s_sql = "CREATE TABLE "+sr_file+" AS " + """
-Select
-    PUR_VNDR_DTL_T.VNDR_ID As VENDOR_ID,
-    UPPER(PUR_VNDR_DTL_T.VNDR_NM) AS VNDR_NM,
-    PUR_VNDR_DTL_T.VNDR_URL_ADDR,
-    PUR_VNDR_HDR_T.VNDR_TAX_NBR,
-    X001ad_vendor_bankacc.VEND_BANK,
-    X001ad_vendor_bankacc.VEND_MAIL,
-    X001ad_vendor_bankacc.EMPL_BANK,
-    X001ad_vendor_bankacc.STUD_BANK,
-    X001ac_vendor_address_comb.FAX,
-    X001ac_vendor_address_comb.EMAIL,
-    X001ac_vendor_address_comb.ADDRESS,
-    X001ac_vendor_address_comb.URL,
-    X001ac_vendor_address_comb.STATE_CD,
-    X001ac_vendor_address_comb.COUNTRY_CD,
-    PUR_VNDR_HDR_T.VNDR_TAX_TYP_CD,
-    PUR_VNDR_HDR_T.VNDR_TYP_CD,
-    PUR_VNDR_DTL_T.VNDR_PMT_TERM_CD,
-    PUR_VNDR_DTL_T.VNDR_SHP_TTL_CD,
-    PUR_VNDR_DTL_T.VNDR_PARENT_IND,
-    PUR_VNDR_DTL_T.VNDR_1ST_LST_NM_IND,
-    PUR_VNDR_DTL_T.COLLECT_TAX_IND,
-    PUR_VNDR_HDR_T.VNDR_FRGN_IND,
-    PUR_VNDR_DTL_T.VNDR_CNFM_IND,
-    PUR_VNDR_DTL_T.VNDR_PRPYMT_IND,
-    PUR_VNDR_DTL_T.VNDR_CCRD_IND,
-    PUR_VNDR_DTL_T.DOBJ_MAINT_CD_ACTV_IND,
-    PUR_VNDR_DTL_T.VNDR_INACTV_REAS_CD
-From
-    PUR_VNDR_DTL_T Left Join
-    PUR_VNDR_HDR_T On PUR_VNDR_HDR_T.VNDR_HDR_GNRTD_ID = PUR_VNDR_DTL_T.VNDR_HDR_GNRTD_ID Left Join
-    X001ac_vendor_address_comb On X001ac_vendor_address_comb.VENDOR_ID = PUR_VNDR_DTL_T.VNDR_ID Left Join
-    X001ad_vendor_bankacc On X001ad_vendor_bankacc.VENDOR_ID = PUR_VNDR_DTL_T.VNDR_ID
-Order by
-    VNDR_NM,
-    VENDOR_ID
-"""
-so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
-so_curs.execute(s_sql)
-so_conn.commit()
-funcfile.writelog("%t BUILD TABLE: "+sr_file)
+
 
 """ ****************************************************************************
 END OF SCRIPT
