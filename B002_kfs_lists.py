4ba537d7edbe43212cbda99c099a708db7b3b99a
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-04-13 11:19:49 +0200

    Update B002_kfs_lists.py
    Build payments

--- B002_kfs_lists.py
+++ B002_kfs_lists.py
@@ -17,6 +17,10 @@ END OF SCRIPT
 
 def Kfs_lists():
 
+    """*****************************************************************************
+    ENVIRONMENT
+    *****************************************************************************"""
+
     # Import python modules
     import datetime
     import sqlite3
@@ -46,11 +50,26 @@ def Kfs_lists():
     s_sql = "" #SQL statements
     l_vacuum = False # Vacuum database
 
-    # Open the SOURCE file
+    """*****************************************************************************
+    OPEN THE DATABASES
+    *****************************************************************************"""
+    print("OPEN THE DATABASES")
+    funcfile.writelog("OPEN THE DATABASES")
+
+    # OPEN THE WORKING DATABASE
     with sqlite3.connect(so_path+so_file) as so_conn:
         so_curs = so_conn.cursor()
+    funcfile.writelog("%t OPEN DATABASE: " + so_file)
 
-    funcfile.writelog("%t OPEN DATABASE: KFS")
+    # ATTACH DATA SOURCES
+    so_curs.execute("ATTACH DATABASE 'W:/People/People.sqlite' AS 'PEOPLE'")
+    funcfile.writelog("%t ATTACH DATABASE: PEOPLE.SQLITE")    
+
+    """ ****************************************************************************
+    BEGIN OF SCRIPT
+    *****************************************************************************"""
+    print("BEGIN OF SCRIPT")
+    funcfile.writelog("BEGIN OF SCRIPT")
 
     # Add calculated fields to the current gl transaction list *********************
 
@@ -617,6 +636,46 @@ def Kfs_lists():
     print("BUILD CURRENT YEAR PAYMENTS")
     funcfile.writelog("BUILD CURRENT YEAR PAYMENTS")
 
+    # BUILD CURR APPROVALS UNIQUE LIST
+    print("Build current unique approvers list...")
+    sr_file = "X000_Approvers_curr_last"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select Distinct
+        ROUTE.ACTN_TKN_ID,
+        ROUTE.DOC_HDR_ID,
+        ROUTE.ACTN_DT,
+        ROUTE.PRNCPL_ID,
+        CASE
+            WHEN ROUTE.PRNCPL_ID = '26807815' THEN 'KFS WORKFLOW SYSTEM USER'
+            WHEN PERSON.NAME_ADDR IS NULL THEN 'UNKNOWN'
+            ELSE PERSON.NAME_ADDR
+        END AS NAME_ADDR,
+        ROUTE.ACTN_CD,
+        CASE
+            WHEN ROUTE.ACTN_CD = 'a' THEN 'SUPER USER APPROVED'
+            WHEN ROUTE.ACTN_CD = 'A' THEN 'APPROVED'
+            WHEN ROUTE.ACTN_CD = 'B' THEN 'BLANKET APPROVED'
+            WHEN ROUTE.ACTN_CD = 'r' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'R' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'v' THEN 'SUPER USER APPROVED'
+           ELSE 'OTHER'
+        END AS ACTN,
+        ROUTE.ANNOTN,
+        Count(ROUTE.DOC_VER_NBR) As APP_COUNT
+    From
+        KREW_ACTN_TKN_T_APP ROUTE Left Join
+        PEOPLE.X002_PEOPLE_CURR_YEAR PERSON On PERSON.EMPLOYEE_NUMBER = ROUTE.PRNCPL_ID
+    Group By
+        ROUTE.DOC_HDR_ID
+    Order By
+        ROUTE.ACTN_DT,
+        ROUTE.ACTN_TKN_ID
+    """
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
     # BUILD PAYMENTS
     print("Build current year payments...")
     sr_file = "X001aa_Report_payments_curr"
@@ -642,15 +701,34 @@ def Kfs_lists():
         DETAIL.PO_NBR,
         DETAIL.INV_DT,
         DETAIL.ORIG_INV_AMT,
-        DETAIL.NET_PMT_AMT
+        DETAIL.NET_PMT_AMT,
+        APPROVE.PRNCPL_ID AS APPROVE_EMP_NO,
+        APPROVE.NAME_ADDR AS APPROVE_EMP_NAME,
+        APPROVE.ACTN_DT AS APPROVE_DATE,
+        APPROVE.ACTN AS APPROVE_STATUS,
+        APPROVE.ANNOTN AS NOTE
     From
         PDP_PMT_GRP_T_CURR PAYMENT
-        Left Join X000_VENDOR_MASTER VENDOR On VENDOR.VENDOR_ID = PAYMENT.PAYEE_ID
+        Left Join X000_Vendor_master VENDOR On VENDOR.VENDOR_ID = PAYMENT.PAYEE_ID
         Left Join PDP_PAYEE_ACH_ACCT_T PAYEE On PAYEE.PAYEE_ID_NBR = PAYMENT.PAYEE_ID And
             PAYEE.PAYEE_ID_TYP_CD = PAYMENT.PAYEE_ID_TYP_CD
         Left Join PDP_PAYEE_TYP_T TYPE ON TYPE.PAYEE_TYP_CD = PAYMENT.PAYEE_ID_TYP_CD
         Left Join PDP_PMT_STAT_CD_T STATUS On STATUS.PMT_STAT_CD = PAYMENT.PMT_STAT_CD
         Left Join PDP_PMT_DTL_T DETAIL On DETAIL.PMT_GRP_ID = PAYMENT.PMT_GRP_ID
+        Left Join X000_Approvers_curr_last APPROVE On APPROVE.DOC_HDR_ID = DETAIL.CUST_PMT_DOC_NBR
+    """
+    """
+        ROUTE.PRNCPL_ID AS APPROVER,
+        CASE
+            WHEN ACTN_CD = 'a' THEN 'APPROVED'
+            WHEN ACTN_CD = 'A' THEN 'SUPER USED APPROVED'
+            WHEN ACTN_CD = 'B' THEN 'BLANKET APPROVED'
+            WHEN ACTN_CD = 'R' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            ELSE 'OTHER'
+        END AS APPROVE_STATUS,
+        ROUTE.ACTN_DT AS APPROVE_DATE
+
+        Left Join KREW_ACTN_TKN_T_APP ROUTE On ROUTE.DOC_HDR_ID = DETAIL.CUST_PMT_DOC_NBR
     """
     so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
     so_curs.execute(s_sql)
@@ -682,6 +760,45 @@ def Kfs_lists():
     print("BUILD PREVIOUS YEAR PAYMENTS")
     funcfile.writelog("BUILD PREVIOUS YEAR PAYMENTS")
 
+    # BUILD PREV APPROVALS UNIQUE LIST
+    print("Build previous year unique approvers list...")
+    sr_file = "X000_Approvers_prev_last"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select Distinct
+        ROUTE.ACTN_TKN_ID,
+        ROUTE.DOC_HDR_ID,
+        ROUTE.ACTN_DT,
+        ROUTE.PRNCPL_ID,
+        CASE
+            WHEN PERSON.NAME_ADDR IS NULL THEN 'UNKNOWN'
+            ELSE PERSON.NAME_ADDR
+        END AS NAME_ADDR,
+        ROUTE.ACTN_CD,
+        CASE
+            WHEN ROUTE.ACTN_CD = 'a' THEN 'SUPER USER APPROVED'
+            WHEN ROUTE.ACTN_CD = 'A' THEN 'APPROVED'
+            WHEN ROUTE.ACTN_CD = 'B' THEN 'BLANKET APPROVED'
+            WHEN ROUTE.ACTN_CD = 'r' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'R' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'v' THEN 'SUPER USER APPROVED'
+           ELSE 'OTHER'
+        END AS ACTN,
+        ROUTE.ANNOTN,
+        Count(ROUTE.DOC_VER_NBR) As APP_COUNT
+    From
+        KREW_ACTN_TKN_T_APP ROUTE Left Join
+        PEOPLE.X002_PEOPLE_PREV_YEAR PERSON On PERSON.EMPLOYEE_NUMBER = ROUTE.PRNCPL_ID
+    Group By
+        ROUTE.DOC_HDR_ID
+    Order By
+        ROUTE.ACTN_DT,
+        ROUTE.ACTN_TKN_ID
+    """
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
     # BUILD PAYMENTS
     print("Build previous year payments...")
     sr_file = "X001aa_Report_payments_prev"
@@ -707,7 +824,12 @@ def Kfs_lists():
         DETAIL.PO_NBR,
         DETAIL.INV_DT,
         DETAIL.ORIG_INV_AMT,
-        DETAIL.NET_PMT_AMT
+        DETAIL.NET_PMT_AMT,
+        APPROVE.PRNCPL_ID AS APPROVE_EMP_NO,
+        APPROVE.NAME_ADDR AS APPROVE_EMP_NAME,
+        APPROVE.ACTN_DT AS APPROVE_DATE,
+        APPROVE.ACTN AS APPROVE_STATUS,
+        APPROVE.ANNOTN AS NOTE    
     From
         PDP_PMT_GRP_T_PREV PAYMENT
         Left Join X000_VENDOR_MASTER VENDOR On VENDOR.VENDOR_ID = PAYMENT.PAYEE_ID
@@ -716,6 +838,7 @@ def Kfs_lists():
         Left Join PDP_PAYEE_TYP_T TYPE ON TYPE.PAYEE_TYP_CD = PAYMENT.PAYEE_ID_TYP_CD
         Left Join PDP_PMT_STAT_CD_T STATUS On STATUS.PMT_STAT_CD = PAYMENT.PMT_STAT_CD
         Left Join PDP_PMT_DTL_T DETAIL On DETAIL.PMT_GRP_ID = PAYMENT.PMT_GRP_ID
+        Left Join X000_Approvers_prev_last APPROVE On APPROVE.DOC_HDR_ID = DETAIL.CUST_PMT_DOC_NBR
     """
     so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
     so_curs.execute(s_sql)
