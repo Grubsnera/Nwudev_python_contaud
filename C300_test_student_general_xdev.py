1f8819d89b74c4b3b987489aab0cf58daa7dcac8
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-04-16 20:09:57 +0200

    Update C300_test_student_general_xdev.py
    No idea

--- C300_test_student_general_xdev.py
+++ C300_test_student_general_xdev.py
@@ -31,7 +31,7 @@ print("-------------------------")
 ilog_severity = 1
 
 # Declare variables
-so_path = "W:/Vss/" #Source database path
+so_path = "W:/Vss_general/" #Source database path
 re_path = "R:/Vss/" #Results
 ed_path = "S:/_external_data/"
 so_file = "Vss_general.sqlite" #Source database
@@ -45,14 +45,44 @@ with sqlite3.connect(so_path+so_file) as so_conn:
 
 funcfile.writelog("%t OPEN DATABASE: VSS_GENERAL")
 
-so_curs.execute("ATTACH DATABASE 'W:/Vss.sqlite' AS 'VSS'")
+so_curs.execute("ATTACH DATABASE 'W:/vss/Vss.sqlite' AS 'VSS'")
 funcfile.writelog("%t ATTACH DATABASE: Vss.sqlite")
 
 """ ****************************************************************************
 BEGIN
 *****************************************************************************"""
 
-
+# Join the tran and party data *********************************************
+print("IDNo list join the vss tran and party data...")
+sr_file = "X001bb_join_tran_vss"
+s_sql = "CREATE TABLE "+sr_file+" AS " + """
+SELECT
+  VSS.X000_party.IDNO,
+  X001aa_impo_vsstran.STUDENT,
+  VSS.X000_Party.FULL_NAME AS NAME,
+  X001aa_impo_vsstran.YEAR,
+  X001aa_impo_vsstran.CAMPUS,
+  Trim(VSS.X000_Party.FIRSTNAMES) AS FIRSTNAME,
+  VSS.X000_Party.INITIALS,
+  VSS.X000_Party.SURNAME,
+  VSS.X000_Party.TITLE,
+  VSS.X000_Party.DATEOFBIRTH,
+  VSS.X000_Party.GENDER,
+  VSS.X000_Party.NATIONALITY,
+  VSS.X000_Party.POPULATION,
+  VSS.X000_Party.RACE,
+  VSS.X000_Party.FAUDITUSERCODE AS PARTY_AUDITDATETIME,
+  VSS.X000_Party.AUDITDATETIME AS PARTY_AUDITUSERCODE
+FROM
+  X001aa_impo_vsstran
+  INNER JOIN VSS.X000_Party ON VSS.X000_Party.KBUSINESSENTITYID = X001aa_impo_vsstran.STUDENT
+ORDER BY
+  X001aa_impo_vsstran.STUDENT
+;"""
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: "+sr_file)
  
 """ ****************************************************************************
 END
