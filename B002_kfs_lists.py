c116b5ee662fc2fddffb0576be7f82138eeb6886
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-04-16 20:04:03 +0200

    Update B002_kfs_lists.py
    Add tables with last approver

--- B002_kfs_lists.py
+++ B002_kfs_lists.py
@@ -636,7 +636,7 @@ def Kfs_lists():
     print("BUILD CURRENT YEAR PAYMENTS")
     funcfile.writelog("BUILD CURRENT YEAR PAYMENTS")
 
-    # BUILD CURR APPROVALS UNIQUE LIST
+    # BUILD CURR APPROVALS UNIQUE LIST OF LAST APPROVER
     print("Build current unique approvers list...")
     sr_file = "X000_Approvers_curr_last"
     s_sql = "CREATE TABLE " + sr_file + " AS " + """
@@ -676,7 +676,7 @@ def Kfs_lists():
     so_conn.commit()
     funcfile.writelog("%t BUILD TABLE: " + sr_file)
 
-    # BUILD PAYMENTS
+    # BUILD PAYMENTS WITH LAST APPROVER
     print("Build current year payments...")
     sr_file = "X001aa_Report_payments_curr"
     s_sql = "CREATE TABLE " + sr_file + " AS " + """
@@ -754,13 +754,90 @@ def Kfs_lists():
     so_conn.commit()
     funcfile.writelog("%t BUILD TABLE: " + sr_file)
 
+    # BUILD CURR APPROVALS ALL APPROVERS
+    print("Build current approvers list...")
+    sr_file = "X000_Approvers_curr"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select Distinct
+        ROUTE.ACTN_TKN_ID,
+        ROUTE.DOC_HDR_ID,
+        ROUTE.ACTN_DT,
+        ROUTE.PRNCPL_ID,
+        CASE
+            WHEN ROUTE.PRNCPL_ID = '26807815' THEN 'KFS WORKFLOW SYSTEM USER'
+            WHEN PERSON.NAME_ADDR IS NULL THEN 'UNKNOWN'
+            ELSE PERSON.NAME_ADDR
+        END AS NAME_ADDR,
+        ROUTE.ACTN_CD,
+        CASE
+            WHEN ROUTE.ACTN_CD = 'a' THEN 'SUPER USER APPROVED'
+            WHEN ROUTE.ACTN_CD = 'A' THEN 'APPROVED'
+            WHEN ROUTE.ACTN_CD = 'B' THEN 'BLANKET APPROVED'
+            WHEN ROUTE.ACTN_CD = 'r' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'R' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'v' THEN 'SUPER USER APPROVED'
+           ELSE 'OTHER'
+        END AS ACTN,
+        ROUTE.ANNOTN
+    From
+        KREW_ACTN_TKN_T_APP ROUTE Left Join
+        PEOPLE.X002_PEOPLE_CURR_YEAR PERSON On PERSON.EMPLOYEE_NUMBER = ROUTE.PRNCPL_ID
+    Order By
+        ROUTE.ACTN_DT,
+        ROUTE.ACTN_TKN_ID
+    """
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # BUILD PAYMENT LIST WITH ALL APPROVERS
+    print("Build current payment full approvers list...")
+    sr_file = "X001ac_Report_payments_approute_curr"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select
+        PAYMENT.PMT_GRP_ID,
+        PAYMENT.VENDOR_ID,
+        PAYMENT.PAYEE_NAME,
+        PAYMENT.VENDOR_NAME,
+        PAYMENT.VENDOR_REG_NR,
+        PAYMENT.VENDOR_TAX_NR,
+        PAYMENT.VENDOR_BANK_NR,
+        PAYMENT.VENDOR_TYPE,
+        PAYMENT.PAYEE_TYP_DESC,
+        PAYMENT.DISB_NBR,
+        PAYMENT.DISB_TS,
+        PAYMENT.PMT_DT,
+        PAYMENT.PMT_STAT_CD,
+        PAYMENT.PAYMENT_STATUS,
+        PAYMENT.CUST_PMT_DOC_NBR,
+        PAYMENT.INV_NBR,
+        PAYMENT.REQS_NBR,
+        PAYMENT.PO_NBR,
+        PAYMENT.INV_DT,
+        PAYMENT.ORIG_INV_AMT,
+        PAYMENT.NET_PMT_AMT,
+        APPROVE.PRNCPL_ID AS APPROVE_EMP_NO,
+        APPROVE.NAME_ADDR AS APPROVE_EMP_NAME,
+        APPROVE.ACTN_DT AS APPROVE_DATE,
+        APPROVE.ACTN AS APPROVE_STATUS,
+        APPROVE.ANNOTN AS NOTE    
+    From
+        X001aa_Report_payments_curr PAYMENT Left Join
+        X000_Approvers_curr APPROVE On APPROVE.DOC_HDR_ID = PAYMENT.CUST_PMT_DOC_NBR
+    """
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
     """ ****************************************************************************
     BUILD PREVIOUS YEAR PAYMENTS
     *****************************************************************************"""
     print("BUILD PREVIOUS YEAR PAYMENTS")
     funcfile.writelog("BUILD PREVIOUS YEAR PAYMENTS")
 
-    # BUILD PREV APPROVALS UNIQUE LIST
+    # BUILD PREV APPROVALS UNIQUE LIST WITH LAST APPROVER
     print("Build previous year unique approvers list...")
     sr_file = "X000_Approvers_prev_last"
     s_sql = "CREATE TABLE " + sr_file + " AS " + """
@@ -799,7 +876,7 @@ def Kfs_lists():
     so_conn.commit()
     funcfile.writelog("%t BUILD TABLE: " + sr_file)
 
-    # BUILD PAYMENTS
+    # BUILD PAYMENTS WITH LAST APPROVER
     print("Build previous year payments...")
     sr_file = "X001aa_Report_payments_prev"
     s_sql = "CREATE TABLE " + sr_file + " AS " + """
@@ -863,6 +940,84 @@ def Kfs_lists():
     so_curs.execute(s_sql)
     so_conn.commit()
     funcfile.writelog("%t BUILD TABLE: " + sr_file)    
+
+    # BUILD PREV APPROVALS WITH ALL APPROVERS
+    print("Build previous approvers list...")
+    sr_file = "X000_Approvers_prev"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select Distinct
+        ROUTE.ACTN_TKN_ID,
+        ROUTE.DOC_HDR_ID,
+        ROUTE.ACTN_DT,
+        ROUTE.PRNCPL_ID,
+        CASE
+            WHEN ROUTE.PRNCPL_ID = '26807815' THEN 'KFS WORKFLOW SYSTEM USER'
+            WHEN PERSON.NAME_ADDR IS NULL THEN 'UNKNOWN'
+            ELSE PERSON.NAME_ADDR
+        END AS NAME_ADDR,
+        ROUTE.ACTN_CD,
+        CASE
+            WHEN ROUTE.ACTN_CD = 'a' THEN 'SUPER USER APPROVED'
+            WHEN ROUTE.ACTN_CD = 'A' THEN 'APPROVED'
+            WHEN ROUTE.ACTN_CD = 'B' THEN 'BLANKET APPROVED'
+            WHEN ROUTE.ACTN_CD = 'r' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'R' THEN 'SUPER USER ROUTE LEVEL APPROVED'
+            WHEN ROUTE.ACTN_CD = 'v' THEN 'SUPER USER APPROVED'
+           ELSE 'OTHER'
+        END AS ACTN,
+        ROUTE.ANNOTN
+    From
+        KREW_ACTN_TKN_T_APP ROUTE Left Join
+        PEOPLE.X002_PEOPLE_PREV_YEAR PERSON On PERSON.EMPLOYEE_NUMBER = ROUTE.PRNCPL_ID
+    Order By
+        ROUTE.ACTN_DT,
+        ROUTE.ACTN_TKN_ID
+    """
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # BUILD PREV PAYMENTS FULL APPROVALS
+    print("Build previous payment full approvers list...")
+    sr_file = "X001ac_Report_payments_approute_prev"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select
+        PAYMENT.PMT_GRP_ID,
+        PAYMENT.VENDOR_ID,
+        PAYMENT.PAYEE_NAME,
+        PAYMENT.VENDOR_NAME,
+        PAYMENT.VENDOR_REG_NR,
+        PAYMENT.VENDOR_TAX_NR,
+        PAYMENT.VENDOR_BANK_NR,
+        PAYMENT.VENDOR_TYPE,
+        PAYMENT.PAYEE_TYP_DESC,
+        PAYMENT.DISB_NBR,
+        PAYMENT.DISB_TS,
+        PAYMENT.PMT_DT,
+        PAYMENT.PMT_STAT_CD,
+        PAYMENT.PAYMENT_STATUS,
+        PAYMENT.CUST_PMT_DOC_NBR,
+        PAYMENT.INV_NBR,
+        PAYMENT.REQS_NBR,
+        PAYMENT.PO_NBR,
+        PAYMENT.INV_DT,
+        PAYMENT.ORIG_INV_AMT,
+        PAYMENT.NET_PMT_AMT,
+        APPROVE.PRNCPL_ID AS APPROVE_EMP_NO,
+        APPROVE.NAME_ADDR AS APPROVE_EMP_NAME,
+        APPROVE.ACTN_DT AS APPROVE_DATE,
+        APPROVE.ACTN AS APPROVE_STATUS,
+        APPROVE.ANNOTN AS NOTE    
+    From
+        X001aa_Report_payments_prev PAYMENT Left Join
+        X000_Approvers_curr APPROVE On APPROVE.DOC_HDR_ID = PAYMENT.CUST_PMT_DOC_NBR
+    """
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
 
     """ ****************************************************************************
     END OF SCRIPT
