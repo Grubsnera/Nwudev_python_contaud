f2eaf1d3a49d277fb901878e52927681c650af17
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-05-11 06:46:24 +0200

    Query development

--- SQL Queries/Vss_deferment/Test.sql
+++ SQL Queries/Vss_deferment/Test.sql
@@ -1,17 +1,4 @@
 ﻿Select
-    X001ab_Students_deferment.DEFER_TYPE,
-    X001ab_Students_deferment.DEFER_TYPE_DESC,
-    X001ab_Students_deferment.FSITEORGUNITNUMBER,
-    Count(X001ab_Students_deferment.KSTUDBUSENTID) As Sum_KSTUDBUSENTID,
-    Sum(X001ab_Students_deferment.BAL_REG_CALC) As Sum_BAL_REG_CALC,
-    Sum(X001ab_Students_deferment.BAL_DEF_CALC) As Sum_BAL_DEF_CALC,
-    Sum(X001ab_Students_deferment.BAL_CUR) As Sum_BAL_CUR
+    SUMM.*
 From
-    X001ab_Students_deferment
-Group By
-    X001ab_Students_deferment.DEFER_TYPE,
-    X001ab_Students_deferment.DEFER_TYPE_DESC,
-    X001ab_Students_deferment.FSITEORGUNITNUMBER
-Order By
-    X001ab_Students_deferment.FSITEORGUNITNUMBER,
-    X001ab_Students_deferment.DEFER_TYPE
+    X001ac_Students_deferment_summ_curr SUMM
