cf01190885bf296aa3c39763ad92ad2fd6025afb
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-05-11 06:55:08 +0200

    Create C301_report_student_deferment_xdev.py
    Script to extract current and previous year deferments

---
+++ C301_report_student_deferment_xdev.py
@@ -0,0 +1,104 @@
+""" Script to extract STUDENT DEFERMENTS FOR THE CURRENT AND PREVIOUS YEAR *****
+Created on: 19 MAR 2018
+Author: Albert J v Rensburg (NWU21162395)
+*****************************************************************************"""
+
+""" INDEX **********************************************************************
+ENVIRONMENT
+OPEN THE DATABASES
+TEMPORARY AREA
+BEGIN OF SCRIPT
+END OF SCRIPT
+*****************************************************************************"""
+
+"""*****************************************************************************
+ENVIRONMENT
+*****************************************************************************"""
+
+# IMPORT PYTHON MODULES
+import sys
+
+# ADD OWN MODULE PATH
+sys.path.append('S:/_my_modules')
+
+# IMPORT PYTHON OBJECTS
+import csv
+import datetime
+import sqlite3
+
+# IMPORT OWN MODULES
+import funcdate
+import funccsv
+import funcfile
+
+# SCRIPT LOG FILE
+funcfile.writelog("Now")
+funcfile.writelog("SCRIPT: REPORT_VSS_DEFERMENTS")
+funcfile.writelog("-----------------------------")
+print("---------------------")    
+print("REPORT_VSS_DEFERMENTS")
+print("---------------------")
+ilog_severity = 1
+
+# DECLARE VARIABLES
+# s_period = "prev"
+# s_year = "2018"
+so_path = "W:/Vss_deferment/" #Source database path
+so_file = "Vss_deferment.sqlite" #Source database
+re_path = "R:/Vss/"
+ed_path = "S:/_external_data/"
+s_sql = "" #SQL statements
+l_export = True
+l_mail = False
+l_record = False
+l_vacuum = False
+
+"""*****************************************************************************
+OPEN THE DATABASES
+*****************************************************************************"""
+print("OPEN THE DATABASES")
+funcfile.writelog("OPEN THE DATABASES")
+
+# OPEN SQLITE SOURCE table
+print("Open sqlite database...")
+with sqlite3.connect(so_path+so_file) as so_conn:
+    so_curs = so_conn.cursor()
+funcfile.writelog("%t OPEN SQLITE DATABASE: VSS_DEFERMENT.SQLITE")
+
+# ATTACH VSS DATABASE
+print("Attach vss database...")
+so_curs.execute("ATTACH DATABASE 'W:/Vss/Vss.sqlite' AS 'VSS'")
+funcfile.writelog("%t ATTACH DATABASE: Vss.sqlite")
+
+""" ****************************************************************************
+TEMPORARY AREA
+*****************************************************************************"""
+print("TEMPORARY AREA")
+funcfile.writelog("TEMPORARY AREA")
+
+""" ****************************************************************************
+BEGIN OF SCRIPT
+*****************************************************************************"""
+print("BEGIN OF SCRIPT")
+funcfile.writelog("BEGIN OF SCRIPT")
+
+
+
+""" ****************************************************************************
+END OF SCRIPT
+*****************************************************************************"""
+print("END OF SCRIPT")
+funcfile.writelog("END OF SCRIPT")
+
+# CLOSE THE DATABASE CONNECTION
+if l_vacuum == True:
+    print("Vacuum the database...")
+    so_conn.commit()
+    so_conn.execute('VACUUM')
+    funcfile.writelog("%t DATABASE: Vacuum Vss_deferment")    
+so_conn.commit()
+so_conn.close()
+
+# CLOSE THE LOG WRITER *********************************************************
+funcfile.writelog("------------------------------------")
+funcfile.writelog("COMPLETED: REPORT_VSS_DEFERMENTS_DEV")
