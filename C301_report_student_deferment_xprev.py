90a76868bf762d853b0c1255574c88a424fdd3e5
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-05-11 06:54:22 +0200

    Create C301_report_student_deferment_xprev.py
    New script to extract previous year deferments

---
+++ C301_report_student_deferment_xprev.py
@@ -0,0 +1,28 @@
+
+import sys
+
+# OWN MODULE PATH
+sys.path.append('S:/_my_modules')
+sys.path.append('S:/')
+
+# IMPORT MODULES
+import funcdate
+import funcfile
+import funcsys
+
+# OPEN THE LOG WRITER
+funcfile.writelog("Now")
+funcfile.writelog("SCRIPT: C301_REPORT_STUDENT_DEFERMENT_PREV")
+funcfile.writelog("------------------------------------------")
+
+import C301_report_student_deferment
+try:
+    C301_report_student_deferment.Studdeb_deferments('prev',funcdate.prev_year())
+except Exception as e:
+    funcsys.ErrMessage(e)
+
+# CLOSE THE LOG WRITER
+funcfile.writelog("Now")
+funcfile.writelog("COMPLETED: C301_REPORT_STUDENT_DEFERMENT_PREV")
+funcfile.writelog("---------------------------------------------")
+
