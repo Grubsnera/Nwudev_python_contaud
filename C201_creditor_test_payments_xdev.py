68ff31f9b92f50cbc0980800f4a3a5351b6be68c
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-05-27 13:54:06 +0200

    Update C201_creditor_test_payments_xdev.py
    Test creditor vendor bank verifications.

--- C201_creditor_test_payments_xdev.py
+++ C201_creditor_test_payments_xdev.py
@@ -76,6 +76,371 @@ BEGIN OF SCRIPT
 print("BEGIN OF SCRIPT")
 funcfile.writelog("BEGIN OF SCRIPT")
 
+""" ****************************************************************************
+TEST CREDITOR BANK VERIFICATION
+*****************************************************************************"""
+print("TEST CREDITOR BANK VERIFICATION")
+funcfile.writelog("TEST CREDITOR BANK VERIFICATION")
+
+# DECLARE TEST VARIABLES
+#l_record = True # Record the findings in the previous reported findings file
+i_find = 0 # Number of findings before previous reported findings
+i_coun = 0 # Number of new findings to report
+
+# OBTAIN CURRENT BANK ACCOUNTS
+print("Build vendor banks...")
+sr_file = "X002_vendor_bank"
+s_sql = "CREATE TABLE " + sr_file + " AS " + """
+Select
+    VEND.VENDOR_ID,
+    VEND.VEND_BANK,
+    VEND.VEND_BRANCH
+From
+    KFS.X000_Vendor_master VEND
+Where
+    VEND.VEND_BANK <> ''
+"""
+so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+if funcdate.today_dayname() == "Mon":
+    s_sql = s_sql.replace('%DAYS%','-3 day')
+else:
+    s_sql = s_sql.replace('%DAYS%','-1 day')
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# GET PREVIOUS BANK ACCOUNTS
+sr_file = "X002_vendor_bank_prev"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+print("Import previous vendor banks...")
+so_curs.execute("CREATE TABLE " + sr_file + "(VENDOR_ID_PREV TEXT,VEND_BANK_PREV TEXT,VEND_BRANCH_PREV TEXT)")
+s_cols = ""
+co = open(ed_path + "201_vendor_bank.csv", "r")
+co_reader = csv.reader(co)
+# Read the COLUMN database data
+for row in co_reader:
+    # Populate the column variables
+    if row[0] == "VENDOR_ID":
+        continue
+    else:
+        s_cols = "INSERT INTO " + sr_file + " VALUES('" + row[0] + "','" + row[1] + "','" + row[2] + "')"
+        so_curs.execute(s_cols)
+so_conn.commit()
+# Close the impoted data file
+co.close()
+funcfile.writelog("%t IMPORT TABLE: " + ed_path + "201_vendor_bank.csv (" + sr_file + ")")
+
+# EXPORT THE PREVIOUS BANK DETAILS
+print("Export previous bank details...")
+sr_filet = "X002_vendor_bank_prev"
+sx_path = ed_path
+sx_file = "201_vendor_bank_prev"
+s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
+funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
+funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
+
+# EXPORT THE CURRENT BANK DETAILS
+print("Export current bank details...")
+sr_filet = "X002_vendor_bank"
+sx_path = ed_path
+sx_file = "201_vendor_bank"
+s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
+funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
+funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
+
+# COMBINE CURRENT AND PREVIOUS BANK ACCOUNTS
+print("Combine current and previous bank accounts...")
+sr_file = "X002aa_bank_change"
+s_sql = "CREATE TABLE " + sr_file + " AS " + """
+Select
+    CURR.VENDOR_ID,
+    CURR.VEND_BANK,
+    PREV.VEND_BANK_PREV,
+    PREV.VEND_BRANCH_PREV
+From
+    X002_vendor_bank CURR Left Join
+    X002_vendor_bank_prev PREV On PREV.VENDOR_ID_PREV = CURR.VENDOR_ID     
+"""
+so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# SELECT NEW AND BANK CHANGES
+print("Select new and bank changes...")
+sr_file = "X002ab_bank_change"
+s_sql = "CREATE TABLE " + sr_file + " AS " + """
+Select
+    VEND.VENDOR_ID,
+    VEND.VEND_BANK,
+    VEND.VEND_BANK_PREV,
+    VEND.VEND_BRANCH_PREV
+From
+    X002aa_bank_change VEND
+Where
+    VEND.VEND_BANK_PREV Is Null Or
+    VEND.VEND_BANK <> VEND.VEND_BANK_PREV
+"""
+so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# COUNT THE NUMBER OF FINDINGS
+i_find = funcsys.tablerowcount(so_curs,sr_file)
+print("*** Found "+str(i_find)+" exceptions ***")
+funcfile.writelog("%t FINDING: "+str(i_find)+" BANK change finding(s)")
+
+# GET PREVIOUS FINDINGS
+sr_file = "X002ac_getprev"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0:
+    print("Import previously reported findings...")
+    so_curs.execute("CREATE TABLE " + sr_file + "(PROCESS TEXT,FIELD1 INT,FIELD2 TEXT,FIELD3 TEXT,FIELD4 TEXT,FIELD5 TEXT,DATE_REPORTED TEXT,DATE_RETEST TEXT,DATE_MAILED TEXT)")
+    s_cols = ""
+    co = open(ed_path + "201_reported.txt", "r")
+    co_reader = csv.reader(co)
+    # Read the COLUMN database data
+    for row in co_reader:
+        # Populate the column variables
+        if row[0] == "PROCESS":
+            continue
+        elif row[0] != "vend_bank_change":
+            continue
+        else:
+            s_cols = "INSERT INTO " + sr_file + " VALUES('" + row[0] + "','" + row[1] + "','" + row[2] + "','" + row[3] + "','" + row[4] + "','" + row[5] + "','" + row[6] + "','" + row[7] + "','" + row[8] + "')"
+            so_curs.execute(s_cols)
+    so_conn.commit()
+    # Close the impoted data file
+    co.close()
+    funcfile.writelog("%t IMPORT TABLE: " + ed_path + "201_reported.txt (" + sr_file + ")")
+
+# ADD PREVIOUS FINDINGS
+sr_file = "X002ad_addprev"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0:
+    print("Join previously reported to current findings...")
+    s_sql = "CREATE TABLE " + sr_file + " AS" + """
+    SELECT
+      FIND.*,
+      'vend_bank_change' AS PROCESS,
+      '%TODAY%' AS DATE_REPORTED,
+      '%TODAYPLUS%' AS DATE_RETEST,
+      PREV.PROCESS AS PREV_PROCESS,
+      PREV.DATE_REPORTED AS PREV_DATE_REPORTED,
+      PREV.DATE_RETEST AS PREV_DATE_RETEST,
+      PREV.DATE_MAILED
+    FROM
+      X002ab_bank_change FIND
+      LEFT JOIN X002ac_getprev PREV ON PREV.FIELD1 = FIND.VENDOR_ID And
+          PREV.FIELD2 = FIND.VEND_BANK
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    s_sql = s_sql.replace("%TODAY%",funcdate.today())
+    s_sql = s_sql.replace("%TODAYPLUS%",funcdate.today_plusdays(0))
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# BUILD LIST TO UPDATE FINDINGS
+sr_file = "X002ae_newprev"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0:
+    s_sql = "CREATE TABLE "+sr_file+" AS " + """
+    SELECT
+      FIND.PROCESS,
+      FIND.VENDOR_ID AS FIELD1,
+      FIND.VEND_BANK AS FIELD2,
+      '' AS FIELD3,
+      '' AS FIELD4,
+      '' AS FIELD5,
+      FIND.DATE_REPORTED,
+      FIND.DATE_RETEST,
+      FIND.DATE_MAILED
+    FROM
+      X002ad_addprev FIND
+    WHERE
+      FIND.PREV_PROCESS IS NULL
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: "+sr_file)
+    # Export findings to previous reported file
+    i_coun = funcsys.tablerowcount(so_curs,sr_file)
+    if i_coun > 0:
+        print("*** " +str(i_coun)+ " Finding(s) to report ***")    
+        sr_filet = sr_file
+        sx_path = ed_path
+        sx_file = "201_reported"
+        # Read the header data
+        s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
+        # Write the data
+        if l_record == True:
+            funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head,"a",".txt")
+            funcfile.writelog("%t FINDING: "+str(i_coun)+" new finding(s) to export")        
+            funcfile.writelog("%t EXPORT DATA: "+sr_file)
+    else:
+        print("*** No new findings to report ***")
+        funcfile.writelog("%t FINDING: No new findings to export")
+
+# IMPORT OFFICERS FOR MAIL REPORTING PURPOSES
+sr_file = "X002af_officer"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0 and i_coun > 0:
+    print("Import reporting officers for mail purposes...")
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    SELECT
+      LOOKUP.LOOKUP,
+      LOOKUP.LOOKUP_CODE AS TYPE,
+      LOOKUP.LOOKUP_DESCRIPTION AS EMP,
+      PERSON.NAME_ADDR AS NAME,
+      PERSON.EMAIL_ADDRESS AS MAIL
+    FROM
+      PEOPLE.X000_OWN_HR_LOOKUPS LOOKUP
+      LEFT JOIN PEOPLE.X002_PEOPLE_CURR PERSON ON PERSON.EMPLOYEE_NUMBER = LOOKUP.LOOKUP_DESCRIPTION
+    WHERE
+      LOOKUP.LOOKUP = 'TEST_VENDOR_BANKACC_VERIFY_OFFICER'
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# IMPORT SUPERVISORS FOR MAIL REPORTING PURPOSES
+sr_file = "X002ag_supervisor"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0 and i_coun > 0:
+    print("Import reporting supervisors for mail purposes...")
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    SELECT
+      LOOKUP.LOOKUP,
+      LOOKUP.LOOKUP_CODE AS TYPE,
+      LOOKUP.LOOKUP_DESCRIPTION AS EMP,
+      PERSON.NAME_ADDR AS NAME,
+      PERSON.EMAIL_ADDRESS AS MAIL
+    FROM
+      PEOPLE.X000_OWN_HR_LOOKUPS LOOKUP
+      LEFT JOIN PEOPLE.X002_PEOPLE_CURR PERSON ON PERSON.EMPLOYEE_NUMBER = LOOKUP.LOOKUP_DESCRIPTION
+    WHERE
+      LOOKUP.LOOKUP = 'TEST_VENDOR_BANKACC_VERIFY_SUPERVISOR'
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# ADD CONTACT DETAILS TO FINDINGS
+sr_file = "X002ah_contact"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0 and i_coun > 0:
+    print("Add contact details to findings...")
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select
+        FIND.VENDOR_ID,
+        FIND.VEND_BANK,
+        VEND.VEND_BRANCH,
+        FIND.VEND_BANK_PREV,
+        FIND.VEND_BRANCH_PREV,
+        VEND.VNDR_NM,
+        CASE
+            WHEN VEND_MAIL = '' And EMAIL = '' Then ''
+            WHEN VEND_MAIL = '' And EMAIL <> '' Then EMAIL
+            ELSE VEND_MAIL
+        END As EMAIL1,
+        VEND.EMAIL,
+        CAMP_OFF.EMP As CAMP_OFF_NUMB,
+        CAMP_OFF.NAME As CAMP_OFF_NAME,
+        CAMP_OFF.MAIL As CAMP_OFF_MAIL,
+        CAMP_SUP.EMP As CAMP_SUP_NUMB,
+        CAMP_SUP.NAME As CAMP_SUP_NAME,
+        CAMP_SUP.MAIL As CAMP_SUP_MAIL,
+        ORG_OFF.EMP As ORG_OFF_NUMB,
+        ORG_OFF.NAME As ORG_OFF_NAME,
+        ORG_OFF.MAIL As ORG_OFF_MAIL,
+        ORG_SUP.EMP As ORG_SUP_NUMB,
+        ORG_SUP.NAME As ORG_SUP_NAME,
+        ORG_SUP.MAIL As ORG_SUP_MAIL
+    From
+        X002ad_addprev FIND
+        Left Join X002af_officer CAMP_OFF On CAMP_OFF.TYPE = 'VEN'
+        Left Join X002af_officer ORG_OFF On ORG_OFF.TYPE = 'NWU'
+        Left Join X002ag_supervisor CAMP_SUP On CAMP_SUP.TYPE = 'VEN'
+        Left Join X002ag_supervisor ORG_SUP On ORG_SUP.TYPE = 'NWU'
+        Left Join KFS.X000_Vendor_master VEND On VEND.VENDOR_ID = FIND.VENDOR_ID
+    Where
+        FIND.PREV_PROCESS IS NULL
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# BUILD THE FINAL TABLE FOR EXPORT AND REPORT
+sr_file = "X002ax_vendor_bank_change"
+so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+if i_find > 0 and i_coun > 0:
+    print("Build the final report")
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select
+        'NWU VENDOR BANK ACCOUNT VERIFY' As AUDIT_FINDING,
+        FIND.VENDOR_ID As NWU_VENDOR_ID,
+        FIND.VNDR_NM As NAME,
+        FIND.EMAIL1 As EMAIL1,
+        CASE
+            WHEN FIND.EMAIL1 <> '' And FIND.EMAIL <> '' And FIND.EMAIL1 <> FIND.EMAIL THEN EMAIL
+            ELSE ''
+        END As EMAIL2,
+        '' As CONTACT,
+        '' As TEL1,
+        '' As TEL2,
+        FIND.VEND_BRANCH As NEW_BRANCH_CODE,
+        FIND.VEND_BANK As NEW_BANK_ACC_NUMBER,
+        FIND.VEND_BRANCH_PREV As OLD_BRANCH_CODE,
+        FIND.VEND_BANK_PREV As OLD_BANK_ACC_NUMBER,
+        FIND.CAMP_OFF_NAME AS RESPONSIBLE_OFFICER,
+        FIND.CAMP_OFF_NUMB AS RESPONSIBLE_OFFICER_NUMB,
+        FIND.CAMP_OFF_MAIL AS RESPONSIBLE_OFFICER_MAIL,
+        FIND.CAMP_SUP_NAME AS SUPERVISOR,
+        FIND.CAMP_SUP_NUMB AS SUPERVISOR_NUMB,
+        FIND.CAMP_SUP_MAIL AS SUPERVISOR_MAIL,
+        FIND.ORG_OFF_NAME AS ORG_OFFICER,
+        FIND.ORG_OFF_NUMB AS ORG_OFFICER_NUMB,
+        FIND.ORG_OFF_MAIL AS ORG_OFFICER_MAIL,
+        FIND.ORG_SUP_NAME AS ORG_SUPERVISOR,
+        FIND.ORG_SUP_NUMB AS ORG_SUPERVISOR_NUMB,
+        FIND.ORG_SUP_MAIL AS ORG_SUPERVISOR_MAIL
+    From
+        X002ah_contact FIND
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+    # Export findings
+    if l_export == True and funcsys.tablerowcount(so_curs,sr_file) > 0:
+        print("Export findings...")
+        sr_filet = sr_file
+        sx_path = re_path + funcdate.cur_year() + "/"
+        sx_file = "Creditor_test_002ax_vendor_bank_verify_"
+        sx_filet = sx_file + funcdate.today_file()
+        s_head = funccsv.get_colnames_sqlite(so_conn, sr_filet)
+        funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
+        funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
+        funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
+else:
+    s_sql = "CREATE TABLE " + sr_file + " (" + """
+    BLANK TEXT
+    );"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)    
+
+
+
+
+
 
 
 """ ****************************************************************************
