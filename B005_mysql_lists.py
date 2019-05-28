98627b11d1edaf91f70c6d6f0bc85bb49ec4ef44
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-05-28 13:41:09 +0200

    Update B005_mysql_lists.py
    Moved export to mysql to another script.

--- B005_mysql_lists.py
+++ B005_mysql_lists.py
@@ -10,6 +10,9 @@ BEGIN OF SCRIPT
 OPEN THE PEOPLE DATABASES
 EXPORT CURRENT PEOPLE
 EXPORT CURRENT PEOPLE STRUCTURE
+OPEN THE KFS VSS STUDDEB DATABASES
+EXPORT STUD DEBTOR MONTHLY BALANCES
+
 END OF SCRIPT
 *****************************************************************************"""
 
@@ -225,7 +228,7 @@ def Mysql_lists():
     ms_cnxn.commit()
     rs_conn.close()
     print("Inserted " + str(i_tota) + " mysql current people...")
-    funcfile.writelog("%t POPULATE MYSQL: " + str(i_tota) + " People records (ia_people)")
+    funcfile.writelog("%t POPULATE MYSQL: " + str(i_tota) + " PEOPLE CURRENT rows (ia_people)")
 
     # Update MYSQL PEOPLE TO WEB FINDING mail trigger ******************************
     print("Update mysql current people mail trigger...")
@@ -362,7 +365,7 @@ def Mysql_lists():
     ms_cnxn.commit()
     rs_conn.close()
     print("Inserted " + str(i_tota) + " mysql current people...")
-    funcfile.writelog("%t POPULATE MYSQL: " + str(i_tota) + " PEOPLE_STRUCT records (ia_people_struct)")
+    funcfile.writelog("%t POPULATE MYSQL: " + str(i_tota) + " PEOPLE STRUCTURE rows (ia_people_struct)")
 
     # Update MYSQL PEOPLE TO WEB FINDING mail trigger ******************************
     print("Update mysql current people hierarchy mail trigger...")
@@ -376,6 +379,186 @@ def Mysql_lists():
     ms_curs.execute(s_sql)
     ms_cnxn.commit()
     funcfile.writelog("%t UPDATED MYSQL TRIGGER: FINDING 4 (people current hierarchy)")
+
+    # CLOSE PEOPLE DATABASE
+    so_conn.commit()
+    so_conn.close()
+
+    """*****************************************************************************
+    OPEN THE KFS VSS STUDDEB DATABASES
+    *****************************************************************************"""
+    print("OPEN THE PEOPLE DATABASES")
+    funcfile.writelog("OPEN THE PEOPLE DATABASES")
+
+    # Open the SQLITE SOURCE file
+    so_path = "W:/Kfs_vss_studdeb/" #Source database path
+    so_file = "Kfs_vss_studdeb.sqlite" #Source database
+    with sqlite3.connect(so_path+so_file) as so_conn:
+        so_curs = so_conn.cursor()
+    funcfile.writelog("%t OPEN SQLITE DATABASE: KFS_VSS_STUDDEB.SQLITE")    
+
+    """*****************************************************************************
+    EXPORT STUD DEBTOR MONTHLY BALANCES
+    *****************************************************************************"""
+    print("EXPORT STUD DEBTOR MONTHLY BALANCES")
+    funcfile.writelog("EXPORT STUD DEBTOR MONTHLY BALANCES")
+
+    # Create MYSQL VSS GL MONTHLY BALANCES TO WEB table ****************************
+    print("Build mysql vss gl monthly balances...")
+    ms_curs.execute("DROP TABLE IF EXISTS ia_finding_5")
+    funcfile.writelog("%t DROPPED MYSQL TABLE: ia_finding_5")
+    s_sql = """
+    CREATE TABLE IF NOT EXISTS ia_finding_5 (
+    ia_find_auto INT(11),
+    ia_find5_auto INT(11) AUTO_INCREMENT,
+    ia_find5_campus VARCHAR(20),
+    ia_find5_month VARCHAR(2),
+    ia_find5_current VARCHAR(1),
+    ia_find5_vss_tran_dt DECIMAL(20,2),
+    ia_find5_vss_tran_ct DECIMAL(20,2),
+    ia_find5_vss_tran DECIMAL(20,2),
+    ia_find5_vss_runbal DECIMAL(20,2),
+    ia_find5_gl_tran DECIMAL(20,2),
+    ia_find5_gl_runbal DECIMAL(20,2),
+    ia_find5_diff DECIMAL(20,2),
+    ia_find5_move DECIMAL(20,2),
+    ia_find5_officer_camp VARCHAR(10),
+    ia_find5_officer_name_camp VARCHAR(50),
+    ia_find5_officer_mail_camp VARCHAR(100),
+    ia_find5_officer_org VARCHAR(10),
+    ia_find5_officer_name_org VARCHAR(50),
+    ia_find5_officer_mail_org VARCHAR(100),
+    ia_find5_supervisor_camp VARCHAR(10),
+    ia_find5_supervisor_name_camp VARCHAR(50),
+    ia_find5_supervisor_mail_camp VARCHAR(100),
+    ia_find5_supervisor_org VARCHAR(10),
+    ia_find5_supervisor_name_org VARCHAR(50),
+    ia_find5_supervisor_mail_org VARCHAR(100),
+    PRIMARY KEY (ia_find5_auto),
+    INDEX fb_order_ia_find5_campus_INDEX (ia_find5_campus),
+    INDEX fb_order_ia_find5_month_INDEX (ia_find5_month)
+    )
+    ENGINE = InnoDB
+    CHARSET=utf8mb4
+    COLLATE utf8mb4_unicode_ci
+    COMMENT = 'Table to store vss and gl monthly balances'
+    """ + ";"
+    ms_curs.execute(s_sql)
+    funcfile.writelog("%t CREATED MYSQL TABLE: ia_finding_5 (vss gl monthly balances per campus per month)")
+    # Open the SOURCE file to obtain column headings
+    print("Build mysql vss gl monthly balance columns...")
+    funcfile.writelog("%t OPEN DATABASE: ia_finding_5")
+    s_head = funcmysql.get_colnames_sqlite_text(so_curs,"X002ex_vss_gl_balance_month","ia_find5_")
+    s_head = "(`ia_find_auto`, " + s_head.rstrip(", ") + ")"
+    #print(s_head)
+    # Open the SOURCE file to obtain the data
+    print("Insert mysql vss gl monthly balance data...")
+    with sqlite3.connect(so_path+so_file) as rs_conn:
+        rs_conn.row_factory = sqlite3.Row
+    rs_curs = rs_conn.cursor()
+    rs_curs.execute("SELECT * FROM X002ex_vss_gl_balance_month")
+    rows = rs_curs.fetchall()
+    i_tota = 0
+    i_coun = 0
+    for row in rows:
+        s_data = "(5, "
+        for member in row:
+            #print(type(member))
+            if type(member) == str:
+                s_data = s_data + "'" + member + "', "
+            elif type(member) == int:
+                s_data = s_data + str(member) + ", "
+            elif type(member) == float:
+                s_data = s_data + str(member) + ", "
+            else:
+                s_data = s_data + "'', "
+        s_data = s_data.rstrip(", ") + ")"
+        #print(s_data)
+        s_sql = "INSERT INTO `ia_finding_5` " + s_head + " VALUES " + s_data + ";"
+        ms_curs.execute(s_sql)
+        i_tota = i_tota + 1
+        i_coun = i_coun + 1
+        if i_coun == 100:
+            ms_cnxn.commit()
+            i_coun = 0
+    ms_cnxn.commit()
+    print("Inserted " + str(i_tota) + " rows...")
+    funcfile.writelog("%t POPULATE MYSQL: " + str(i_tota) + " STUD DEBT MONTHLY BAL rows (ia_finding_5)")
+
+    """*****************************************************************************
+    EXPORT STUD DEBTOR COMPARISON CAMPUS MONTH SUMMARY
+    *****************************************************************************"""
+    print("EXPORT STUD DEBTOR COMPARISON CAMPUS MONTH SUMMARY")
+    funcfile.writelog("EXPORT STUD DEBTOR COMPARISON CAMPUS MONTH SUMMARY")
+
+    # EXPORT MYSQL VSS GL COMPARISON PER CAMPUS PER MONTH TO WEB
+    print("Build mysql vss gl comparison campus month...")
+    ms_curs.execute("DROP TABLE IF EXISTS ia_finding_6")
+    funcfile.writelog("%t DROPPED MYSQL TABLE: ia_finding_6")
+    s_sql = """
+    CREATE TABLE IF NOT EXISTS ia_finding_6 (
+    ia_find_auto INT(11),
+    ia_find6_auto INT(11) AUTO_INCREMENT,
+    ia_find6_campus VARCHAR(20),
+    ia_find6_month VARCHAR(2),
+    ia_find6_trancode VARCHAR(5),
+    ia_find6_vss_description VARCHAR(150),
+    ia_find6_vss_amount DECIMAL(20,2),
+    ia_find6_gl_description VARCHAR(150),
+    ia_find6_gl_amount DECIMAL(20,2),
+    ia_find6_diff DECIMAL(20,2),
+    ia_find6_matched VARCHAR(2),
+    ia_find6_period VARCHAR(7),
+    PRIMARY KEY (ia_find6_auto),
+    INDEX fb_order_ia_find6_campus_INDEX (ia_find6_campus),
+    INDEX fb_order_ia_find6_month_INDEX (ia_find6_month)
+    )
+    ENGINE = InnoDB
+    CHARSET=utf8mb4
+    COLLATE utf8mb4_unicode_ci
+    COMMENT = 'Table to store vss and gl monthly comparisons'
+    """ + ";"
+    ms_curs.execute(s_sql)
+    funcfile.writelog("%t CREATED MYSQL TABLE: ia_finding_6 (vss gl comparison per campus per month)")
+    # Open the SOURCE file to obtain column headings
+    print("Build mysql vss gl comparison columns...")
+    funcfile.writelog("%t OPEN DATABASE: ia_finding_6")
+    s_head = funcmysql.get_colnames_sqlite_text(so_curs,"X003ax_vss_gl_join","ia_find6_")
+    s_head = "(ia_find_auto, " + s_head.rstrip(", ") + ")"
+    #print(s_head)
+    # Open the SOURCE file to obtain the data
+    print("Insert mysql vss gl comparison data...")
+    with sqlite3.connect(so_path+so_file) as rs_conn:
+        rs_conn.row_factory = sqlite3.Row
+    rs_curs = rs_conn.cursor()
+    rs_curs.execute("SELECT * FROM X003ax_vss_gl_join")
+    rows = rs_curs.fetchall()
+    i_tota = 0
+    i_coun = 0
+    for row in rows:
+        s_data = "(6, "
+        for member in row:
+            #print(type(member))
+            if type(member) == str:
+                s_data = s_data + "'" + member + "', "
+            elif type(member) == int:
+                s_data = s_data + str(member) + ", "
+            elif type(member) == float:
+                s_data = s_data + str(member) + ", "
+            else:
+                s_data = s_data + "'', "
+        s_data = s_data.rstrip(", ") + ")"
+        #print(s_data)
+        s_sql = "INSERT INTO `ia_finding_6` " + s_head + " VALUES " + s_data + ";"
+        ms_curs.execute(s_sql)
+        i_tota = i_tota + 1
+        i_coun = i_coun + 1
+        if i_coun == 100:
+            ms_cnxn.commit()
+            i_coun = 0
+    ms_cnxn.commit()
+    print("Inserted " + str(i_tota) + " rows...")
+    funcfile.writelog("%t POPULATE MYSQL: " + str(i_tota) + " STUD DEBT COMPARISON rows (ia_finding_6)")
 
     # CLOSE PEOPLE DATABASE
     so_conn.commit()
