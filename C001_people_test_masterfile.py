9e16ff5f639048135b66fcb6ab037d45eedb9898
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-06-07 16:06:50 +0200

    Add people test employee work permit expired.

--- C001_people_test_masterfile.py
+++ C001_people_test_masterfile.py
@@ -35,6 +35,7 @@ TEST ADDRESS DUPLICATE (In development)
 PASSPORT NUMBER MASTER FILE
 TEST PASSPORT NUMBER BLANK *
 TEST PASSPORT NUMBER DUPLICATE
+TEST WORK PERMIT EXPIRED
 
 BANK NUMBER MASTER FILE
 TEST BANK NUMBER DUPLICATE
@@ -1696,17 +1697,19 @@ def People_test_masterfile():
     s_sql = "CREATE TABLE "+sr_file+" AS " + """
     Select
         'NWU' AS ORG,
-        CASE LOCATION_DESCRIPTION
+        CASE MASTER.LOCATION_DESCRIPTION
             WHEN 'MAFIKENG CAMPUS' THEN 'MAF'
             WHEN 'POTCHEFSTROOM CAMPUS' THEN 'POT'
             WHEN 'VAAL TRIANGLE CAMPUS' THEN 'VAA'
             ELSE 'NWU'
         END AS LOC,
-        PEOPLE.X002_PEOPLE_CURR.EMPLOYEE_NUMBER AS EMP,
-        PEOPLE.X002_PEOPLE_CURR.PASSPORT AS NUMB,
-        PEOPLE.X002_PEOPLE_CURR.NATIONALITY AS NAT
+        MASTER.EMPLOYEE_NUMBER AS EMP,
+        MASTER.PASSPORT AS NUMB,
+        MASTER.PERMIT,
+        MASTER.PERMIT_EXPIRE,
+        MASTER.NATIONALITY AS NAT
     From
-        PEOPLE.X002_PEOPLE_CURR
+        PEOPLE.X002_PEOPLE_CURR MASTER
     ;"""
     so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
     so_curs.execute(s_sql)
@@ -2276,6 +2279,313 @@ def People_test_masterfile():
             funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_file, s_head)
             funccsv.write_data(so_conn, "main", sr_filet, sx_path, sx_filet, s_head)
             funcfile.writelog("%t EXPORT DATA: "+sx_path+sx_file)
+    else:
+        s_sql = "CREATE TABLE " + sr_file + " (" + """
+        BLANK TEXT
+        );"""
+        so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+        so_curs.execute(s_sql)
+        so_conn.commit()
+        funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    """ ****************************************************************************
+    TEST WORK PERMIT EXPIRED
+    *****************************************************************************"""
+    print("WORK PERMIT EXPIRED")
+    funcfile.writelog("WORK PERMIT EXPIRED")
+
+    # DECLARE TEST VARIABLES
+    i_finding_after: int = 0
+
+    # OBTAIN TEST DATA
+    print("Obtain test data and add employee details...")
+    sr_file: str = "X003ea_permit_expire"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select
+        MASTER.ORG,
+        MASTER.LOC,
+        MASTER.EMP,
+        MASTER.NUMB,
+        MASTER.PERMIT,
+        Substr(Replace(MASTER.PERMIT_EXPIRE,'/','-'),1,10) As EXPIRE_DATE
+    From
+        X003_pass_master MASTER
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # IDENTIFY FINDINGS
+    print("Identify findings...")
+    sr_file = "X003eb_findings"
+    s_sql = "CREATE TABLE " + sr_file + " AS " + """
+    Select
+        CURR.ORG,
+        CURR.LOC,
+        CURR.EMP,
+        CURR.NUMB,
+        CURR.PERMIT,
+        CURR.EXPIRE_DATE
+    From
+        X003ea_permit_expire CURR
+    Where
+        CURR.PERMIT <> '' And
+        CURR.EXPIRE_DATE < Date('%TODAY%')
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    s_sql = s_sql.replace("%TODAY%", funcdate.today())
+    so_curs.execute(s_sql)
+    so_conn.commit()
+    funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # COUNT THE NUMBER OF FINDINGS
+    i_finding_before: int = funcsys.tablerowcount(so_curs, sr_file)
+    print("*** Found " + str(i_finding_before) + " exceptions ***")
+    funcfile.writelog("%t FINDING: " + str(i_finding_before) + " EMPL WORK PERMIT EXPIRED finding(s)")
+
+    # GET PREVIOUS FINDINGS
+    sr_file = "X003ec_get_previous"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    if i_finding_before > 0:
+        print("Import previously reported findings...")
+        so_curs.execute(
+            "CREATE TABLE " + sr_file + """
+            (PROCESS TEXT,
+            FIELD1 INT,
+            FIELD2 TEXT,
+            FIELD3 TEXT,
+            FIELD4 TEXT,
+            FIELD5 TEXT,
+            DATE_REPORTED TEXT,
+            DATE_RETEST TEXT,
+            DATE_MAILED TEXT)
+            """)
+        s_cols = ""
+        co = open(ed_path + "001_reported.txt", "r")
+        co_reader = csv.reader(co)
+        # Read the COLUMN database data
+        for row in co_reader:
+            # Populate the column variables
+            if row[0] == "PROCESS":
+                continue
+            elif row[0] != "work_permit_expire":
+                continue
+            else:
+                s_cols = "INSERT INTO " + sr_file + " VALUES('" + row[0] + "','" + row[1] + "','" + row[2] + "','" + \
+                         row[
+                             3] + "','" + row[4] + "','" + row[5] + "','" + row[6] + "','" + row[7] + "','" + row[
+                             8] + "')"
+                so_curs.execute(s_cols)
+        so_conn.commit()
+        # Close the imported data file
+        co.close()
+        funcfile.writelog("%t IMPORT TABLE: " + ed_path + "001_reported.txt (" + sr_file + ")")
+
+    # ADD PREVIOUS FINDINGS
+    sr_file = "X003ed_add_previous"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    if i_finding_before > 0:
+        print("Join previously reported to current findings...")
+        s_sql = "CREATE TABLE " + sr_file + " AS" + """
+        Select
+            FIND.*,
+            'work_permit_expire' AS PROCESS,
+            '%TODAY%' AS DATE_REPORTED,
+            '%DAYS%' AS DATE_RETEST,
+            PREV.PROCESS AS PREV_PROCESS,
+            PREV.DATE_REPORTED AS PREV_DATE_REPORTED,
+            PREV.DATE_RETEST AS PREV_DATE_RETEST,
+            PREV.DATE_MAILED
+        From
+            X003eb_findings FIND Left Join
+            X003ec_get_previous PREV ON PREV.FIELD1 = FIND.EMP AND
+                PREV.DATE_RETEST >= Date('%TODAY%')
+        ;"""
+        so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+        s_sql = s_sql.replace("%TODAY%", funcdate.today())
+        s_sql = s_sql.replace("%DAYS%", funcdate.today_plusdays(10))
+        so_curs.execute(s_sql)
+        so_conn.commit()
+        funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # BUILD LIST TO UPDATE FINDINGS
+    # NOTE ADD CODE
+    sr_file = "X003ee_new_previous"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    if i_finding_before > 0:
+        s_sql = "CREATE TABLE " + sr_file + " AS " + """
+        Select
+            PREV.PROCESS,
+            PREV.EMP AS FIELD1,
+            '' AS FIELD2,
+            '' AS FIELD3,
+            '' AS FIELD4,
+            '' AS FIELD5,
+            PREV.DATE_REPORTED,
+            PREV.DATE_RETEST,
+            PREV.DATE_MAILED
+        From
+            X003ed_add_previous PREV
+        Where
+            PREV.PREV_PROCESS Is Null
+        ;"""
+        so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+        so_curs.execute(s_sql)
+        so_conn.commit()
+        funcfile.writelog("%t BUILD TABLE: " + sr_file)
+        # Export findings to previous reported file
+        i_finding_after = funcsys.tablerowcount(so_curs, sr_file)
+        if i_finding_after > 0:
+            print("*** " + str(i_finding_after) + " Finding(s) to report ***")
+            sx_path = ed_path
+            sx_file = "001_reported"
+            # Read the header data
+            s_head = funccsv.get_colnames_sqlite(so_conn, sr_file)
+            # Write the data
+            if l_record:
+                funccsv.write_data(so_conn, "main", sr_file, sx_path, sx_file, s_head, "a", ".txt")
+                funcfile.writelog("%t FINDING: " + str(i_finding_after) + " new finding(s) to export")
+                funcfile.writelog("%t EXPORT DATA: " + sr_file)
+        else:
+            print("*** No new findings to report ***")
+            funcfile.writelog("%t FINDING: No new findings to export")
+
+    # IMPORT OFFICERS FOR MAIL REPORTING PURPOSES
+    sr_file = "X003ef_officer"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    if i_finding_before > 0:
+        if i_finding_after > 0:
+            print("Import reporting officers for mail purposes...")
+            s_sql = "CREATE TABLE " + sr_file + " AS " + """
+            Select
+                OFFICER.LOOKUP,
+                OFFICER.LOOKUP_CODE AS CAMPUS,
+                OFFICER.LOOKUP_DESCRIPTION AS EMPLOYEE_NUMBER,
+                PEOP.NAME_ADDR As NAME,
+                PEOP.EMAIL_ADDRESS
+            From
+                PEOPLE.X000_OWN_HR_LOOKUPS OFFICER Left Join
+                PEOPLE.X002_PEOPLE_CURR PEOP ON
+                    PEOP.EMPLOYEE_NUMBER = OFFICER.LOOKUP_DESCRIPTION
+            Where
+                OFFICER.LOOKUP = 'TEST_WORK_PERMIT_EXPIRE_OFFICER'
+            ;"""
+            so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+            so_curs.execute(s_sql)
+            so_conn.commit()
+            funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # IMPORT SUPERVISORS FOR MAIL REPORTING PURPOSES
+    sr_file = "X003eg_supervisor"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    if i_finding_before > 0 and i_finding_after > 0:
+        print("Import reporting supervisors for mail purposes...")
+        s_sql = "CREATE TABLE " + sr_file + " AS " + """
+        Select
+            SUPERVISOR.LOOKUP,
+            SUPERVISOR.LOOKUP_CODE AS CAMPUS,
+            SUPERVISOR.LOOKUP_DESCRIPTION AS EMPLOYEE_NUMBER,
+            PEOP.NAME_ADDR As NAME,
+            PEOP.EMAIL_ADDRESS
+        From
+            PEOPLE.X000_OWN_HR_LOOKUPS SUPERVISOR Left Join
+            PEOPLE.X002_PEOPLE_CURR PEOP ON 
+                PEOP.EMPLOYEE_NUMBER = SUPERVISOR.LOOKUP_DESCRIPTION
+        Where
+            SUPERVISOR.LOOKUP = 'TEST_WORK_PERMIT_EXPIRE_SUPERVISOR'
+        ;"""
+        so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+        so_curs.execute(s_sql)
+        so_conn.commit()
+        funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # ADD CONTACT DETAILS TO FINDINGS
+    sr_file = "X003eh_detail"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    if i_finding_before > 0 and i_finding_after > 0:
+        print("Add contact details to findings...")
+        s_sql = "CREATE TABLE " + sr_file + " AS " + """
+        Select
+            PREV.ORG,
+            PREV.LOC,
+            PREV.EMP,
+            PEOP.NAME_LIST,
+            PREV.NUMB,
+            PEOP.NATIONALITY_NAME,
+            PREV.PERMIT,
+            PREV.EXPIRE_DATE,
+            CAMP_OFF.EMPLOYEE_NUMBER As CAMP_OFF_NUMB,
+            CAMP_OFF.NAME As CAMP_OFF_NAME,
+            CAMP_OFF.EMAIL_ADDRESS As CAMP_OFF_MAIL,
+            CAMP_SUP.EMPLOYEE_NUMBER As CAMP_SUP_NUMB,
+            CAMP_SUP.NAME As CAMP_SUP_NAME,
+            CAMP_SUP.EMAIL_ADDRESS As CAMP_SUP_MAIL,
+            ORG_OFF.EMPLOYEE_NUMBER As ORG_OFF_NUMB,
+            ORG_OFF.NAME As ORG_OFF_NAME,
+            ORG_OFF.EMAIL_ADDRESS As ORG_OFF_MAIL,
+            ORG_SUP.EMPLOYEE_NUMBER As ORG_SUP_NUMB,
+            ORG_SUP.NAME As ORG_SUP_NAME,
+            ORG_SUP.EMAIL_ADDRESS As ORG_SUP_MAIL
+        From
+            X003ed_add_previous PREV
+            Left Join PEOPLE.X002_PEOPLE_CURR PEOP On PEOP.EMPLOYEE_NUMBER = PREV.EMP
+            Left Join X003ef_officer CAMP_OFF On CAMP_OFF.CAMPUS = PREV.LOC
+            Left Join X003ef_officer ORG_OFF On ORG_OFF.CAMPUS = PREV.ORG
+            Left Join X003eg_supervisor CAMP_SUP On CAMP_SUP.CAMPUS = PREV.LOC
+            Left Join X003eg_supervisor ORG_SUP On ORG_SUP.CAMPUS = PREV.ORG
+        Where
+          PREV.PREV_PROCESS IS NULL
+        ;"""
+        so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+        so_curs.execute(s_sql)
+        so_conn.commit()
+        funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+    # BUILD THE FINAL TABLE FOR EXPORT AND REPORT
+    sr_file = "X003ex_work_permit_expire"
+    so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+    print("Build the final report")
+    if i_finding_before > 0 and i_finding_after > 0:
+        s_sql = "CREATE TABLE " + sr_file + " AS " + """
+        Select
+            'EMPLOYEE WORK PERMIT EXPIRED' As Audit_finding,
+            FIND.EMP AS Employee,
+            FIND.NAME_LIST As Name,
+            FIND.NUMB As Passport,
+            FIND.NATIONALITY_NAME As Nationality,
+            FIND.PERMIT As Work_permit,
+            FIND.EXPIRE_DATE As Permit_expire_date,
+            FIND.CAMP_OFF_NAME AS Responsible_Officer,
+            FIND.CAMP_OFF_NUMB AS Responsible_Officer_Numb,
+            FIND.CAMP_OFF_MAIL AS Responsible_Officer_Mail,
+            FIND.CAMP_SUP_NAME AS Supervisor,
+            FIND.CAMP_SUP_NUMB AS Supervisor_Numb,
+            FIND.CAMP_SUP_MAIL AS Supervisor_Mail,
+            FIND.ORG_OFF_NAME AS Org_Officer,
+            FIND.ORG_OFF_NUMB AS Org_Officer_Numb,
+            FIND.ORG_OFF_MAIL AS Org_Officer_Mail,
+            FIND.ORG_SUP_NAME AS Org_Supervisor,
+            FIND.ORG_SUP_NUMB AS Org_Supervisor_Numb,
+            FIND.ORG_SUP_MAIL AS Org_Supervisor_Mail            
+
+        From
+            X003eh_detail FIND
+        ;"""
+        so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+        so_curs.execute(s_sql)
+        so_conn.commit()
+        funcfile.writelog("%t BUILD TABLE: " + sr_file)
+        # Export findings
+        if l_export and funcsys.tablerowcount(so_curs, sr_file) > 0:
+            print("Export findings...")
+            sx_path = re_path + funcdate.cur_year() + "/"
+            sx_file = "People_test_003ex_work_permit_expire_"
+            sx_file_dated = sx_file + funcdate.today_file()
+            s_head = funccsv.get_colnames_sqlite(so_conn, sr_file)
+            funccsv.write_data(so_conn, "main", sr_file, sx_path, sx_file, s_head)
+            funccsv.write_data(so_conn, "main", sr_file, sx_path, sx_file_dated, s_head)
+            funcfile.writelog("%t EXPORT DATA: " + sx_path + sx_file)
     else:
         s_sql = "CREATE TABLE " + sr_file + " (" + """
         BLANK TEXT
