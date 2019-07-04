77e98982fa372726cbb3921971784604dcb0435a
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-07-04 13:12:24 +0200

    Script to run the gl test transactions.

--- C202_gl_test_transactions_run.py
+++ C202_gl_test_transactions_run.py
@@ -1,13 +1,17 @@
 """
-Script to RUN GL TRANSACTION tests
+Script to RUN the GL TRANSACTION TESTS
 Created: 2 Jul 2019
-Author: Albert J van Rensburg (NWU21162395)
+Author: Albert B Janse van Rensburg
 """
 
 # IMPORT OWN MODULES
 from _my_modules import funcfile
+from _my_modules import funcmail
 from _my_modules import funcsys
 import C202_gl_test_transactions
+
+# DECLARE VARIABLES
+l_mail = False
 
 # OPEN THE LOG WRITER
 funcfile.writelog("Now")
@@ -16,11 +20,12 @@ funcfile.writelog("-------------------------------------")
 
 try:
     C202_gl_test_transactions.gl_test_transactions()
+    if l_mail:
+        funcmail.Mail('std_success_gmail', 'NWUIAPython:Success:C202_GL_TEST_TRANSACTIONS', 'NWUIAPython: Success: C202_GL_TEST_TRANSACTIONS')
 except Exception as e:
-    funcsys.ErrMessage(e)
+    funcsys.ErrMessage(e, l_mail, 'NWUIAPython:Fail:C202_GL_TEST_TRANSACTIONS', 'NWUIAPython: Fail: C202_GL_TEST_TRANSACTIONS')
 
 # CLOSE THE LOG WRITER
 funcfile.writelog("Now")
 funcfile.writelog("COMPLETED: C202_GL_TEST_TRANSACTIONS_RUN")
 funcfile.writelog("----------------------------------------")
-
