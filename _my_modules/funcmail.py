4a81e1340e635b3413bc864d5b9f4ff593869c45
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-07-04 09:41:47 +0200

    PEP8.

--- _my_modules/funcmail.py
+++ _my_modules/funcmail.py
@@ -1,38 +1,31 @@
-""" FUNCMAIL Script to handle mail sending
-Copyright (c) Albert J van Rensburg
-Develope: 30 May 2018
+"""
+Script to prepare and send email
+Develop: 30 May 2018
 """
 
-""" Summary of functions *******************************************************
-def Mail Function to read mail parameters from 000m_Mail.csv
-         Prerequisite: 000m_Mail.csv
-def Send Function to do the actual mail sending
-         Currently sending via nwu.internal.audit@gmail.com
-*****************************************************************************"""
+# IMPORT PYTHON OBJECTS
+import csv
 
-def Mail(s_trigger):
+# IMPORT OWN OBJECTS
+from _my_modules import funcfile
+from _my_modules import funcdate
+from _my_modules import funcstr
 
-    """ Parameters
-    s_trigger = Name of the record flag or trigger in 000m_Mail.csv
+
+def Mail(s_trigger, s_subject='', s_body=''):
+    """
+    Function to PREPARE email using parameters from 000m_Mail.csv
+    :rtype: str
+    :param s_trigger: Mail function trigger
+    :param s_subject: Mail subject
+    :param s_body: Mail body
+    :return: Text message to indicate successful mail send
     """
 
-    # Import python objects
-    import sys
-    import csv
-
-    # Add own module path
-    sys.path.append('S:/_my_modules')
-    
-    # Import own modules
-    import funcfile # File functions
-    import funcdate # Date functions
-    import funcmail # Mailing functions
-    import funcstr  # String functions
-
-    # Open the script log file
+    # OPEN THE SCRIPT LOG FILE
     funcfile.writelog("%t DEFINITION: Send mail " + s_trigger)
 
-    # Declare the global variables ************************************************
+    # DECLARE VARIABLES
     sl_path = "S:/_external_data/"
 
     print("Send email " + s_trigger)
@@ -42,18 +35,9 @@ def Mail(s_trigger):
     co_reader = csv.reader(co)
 
     # Read the COLUMN database data
-
     for row in co_reader:
 
         # Populate the local variables
-        s_result = ""
-        to_name = ""
-        to_address = ""
-        mail_language = ""
-        mail_subject = ""
-        mail_body = ""
-        file_path = ""
-        file_name = ""
         send_mail = False
 
         # Populate the column variables
@@ -69,29 +53,35 @@ def Mail(s_trigger):
         else:
             send_mail = True
 
-        if send_mail == True:    
+        if send_mail:
 
             # Build the mail parameters from the 000m_Mail.csv file
             to_name = row[2]
             to_address = row[3]
             mail_language = row[4]
-            mail_subject = row[5]
-            mail_body = row[6]
+            if s_subject == '':
+                mail_subject = row[5]
+            else:
+                mail_subject = s_subject
+            if s_body == '':
+                mail_body = row[6]
+            else:
+                mail_body = s_body
             file_path = row[7]
-            file_path = file_path.replace("%PYEAR%",funcdate.prev_year())
-            file_path = file_path.replace("%PMONTH%",funcdate.prev_month())
-            file_path = file_path.replace("%CYEAR%",funcdate.cur_year())
-            file_path = file_path.replace("%CMONTH%",funcdate.cur_month())
-            file_path = file_path.replace("%TODAY%",funcdate.today_file())
+            file_path = file_path.replace("%PYEAR%", funcdate.prev_year())
+            file_path = file_path.replace("%PMONTH%", funcdate.prev_month())
+            file_path = file_path.replace("%CYEAR%", funcdate.cur_year())
+            file_path = file_path.replace("%CMONTH%", funcdate.cur_month())
+            file_path = file_path.replace("%TODAY%", funcdate.today_file())
             file_name = row[8]
-            file_name = file_name.replace("%PYEAR%",funcdate.prev_year())
-            file_name = file_name.replace("%PMONTH%",funcdate.prev_month())
-            file_name = file_name.replace("%CYEAR%",funcdate.cur_year())
-            file_name = file_name.replace("%CMONTH%",funcdate.cur_month())
-            file_name = file_name.replace("%TODAY%",funcdate.today_file())
+            file_name = file_name.replace("%PYEAR%", funcdate.prev_year())
+            file_name = file_name.replace("%PMONTH%", funcdate.prev_month())
+            file_name = file_name.replace("%CYEAR%", funcdate.cur_year())
+            file_name = file_name.replace("%CMONTH%", funcdate.cur_month())
+            file_name = file_name.replace("%TODAY%", funcdate.today_file())
 
             # Send the mail
-            s_result = funcmail.Send(to_name,to_address,mail_language,mail_subject,mail_body,file_path,file_name)
+            s_result = send(to_name, to_address, mail_language, mail_subject, mail_body, file_path, file_name)
 
             # Mail result log
             if s_result == "Successfully sent email":
@@ -102,43 +92,43 @@ def Mail(s_trigger):
                 print("FAIL REASON: " + s_result)
                 funcfile.writelog("%t MAIL FAIL: " + to_address + " (" + to_name + ")")
                 funcfile.writelog("%t REASON FAIL: " + s_result)
-            
-    # Close the impoted data file
+
+    # Close the imported data file
     co.close()
 
-def Send(to_name,to_addr,mail_lang,mail_subject,mail_body,file_path,file_name):
 
-    """ Parameters
-    to_name = Name of the recipient
-    to_addr = Email address of the recipient
-    mail_subject = Email subject
-    mail_body = Email body
+def send(to_name, to_addr, mail_lang, mail_subject, mail_body, file_path, file_name):
     """
-    
+    Mail parameters
+    :param to_name: Name of the recipient
+    :param to_addr: Email address of the recipient
+    :param mail_lang: Language indicator A=Afrikaans Else=English
+    :param mail_subject: Email subject
+    :param mail_body: Email body
+    :param file_path: Attachment path
+    :param file_name: Attachment name
+    :return: Text message to indicate successful mail sending
+    """
+
     import smtplib
-    #from smtplib import SMTP # Standard connection
-    from smtplib import SMTP_SSL as SMTP #SSL connection
+    # from smtplib import SMTP # Standard connection
+    from smtplib import SMTP_SSL as SMTP  # SSL connection
     from email.mime.base import MIMEBase
     from email.mime.text import MIMEText
     from email.mime.multipart import MIMEMultipart
     from email import encoders
 
-    # Import own modules
-    import funcfile
-    import funcdate # Date functions
-
     # Declare variables
     s_return = ""
 
     from_addr = "nwu.internal.audit@gmail.com"
-    #from_addr = "21162395@nwu.ac.za"
+    # from_addr = "21162395@nwu.ac.za"
     msg = MIMEMultipart()
     msg['From'] = from_addr
     msg['To'] = to_addr
     msg['Subject'] = mail_subject
 
     if mail_lang == "A":
-
         body = "Hallo " + to_name + """
 
 """ + mail_body + """
@@ -146,9 +136,7 @@ def Send(to_name,to_addr,mail_lang,mail_subject,mail_body,file_path,file_name):
 Vriendelike groete
 Albert
 """
-
     else:
-
         body = "Hallo " + to_name + """
 
 """ + mail_body + """
@@ -156,34 +144,32 @@ Albert
 Best regards
 Albert
 """
-        
+
     msg.attach(MIMEText(body, 'plain'))
 
     if file_name != "":
         attachment = open(file_path + file_name, "rb")
         part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
+        part.set_payload(attachment.read())
         encoders.encode_base64(part)
         part.add_header('Content-Disposition', "attachment; filename= %s" % file_name)
         msg.attach(part)
 
-    ServerConnect = False
+    server_connect = False
     try:
-        
-        server = SMTP('smtp.gmail.com','465')
+        server = SMTP('smtp.gmail.com', '465')
         server.login("nwu.internal.audit@gmail.com", "fWQZgiUEaZIA10coueMj")
-        
-        #server = SMTP('smtp.gmail.com','568')
-        #server.login("nwu.internal.audit@gmail.com", "fWQZgiUEaZIA10coueMj")
-        
-        #server = SMTP('v-utl-lnx3.nwu.ac.za','25')
-        #server.login("21162395", "vsnpj%K0L%")
-        
-        #server.ehlo()
-        #server.starttls()
-        #server.ehlo()
-        
-        ServerConnect = True
+        # gmail from home
+        # server = SMTP('smtp.gmail.com','568')
+        # server.login("nwu.internal.audit@gmail.com", "fWQZgiUEaZIA10coueMj")
+        # nwu server
+        # server = SMTP('v-utl-lnx3.nwu.ac.za','25')
+        # server.login("21162395", "vsnpj%K0L%")
+        # ??? do not know when to use
+        # server.ehlo()
+        # server.starttls()
+        # server.ehlo()
+        server_connect = True
     except smtplib.SMTPHeloError as e:
         s_return = "Server did not reply"
     except smtplib.SMTPAuthenticationError as e:
@@ -191,7 +177,7 @@ Albert
     except smtplib.SMTPException as e:
         s_return = "Authentication failed"
 
-    if ServerConnect == True:
+    if server_connect:
         try:
             server.sendmail(from_addr, to_addr, msg.as_string())
             s_return = "Successfully sent email"
@@ -199,6 +185,5 @@ Albert
             s_return = "Error: unable to send email", e
         finally:
             server.close()
-    
-    return s_return
 
+    return s_return
