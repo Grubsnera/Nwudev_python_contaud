fb5e8c439106cddc9a2049c46d608aa00739aa32
grubsnera <7260880WF2Y0nGCE3Ngj>
2019-07-13 21:22:04 +0200

    PEP8

--- Mailer.py
+++ Mailer.py
@@ -1,20 +1,13 @@
-""" MAILER Function to invoke the mailer ***************************************
+"""
+MAILER Function to invoke the mailer ***************************************
 Copyright (c) Albert J van Rensburg
 30 May 2018
 *****************************************************************************"""
 
-# Import python objects
-import sys
-
-# Add own module path
-sys.path.append('S:/_my_modules')
-
 # Import own modules
-import funcmail
+from _my_modules import funcmail
 
 # Ask which mail trigger to send
 s_trigger = input("Mail trigger: ")
 
 funcmail.Mail(s_trigger)
-
-
