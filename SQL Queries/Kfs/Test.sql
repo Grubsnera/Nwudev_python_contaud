1544c795d9519f4696846eaa554112064a8de9d5
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-08-26 15:50:07 +0200

    Sql script testing.

---
+++ SQL Queries/Kfs/Test.sql
@@ -0,0 +1,15 @@
+﻿Select
+    X001ae_Report_payments_accroute_curr.PAYEE_NAME,
+    X001ae_Report_payments_accroute_curr.ORG_NM,
+    X001ae_Report_payments_accroute_curr.VENDOR_TYPE,
+    X001ae_Report_payments_accroute_curr.DOC_TYPE,
+    Sum(X001ae_Report_payments_accroute_curr.ACC_AMOUNT) As Sum_ACC_AMOUNT
+From
+    X001ae_Report_payments_accroute_curr
+Where
+    X001ae_Report_payments_accroute_curr.VENDOR_TYPE = 'V'
+Group By
+    X001ae_Report_payments_accroute_curr.PAYEE_NAME,
+    X001ae_Report_payments_accroute_curr.ORG_NM,
+    X001ae_Report_payments_accroute_curr.VENDOR_TYPE,
+    X001ae_Report_payments_accroute_curr.DOC_TYPE
