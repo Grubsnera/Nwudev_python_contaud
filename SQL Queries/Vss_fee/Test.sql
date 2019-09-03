d88e808568589a14619629ef444f4e2af4f86e7e
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-09-03 16:53:46 +0200

    New sql test queries

---
+++ SQL Queries/Vss_fee/Test.sql
@@ -0,0 +1,8 @@
+﻿Select
+    X010_Student_feereg.FEE_TYPE,
+    Count(X010_Student_feereg.FBUSINESSENTITYID) As COUNT,
+    Total(X010_Student_feereg.FEE_REG) As TOTAL
+From
+    X010_Student_feereg
+Group By
+    X010_Student_feereg.FEE_TYPE
