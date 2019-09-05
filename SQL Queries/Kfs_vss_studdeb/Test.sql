e441d4b94e5cd2af72c5d7b54cbb6444a80f9745
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-09-05 15:47:32 +0200

    New vss student debtor sql test queries.

---
+++ SQL Queries/Kfs_vss_studdeb/Test.sql
@@ -0,0 +1,9 @@
+﻿Select
+    X002ab_vss_transort.*,
+    X002ab_vss_transort.STUDENT_VSS As STUDENT_VSS1,
+    X002ab_vss_transort.TRANSCODE_VSS As TRANSCODE_VSS1
+From
+    X002ab_vss_transort
+Where
+    X002ab_vss_transort.STUDENT_VSS = '21573557' And
+    X002ab_vss_transort.TRANSCODE_VSS = '905'
