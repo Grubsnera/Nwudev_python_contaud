f2e22bf0ee735d4b8e215f77fc64ed890e97cc05
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-09-11 14:43:11 +0200

    Add new files.

---
+++ SQL Queries/Admin/Test.sql
@@ -0,0 +1,8 @@
+﻿Select
+    X001ad_auto_time."ACTION",
+    Count(X001ad_auto_time.LOG_ELAPSED) As Count_LOG_ELAPSED,
+    Total(X001ad_auto_time.LOG_SECOND) As Total_LOG_SECOND
+From
+    X001ad_auto_time
+Group By
+    X001ad_auto_time."ACTION"
