3f46e745d57aac466dd1d3f1d11a0e77308c39c3
grubsnera <7260880WF2Y0nGCE3Ngj>
2019-09-13 05:46:49 +0200

    Update vss dev area to PEP8.

--- B003_vss_lists_xdev.py
+++ B003_vss_lists_xdev.py
@@ -1,71 +1,132 @@
-# Import python modules
+"""
+Development area for VSS lists
+12 Sep 2019
+AB Janse van Rensburg (NWU21162395)
+"""
+
+# IMPORT PYTHON MODULES
 import csv
-import datetime
 import sqlite3
-import sys
 
-# Add own module path
-sys.path.append('S:/_my_modules')
-#print(sys.path)
+# IMPORT OWN MODULES
+from _my_modules import funcdate
+from _my_modules import funcfile
+from _my_modules import funcstudent
 
-# Import own modules
-import funcdate
-import funccsv
-import funcfile
+""" INDEX **********************************************************************
+ENVIRONMENT
+OPEN THE DATABASES
+TEMPORARY AREA
+BEGIN OF SCRIPT
+END OF SCRIPT
+*****************************************************************************"""
 
-# Open the script log file ******************************************************
+"""*****************************************************************************
+ENVIRONMENT
+*****************************************************************************"""
 
+# SCRIPT LOG FILE
 funcfile.writelog("Now")
-funcfile.writelog("SCRIPT: B003_VSS_LISTS_DEV")
-funcfile.writelog("--------------------------")
-print("--------------")
-print("B003_VSS_LISTS")
-print("--------------")
-ilog_severity = 1
+funcfile.writelog("SCRIPT: B003_VSS_LISTS_XDEV")
+funcfile.writelog("---------------------------")
+print("-------------------")
+print("B003_VSS_LISTS_XDEV")
+print("-------------------")
 
-# Declare variables
-so_path = "W:/Vss/" #Source database path
-re_path = "R:/Vss/" #Results
-so_file = "Vss.sqlite" #Source database
-ed_path = "S:/_external_data/"
-s_sql = "" #SQL statements
+# DECLARE VARIABLES
+ed_path = "S:/_external_data/"  # External data path
+so_path = "W:/Vss/"  # Source database path
+so_file = "Vss.sqlite"  # Source database
+re_path = "R:/Vss/"
+l_vacuum: bool = False
 
-# Open the SOURCE file
+"""*****************************************************************************
+OPEN THE DATABASES
+*****************************************************************************"""
+print("OPEN THE DATABASES")
+funcfile.writelog("OPEN THE DATABASES")
+
+# OPEN SQLITE SOURCE table
+print("Open sqlite database...")
 with sqlite3.connect(so_path+so_file) as so_conn:
     so_curs = so_conn.cursor()
-
 funcfile.writelog("OPEN DATABASE: " + so_file)
 
-# Import OWN LOOKUPS table *************************************************
+# ATTACH VSS DATABASE
+# print("Attach vss database...")
+
+"""*****************************************************************************
+TEMPORARY AREA
+*****************************************************************************"""
+print("TEMPORARY AREA")
+funcfile.writelog("TEMPORARY AREA")
+
+# IMPORT OWN LOOKUPS
 print("Import own lookups...")
-tb_name = "X000_OWN_LOOKUPS"
-so_curs.execute("DROP TABLE IF EXISTS " + tb_name)
-so_curs.execute("CREATE TABLE " + tb_name + "(LOOKUP TEXT,LOOKUP_CODE TEXT,LOOKUP_DESCRIPTION TEXT)")
+sr_file = "X000_OWN_LOOKUPS"
+so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+so_curs.execute("CREATE TABLE " + sr_file + "(LOOKUP TEXT,LOOKUP_CODE TEXT,LOOKUP_DESCRIPTION TEXT)")
 s_cols = ""
-co = open(ed_path + "001_own_vss_lookups.csv", "rU")
+co = open(ed_path + "001_own_vss_lookups.csv", newline=None)
 co_reader = csv.reader(co)
 for row in co_reader:
     if row[0] == "LOOKUP":
         continue
     else:
-        s_cols = "INSERT INTO " + tb_name + " VALUES('" + row[0] + "','" + row[1] + "','" + row[2] + "')"
+        s_cols = "INSERT INTO " + sr_file + " VALUES('" + row[0] + "','" + row[1] + "','" + row[2] + "')"
         so_curs.execute(s_cols)
 so_conn.commit()
-# Close the impoted data file
 co.close()
-funcfile.writelog("%t IMPORT TABLE: " + tb_name)
+funcfile.writelog("%t IMPORT TABLE: " + sr_file)
 
 """*****************************************************************************
-BEGIN
+BEGIN OF SCRIPT
 *****************************************************************************"""
+print("BEGIN OF SCRIPT")
+funcfile.writelog("BEGIN OF SCRIPT")
+
+# CALCULATE THE REGISTRATION FEES LEVIED
+print("Join presentation and qualification levels...")
+sr_file = "X006aa_present_quallevel"
+s_sql = "Create view " + sr_file + " AS" + """
+Select
+    PRES.KENROLMENTPRESENTATIONID,
+    PRES.FQUALPRESENTINGOUID,
+    PRES.FENROLMENTCATEGORYCODEID,
+    PRES.FPRESENTATIONCATEGORYCODEID,
+    QUAL.FBUSINESSENTITYID,
+    QUAL.FQUALLEVELAPID,
+    PRES.STARTDATE As PRES_STARTDATE,
+    PRES.ENDDATE As PRES_ENDDATE,
+    PRES.AUDITDATETIME As PRES_AUDITDATE,
+    PRES.FAUDITUSERCODE As PRES_AUDITUSER,
+    QUAL.STARTDATE As QUAL_STARTDATE,
+    QUAL.ENDDATE As QUAL_ENDDATE,
+    QUAL.AUDITDATETIME As QUAL_AUDITDATE,
+    QUAL.FAUDITUSERCODE As QUAL_AUDITUSER
+From
+    PRESENTOUENROLPRESENTCAT PRES Left Join
+    QUALLEVELPRESENTINGOU QUAL On QUAL.KPRESENTINGOUID = PRES.FQUALPRESENTINGOUID
+;"""
+so_curs.execute("DROP VIEW IF EXISTS " + sr_file)
+so_curs.execute(s_sql)
+funcfile.writelog("%t BUILD VIEW: " + sr_file)
 
 """*****************************************************************************
-END
+END OF SCRIPT
 *****************************************************************************"""
+print("END OF SCRIPT")
+funcfile.writelog("END OF SCRIPT")
 
-# Close the connection *********************************************************
+# CLOSE THE DATABASE CONNECTION
+if l_vacuum:
+    print("Vacuum the database...")
+    so_conn.commit()
+    so_conn.execute('VACUUM')
+    funcfile.writelog("%t VACUUM DATABASE: " + so_file)
+so_conn.commit()
 so_conn.close()
 
-# Close the log writer *********************************************************
-funcfile.writelog("-----------------------------")
-funcfile.writelog("COMPLETED: B003_VSS_LISTS_DEV")
+# CLOSE THE LOG WRITER *********************************************************
+funcfile.writelog("------------------------------")
+funcfile.writelog("COMPLETED: B003_VSS_LISTS_XDEV")
