94962170e81097dc84e051fa67414a8fe8c6ca07
grubsnera <7260880WF2Y0nGCE3Ngj>
2019-09-13 05:20:32 +0200

    Add statistic population standard deviation. Add statistic highest value.

--- _my_modules/funcstat.py
+++ _my_modules/funcstat.py
@@ -8,14 +8,72 @@ Author: Albert J van Rensburg (NWU:21162395)
 import statistics
 
 
-def stat_mode(o_cursor, s_table, s_column):
+def stat_mode(o_cursor, s_table, s_column, s_where=""):
     """
-    Function to calculate the MODE of any table column
-    :return: The table column mode
+    Function to return the statistic mode for a column
+    :param o_cursor: Database cursor
+    :param s_table: Table in database
+    :param s_column: Column to calculate
+    :param s_where: Where clause
+    :return: Mode statistic
     """
 
     # Convert the table column into a list and the calculate the mode
+    s_sql = "SELECT " + s_column
+    s_sql += " FROM " + s_table
+    s_sql += " WHERE " + s_column + " != 0"
+    if s_where != "":
+        s_sql += " And " + s_where
+    s_sql += " ;"
+    # print(s_sql)
     a_list = []
-    for row in o_cursor.execute("SELECT " + s_column + " FROM " + s_table + " WHERE " + s_column + " != 0 ;").fetchall():
+    for row in o_cursor.execute(s_sql).fetchall():
         a_list.append(row[0])
     return statistics.mode(a_list)
+
+
+def stat_pstdev(o_cursor, s_table, s_column, s_where=""):
+    """
+    Function to return the statistic mode for a column
+    :param o_cursor: Database cursor
+    :param s_table: Table in database
+    :param s_column: Column to calculate
+    :param s_where: Where clause
+    :return: Population standard deviation statistic
+    """
+
+    # Convert the table column into a list and the calculate the mode
+    s_sql = "SELECT " + s_column
+    s_sql += " FROM " + s_table
+    s_sql += " WHERE " + s_column + " != 0"
+    if s_where != "":
+        s_sql += " And " + s_where
+    s_sql += " ;"
+    # print(s_sql)
+    a_list = []
+    for row in o_cursor.execute(s_sql).fetchall():
+        a_list.append(row[0])
+    return statistics.pstdev(a_list)
+
+
+def stat_highest_value(o_cursor, s_table, s_column, s_where=""):
+    """
+    Function to return the statistic highest value for a column
+    :param o_cursor: Database cursor
+    :param s_table: Table in database
+    :param s_column: Column to calculate
+    :param s_where: Where clause
+    :return: Mode statistic
+    """
+
+    # Convert the table column into a list and the calculate the mode
+    s_sql = "SELECT " + s_column
+    s_sql += " FROM " + s_table
+    s_sql += " WHERE " + s_column + " != 0"
+    if s_where != "":
+        s_sql += " And " + s_where
+    s_sql += " ORDER BY " + s_column + " DESC "
+    s_sql += " ;"
+    # print(s_sql)
+    row = o_cursor.execute(s_sql).fetchone()
+    return row[0]
