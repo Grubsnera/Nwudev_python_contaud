08bc68801048a461709ad4e8e365cec8a3eea66b
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-09-25 09:18:41 +0200

    Query dev and testing.

--- SQL Queries/Vss_deferment/Test.sql
+++ SQL Queries/Vss_deferment/Test.sql
@@ -1,4 +1,7 @@
 ﻿Select
-    SUMM.*
+    X001ax_Deferments_final_curr.DEFER_TYPE_DESC,
+    Count(X001ax_Deferments_final_curr.STUDENT_VSS) As Count_STUDENT_VSS
 From
-    X001ac_Students_deferment_summ_curr SUMM
+    X001ax_Deferments_final_curr
+Group By
+    X001ax_Deferments_final_curr.DEFER_TYPE_DESC
