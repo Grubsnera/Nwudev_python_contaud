d6faecb10e03bf0cca8422e04f1759396462e14e
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-11-15 08:39:02 +0200

    Test queries.

--- SQL Queries/Vss_fee/Test.sql
+++ SQL Queries/Vss_fee/Test.sql
@@ -1,11 +1,4 @@
 ﻿Select
-    X020bx_Student_master_sort.KSTUDBUSENTID,
-    Count(X020bx_Student_master_sort.VALID) As Count_VALID
+    *
 From
     X020bx_Student_master_sort
-Where
-    X020bx_Student_master_sort.FEE_LEVIED_TYPE Like ('4%')
-Group By
-    X020bx_Student_master_sort.KSTUDBUSENTID
-Having
-    Count(X020bx_Student_master_sort.VALID) > 1
