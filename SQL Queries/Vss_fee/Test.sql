3f97dfacf7f20aabe6352008ecaf551742d5e873
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2019-11-20 07:26:58 +0200

    Sql test queries.

--- SQL Queries/Vss_fee/Test.sql
+++ SQL Queries/Vss_fee/Test.sql
@@ -1,4 +1,14 @@
 ﻿Select
-    *
+    X020ae_Student_convert.KSTUDBUSENTID,
+    X020ae_Student_convert1.FQUALLEVELAPID,
+    X020ae_Student_convert1.CONV_IND
 From
-    X020bx_Student_master_sort
+    X020ae_Student_convert Inner Join
+    X020ae_Student_convert X020ae_Student_convert1 On X020ae_Student_convert1.KSTUDBUSENTID =
+            X020ae_Student_convert.KSTUDBUSENTID
+Group By
+    X020ae_Student_convert.KSTUDBUSENTID,
+    X020ae_Student_convert1.FQUALLEVELAPID,
+    X020ae_Student_convert1.CONV_IND
+Having
+    Count(X020ae_Student_convert.FQUALLEVELAPID) = 2
