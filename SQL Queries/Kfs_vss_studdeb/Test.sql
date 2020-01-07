01624a43784cd9c530b5ed198b9460adf6e8c005
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-01-07 16:47:53 +0200

    Test statements.

--- SQL Queries/Kfs_vss_studdeb/Test.sql
+++ SQL Queries/Kfs_vss_studdeb/Test.sql
@@ -1,9 +1,9 @@
 ﻿Select
-    X002ab_vss_transort.*,
-    X002ab_vss_transort.STUDENT_VSS As STUDENT_VSS1,
-    X002ab_vss_transort.TRANSCODE_VSS As TRANSCODE_VSS1
+    TYPE.STUDENT,
+    Count(TYPE.BAL_CLOS) As Total_BAL_CLOS,
+    Total(TYPE.BAL_OPEN) As Total_BAL_OPEN,
+    Total(TYPE.DIFF_BAL) As Total_DIFF_BAL
 From
-    X002ab_vss_transort
-Where
-    X002ab_vss_transort.STUDENT_VSS = '21573557' And
-    X002ab_vss_transort.TRANSCODE_VSS = '905'
+    X002dd_vss_closing_open_differ TYPE
+Group By
+    TYPE.STUDENT
