ca13bb600ebfa79ebe0a1da1b58cba65447b56e5
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-01-08 12:55:34 +0200

    Add run script for previous year fee tests.

---
+++ C302_test_student_fee_run_prev.py
@@ -0,0 +1,19 @@
+# IMPORT MODULES
+import C302_test_student_fee
+from _my_modules import funcfile
+from _my_modules import funcsys
+
+# OPEN THE LOG WRITER
+funcfile.writelog("Now")
+funcfile.writelog("SCRIPT: C302_TEST_STUDENT_FEE_RUN")
+funcfile.writelog("---------------------------------")
+
+try:
+    C302_test_student_fee.student_fee("curr")
+except Exception as e:
+    funcsys.ErrMessage(e)
+
+# CLOSE THE LOG WRITER
+funcfile.writelog("Now")
+funcfile.writelog("COMPLETED: C302_TEST_STUDENT_FEE_RUN")
+funcfile.writelog("------------------------------------")
