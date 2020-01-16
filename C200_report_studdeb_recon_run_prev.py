f6f3700cdca919dc7cdbef7ccd52bfbe85e917b8
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-01-16 10:39:13 +0200

    Add previous year kfs vss recon run script.

---
+++ C200_report_studdeb_recon_run_prev.py
@@ -0,0 +1,30 @@
+"""
+Script to run C200_REPORT_STUDDEB_RECON_RUN
+"""
+
+# Import own modules
+from _my_modules import funcfile
+from _my_modules import funcsys
+
+import C200_report_studdeb_recon
+
+funcfile.writelog("Now")
+funcfile.writelog("SCRIPT: C200_REPORT_STUDDEB_RECON_RUN")
+funcfile.writelog("-------------------------------------")
+
+# Environment
+l_mail = True
+
+# Kfs reports
+try:
+    # C200_report_studdeb_recon.Report_studdeb_recon('48478050.09', '-12494403.94', '50039673.35', "curr")
+    C200_report_studdeb_recon.Report_studdeb_recon(0, 0, 0, "prev", "2019")
+    # 2019 Opening balances
+    # C200_report_studdeb_recon.Report_studdeb_recon('66561452.48','-18340951.06','39482933.18')
+except Exception as e:
+    funcsys.ErrMessage(e)
+
+# Close the log writer
+funcfile.writelog("Now")
+funcfile.writelog("COMPLETED: C200_REPORT_STUDDEB_RECON_RUN")
+funcfile.writelog("----------------------------------------")
