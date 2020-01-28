b0118ebd75306225e89277a791140543dd8ce64d
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-01-28 09:54:49 +0200

    Test script.

--- Test script.py
+++ Test script.py
@@ -7,20 +7,24 @@
 
 # GET PREVIOUS FINDINGS
 if i_find > 0:
-    i = functest.get_previous_finding(so_curs, ed_path, "?.txt", "", "ITTTT")
+    i = functest.get_previous_finding(so_curs, ed_path, "?.txt", "?", "ITTTT")
+    print(i)
     so_conn.commit()
 
 # SET PREVIOUS FINDINGS
 if i_find > 0:
     i = functest.set_previous_finding(so_curs)
+    print(i)
     so_conn.commit()
 
 # IMPORT OFFICERS FOR MAIL REPORTING PURPOSES
 if i_find > 0 and i_coun > 0:
-    i = functest.get_officer(so_curs, "")
+    i = functest.get_officer(so_curs, "?")
+    print(i)
     so_conn.commit()
 
 # IMPORT SUPERVISORS FOR MAIL REPORTING PURPOSES
 if i_find > 0 and i_coun > 0:
-    i = functest.get_supervisor(so_curs, "")
+    i = functest.get_supervisor(so_curs, "?")
+    print(i)
     so_conn.commit()
