12f667e86daabe784a5b4e421d536a55389c7e85
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-01-28 08:53:32 +0200

    Test area for any script. No perm info stored here.

---
+++ Test script.py
@@ -0,0 +1,26 @@
+# TEST GROUND FOR SCRIPTS
+# CAN BE DELETED - NO PERMANENT CODE STORED HERE
+
+# SYSTEM MODULES
+
+# IMPORT OWN MODULES
+
+# GET PREVIOUS FINDINGS
+if i_find > 0:
+    i = functest.get_previous_finding(so_curs, ed_path, "?.txt", "", "ITTTT")
+    so_conn.commit()
+
+# SET PREVIOUS FINDINGS
+if i_find > 0:
+    i = functest.set_previous_finding(so_curs)
+    so_conn.commit()
+
+# IMPORT OFFICERS FOR MAIL REPORTING PURPOSES
+if i_find > 0 and i_coun > 0:
+    i = functest.get_officer(so_curs, "")
+    so_conn.commit()
+
+# IMPORT SUPERVISORS FOR MAIL REPORTING PURPOSES
+if i_find > 0 and i_coun > 0:
+    i = functest.get_supervisor(so_curs, "")
+    so_conn.commit()
