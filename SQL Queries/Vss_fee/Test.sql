3e2fb17c95d0983536eefa656369eb89d26d2481
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-02-03 15:07:35 +0200

    Test scripts.

--- SQL Queries/Vss_fee/Test.sql
+++ SQL Queries/Vss_fee/Test.sql
@@ -1,14 +1,13 @@
 ﻿Select
-    X000_Transaction.STUDENT,
-    X000_Transaction.MODULE,
-    X000_Transaction.MODULE_NAME,
-    Count(X000_Transaction.FQUALLEVELAPID) As Count_FQUALLEVELAPID,
-    Total(X000_Transaction.AMOUNT) As Total_AMOUNT
+    FIND.FMODULEAPID,
+    FIND.CAMPUS,
+    FIND.PRESENT_ID,
+    FIND.PRESENT_CATEGORY,
+    FIND.ENROL_ID,
+    FIND.ENROL_CATEGORY,
+    FIND.MODULE,
+    FIND.MODULE_NAME,
+    FIND.COUNT,
+    FIND.AMOUNT
 From
-    X000_Transaction
-Where
-    X000_Transaction.STUDENT = 24823252
-Group By
-    X000_Transaction.STUDENT,
-    X000_Transaction.MODULE,
-    X000_Transaction.MODULE_NAME
+    X031aa_Modu_nofee_loaded FIND
