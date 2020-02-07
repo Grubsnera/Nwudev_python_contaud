5de0c293783c89d44a48fc63a0a385f767a22251
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-02-07 16:25:47 +0200

    Develope small split payments test.

--- C201_creditor_test_payments_xdev.py
+++ C201_creditor_test_payments_xdev.py
@@ -49,6 +49,10 @@ funcfile.writelog("OPEN DATABASE: " + so_file)
 # ATTACH DATA SOURCES
 so_curs.execute("ATTACH DATABASE 'W:/Kfs/Kfs.sqlite' AS 'KFS'")
 funcfile.writelog("%t ATTACH DATABASE: KFS.SQLITE")
+so_curs.execute("ATTACH DATABASE 'W:/Kfs/Kfs_curr.sqlite' AS 'KFSCURR'")
+funcfile.writelog("%t ATTACH DATABASE: KFS_CURR.SQLITE")
+so_curs.execute("ATTACH DATABASE 'W:/Kfs/Kfs_prev.sqlite' AS 'KFSPREV'")
+funcfile.writelog("%t ATTACH DATABASE: KFS_PREV.SQLITE")
 so_curs.execute("ATTACH DATABASE 'W:/People/People.sqlite' AS 'PEOPLE'")
 funcfile.writelog("%t ATTACH DATABASE: PEOPLE.SQLITE")
 
@@ -57,6 +61,56 @@ BEGIN OF SCRIPT
 *****************************************************************************"""
 print("BEGIN OF SCRIPT")
 funcfile.writelog("BEGIN OF SCRIPT")
+
+# TODO Develope script to test small split payments
+
+# IDENTIFY AND SUMMARIZE SMALL PAYMENTS
+# Payments between R4000 and R5000
+print("Identify and summarize small payments...")
+sr_file: str = "X001ba_Small_split_pay_summ"
+so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+s_sql = "Create Table " + sr_file + " As " + """
+Select
+    SUMM.INIT_EMP_NO,
+    SUMM.INIT_EMP_NAME,
+    SUMM.VENDOR_ID,
+    SUMM.VENDOR_NAME,
+    Count(SUMM.EDOC) As Count_EDOC
+From
+    KFSCURR.X001ab_Report_payments_initiate SUMM
+Where
+    SUMM.INIT_EMP_NO <> "" And
+    SUMM.NET_PMT_AMT >= 4000 And
+    SUMM.NET_PMT_AMT <= 5000
+Group By
+    SUMM.INIT_EMP_NO,
+    SUMM.VENDOR_ID,
+    SUMM.NET_PMT_AMT
+Having
+    Count(SUMM.EDOC) > 1
+;"""
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: " + sr_file)
+
+# IDENTIFY PAYMENT TRANSACTIONS
+print("Identify payment transactions...")
+sr_file: str = "X001ba_Small_split_pay_list"
+so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
+s_sql = "Create Table " + sr_file + " As " + """
+Select
+    LIST.*
+From
+    KFSCURR.X001ab_Report_payments_initiate LIST Inner Join
+    X001ba_Small_split_pay_summ SUMM On SUMM.INIT_EMP_NO = LIST.INIT_EMP_NO And SUMM.VENDOR_ID = LIST.VENDOR_ID
+Order By
+    LIST.INIT_EMP_NO,
+    LIST.VENDOR_ID,
+    LIST.PMT_DT    
+;"""
+so_curs.execute(s_sql)
+so_conn.commit()
+funcfile.writelog("%t BUILD TABLE: " + sr_file)
 
 """ ****************************************************************************
 END OF SCRIPT
