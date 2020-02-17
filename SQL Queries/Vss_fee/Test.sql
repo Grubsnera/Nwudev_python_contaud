d6921ea73ef637229dfefafe93f79fe14a435896
Grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-02-17 16:20:31 +0200

    Test scripts.

--- SQL Queries/Vss_fee/Test.sql
+++ SQL Queries/Vss_fee/Test.sql
@@ -1,22 +1,12 @@
 ﻿Select
-    X020ba_Student_master.FQUALLEVELAPID,
-    X020ba_Student_master.CAMPUS,
-    X020ba_Student_master.PRESENT_CAT,
-    X020ba_Student_master.ENROL_CAT,
-    X020ba_Student_master.QUALIFICATION,
-    X020ba_Student_master.QUALIFICATION_NAME,
-    Count(X020ba_Student_master.VALID) As COUNT
+    MERG.*,
+    MERG.FQUALLEVELAPID As FQUALLEVELAPID1,
+    MERG.CAMPUS As CAMPUS1,
+    Count(MERG.PRESENT_ID) As COUNT_OCC
 From
-    X020ba_Student_master
-Where
-    X020ba_Student_master.FEE_LEVIED_TYPE = '1 NO TRANS/ZERO FEE' And
-    X020ba_Student_master.FEE_SHOULD_BE Like ('4%')
+    X021ga_Qual_present_dmerg MERG
 Group By
-    X020ba_Student_master.FQUALLEVELAPID,
-    X020ba_Student_master.CAMPUS,
-    X020ba_Student_master.PRESENT_CAT,
-    X020ba_Student_master.ENROL_CAT,
-    X020ba_Student_master.QUALIFICATION,
-    X020ba_Student_master.QUALIFICATION_NAME,
-    X020ba_Student_master.FEE_LEVIED_TYPE,
-    X020ba_Student_master.FEE_SHOULD_BE
+    MERG.FQUALLEVELAPID,
+    MERG.CAMPUS
+Having
+    Count(MERG.PRESENT_ID) = 1
