ae714855e7374d2ef51452daa3a579f8bcf75e25
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-03-07 14:58:45 +0200

    Reset temporary parameters.

--- _my_modules/funcsms.py
+++ _my_modules/funcsms.py
@@ -4,7 +4,7 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 account_sid = 'AC5dd70f4554e5b8ab34798bfcc91bd5b3'
-auth_token = 'f9fde5a696576045985bd6287ebb6ef2'
+auth_token = 'aaf9fde5a696576045985bd6287ebb6ef2'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
