22392427acc3b3d4989d63d80c7b0f36cf96a007
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-03-07 16:48:46 +0200

    Add test mail script.

---
+++ Test mail.py
@@ -0,0 +1,13 @@
+"""
+MAILER Function to invoke the mailer ***************************************
+Copyright (c) Albert J van Rensburg
+30 May 2018
+*****************************************************************************"""
+
+# Import own modules
+from _my_modules import funcmail
+
+# Ask which mail trigger to send
+s_trigger = input("Mail trigger: ")
+
+funcmail.Mail(s_trigger)
