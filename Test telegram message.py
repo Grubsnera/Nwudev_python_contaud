7f9f6228d22144b66911852a32280c5a9f96b24d
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-03-09 16:39:15 +0200

    Telegram message test script.

---
+++ Test telegram message.py
@@ -0,0 +1,10 @@
+# TEST GROUND FOR SCRIPTS
+# CAN BE DELETED - NO PERMANENT CODE STORED HERE
+
+# SYSTEM MODULES
+from _my_modules import funcsms
+
+# IMPORT OWN MODULES
+
+# SEND FIRST MESSAGE
+print(funcsms.send_telegram('Test message from telegram @nwuia_bot'))
