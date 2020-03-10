a4bdf40cd1c87175a4dd80a420bb977fd0099b0b
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-03-10 08:38:18 +0200

    New script to test server readiness.

---
+++ A000_runservertest.py
@@ -0,0 +1,7 @@
+"""
+Script to tests to see if SERVER is up and running, and
+    to make all finding tables blank if run is not completed successfully
+Author: Aalbert B Janse van Rensburg (NWU:21162395)
+Date create: 10 Mar 2020
+"""
+
