be47b176cb343b6fe464022defbc3b44afd8bd22
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-03-23 15:14:27 +0200

    Test. Test sql scripts.

--- SQL Queries/Kfs/Test.sql
+++ SQL Queries/Kfs/Test.sql
@@ -1,9 +1,16 @@
 ﻿Select
-    X001ad_Report_payments_accroute.*,
-    X001ad_Report_payments_accroute.VENDOR_ID As VENDOR_ID1,
-    X001ad_Report_payments_accroute.INIT_EMP_NAME As INIT_EMP_NAME1
+    X000_Vendor.VENDOR_ID,
+    X000_Vendor.VNDR_NM,
+    X000_Vendor.VNDR_TYP_CD,
+    Lower(X000_Vendor.VEND_MAIL) As VEND_MAIL,
+    Lower(X000_Vendor.EMAIL) As VEND_MAIL2,
+    Lower(X000_Vendor.EMAIL_CONTACT) As VEND_MAILC
 From
-    X001ad_Report_payments_accroute
+    X000_Vendor
 Where
-    X001ad_Report_payments_accroute.VENDOR_ID = '11274298-0' And
-    X001ad_Report_payments_accroute.INIT_EMP_NAME Like ('%PYB VAN BLERK%')
+    (Lower(X000_Vendor.VEND_MAIL) Like ('%nwu.ac.za%') And
+        X000_Vendor.DOBJ_MAINT_CD_ACTV_IND = 'Y') Or
+    (Lower(X000_Vendor.EMAIL) Like ('%nwu.ac.za%') And
+        X000_Vendor.DOBJ_MAINT_CD_ACTV_IND = 'Y') Or
+    (Lower(X000_Vendor.EMAIL_CONTACT) Like ('%nwu.ac.za%') And
+        X000_Vendor.DOBJ_MAINT_CD_ACTV_IND = 'Y')
---
+++ SQL Queries/Kfs/Vendor_pay_summ.sql
@@ -0,0 +1,8 @@
+﻿Select
+    X000_Payments.VENDOR_ID,
+    Max(X000_Payments.PMT_DT) As Max_PMT_DT,
+    Total(X000_Payments.NET_PMT_AMT) As Total_NET_PMT_AMT
+From
+    X000_Payments
+Group By
+    X000_Payments.VENDOR_ID
