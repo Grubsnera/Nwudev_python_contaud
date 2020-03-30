db423acfef6019ad6552e314590470b4aefb3701
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-03-30 11:35:46 +0200

    Delete. Delete test queries.

--- SQL Queries/Kfs/Vendor_type_summary.sql
+++
@@ -1,9 +0,0 @@
-﻿Select
-    X000_Vendor.VNDR_TYP_CD,
-    X000_Vendor.DOBJ_MAINT_CD_ACTV_IND,
-    Count(X000_Vendor.VENDOR_ID) As Count_VENDOR_ID
-From
-    X000_Vendor
-Group By
-    X000_Vendor.VNDR_TYP_CD,
-    X000_Vendor.DOBJ_MAINT_CD_ACTV_IND
