9169e00b78c58f7777b36578e555067e9a5f7cc8
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-04-06 12:25:53 +0200

    New. Sql to display payment document type summary.

---
+++ SQL Queries/Kfs/Doc_label_summary.sql
@@ -0,0 +1,7 @@
+﻿Select
+    X001aa_Report_payments.DOC_LABEL,
+    Count(X001aa_Report_payments.EDOC) As Count_EDOC
+From
+    X001aa_Report_payments
+Group By
+    X001aa_Report_payments.DOC_LABEL
