4e64d3d773e119b5cfcd897c0627fed0f3a4d1ed
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-04-08 09:41:23 +0200

    Test. Vendor type summary.

---
+++ SQL Queries/Kfs/Vendor_type_summary.sql
@@ -0,0 +1,10 @@
+﻿Select
+    X000_Vendor.VNDR_TYP_CD,
+    Count(X000_Vendor.VENDOR_ID) As Count_VENDOR_ID
+From
+    X000_Vendor
+Where
+    X000_Vendor.DOBJ_MAINT_CD_ACTV_IND = 'Y'
+Group By
+    X000_Vendor.VNDR_TYP_CD,
+    X000_Vendor.DOBJ_MAINT_CD_ACTV_IND
