a6e9eb2e238a4ab2b1955ab9e5efb58fd300ea4f
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-04-08 09:41:50 +0200

    Test. Payment document type summary.

--- SQL Queries/Kfs/Doc_label_summary.sql
+++ SQL Queries/Kfs/Doc_label_summary.sql
@@ -1,7 +1,9 @@
 ﻿Select
     X001aa_Report_payments.DOC_LABEL,
+    X001aa_Report_payments.DOC_TYPE,
     Count(X001aa_Report_payments.EDOC) As Count_EDOC
 From
     X001aa_Report_payments
 Group By
-    X001aa_Report_payments.DOC_LABEL
+    X001aa_Report_payments.DOC_LABEL,
+    X001aa_Report_payments.DOC_TYPE
