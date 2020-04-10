c7b3933f2b87842a098d096682dea5c1984309e8
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-04-10 08:39:24 +0200

    Test. Test sql scripts.

--- SQL Queries/Kfs/Test.sql
+++ SQL Queries/Kfs/Test.sql
@@ -1,7 +1,24 @@
 ﻿Select
-    X001aa_Report_payments.DOC_LABEL,
-    Count(X001aa_Report_payments.EDOC) As Count_EDOC
+    PAYM.ORG_NM,
+    PAYM.EDOC,
+    PAYM.VENDOR_ID,
+    PAYM.VENDOR_TYPE,
+    PAYM.PMT_DT,
+    Total(PAYM.ACC_AMOUNT) As Total_ACC_AMOUNT
 From
-    X001aa_Report_payments
+    X001ad_Report_payments_accroute PAYM
+Where
+    PAYM.VENDOR_TYPE = 'V'
 Group By
-    X001aa_Report_payments.DOC_LABEL
+    PAYM.ORG_NM,
+    PAYM.EDOC,
+    PAYM.VENDOR_ID,
+    PAYM.VENDOR_TYPE,
+    PAYM.PMT_DT
+Having
+    Total(PAYM.ACC_AMOUNT) > 2500 And
+    Total(PAYM.ACC_AMOUNT) < 5000
+Order By
+    PAYM.ORG_NM,
+    PAYM.VENDOR_ID,
+    PAYM.PMT_DT
