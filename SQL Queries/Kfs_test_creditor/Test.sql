81ddc6f372aea50b8e7192f2da1b4e1280598d26
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-04-15 16:04:51 +0200

    Test scripts.

--- SQL Queries/Kfs_test_creditor/Test.sql
+++ SQL Queries/Kfs_test_creditor/Test.sql
@@ -1,24 +1,15 @@
 ﻿Select
-    a.ORG_NM,
-    a.FIN_OBJ_CD_NM,
-    a.VENDOR_ID,
-    a.VENDOR_TYPE,
-    a.DOC_TYPE,
-    a.PMT_DT,
-    a.EDOC,
-    a.TOT_AMOUNT,
-    b.PMT_DT As PMT_DT1,
-    cast(julianday(b.PMT_DT) - julianday(a.PMT_DT) As int) As DAYS,
-    b.EDOC As EDOC1,
-    b.TRAN_COUNT,
-    b.TOT_AMOUNT As TOT_AMOUNT1,
-    cast(a.TOT_AMOUNT + b.TOT_AMOUNT As real) As TOTAL
+    X001bac_Small_split_pay_select.ACC_COST_STRING,
+    X001bac_Small_split_pay_select.EDOC_A,
+    X001bac_Small_split_pay_select.AMOUNT_PD_A,
+    X001bac_Small_split_pay_select.EDOC_B,
+    X001bac_Small_split_pay_select.AMOUNT_PD_B,
+    Count(X001bac_Small_split_pay_select.DAYS_AFTER) As Count_DAYS_AFTER
 From
-    X001baa_Small_split_pay_summ a Inner Join
-    X001bab_Small_split_pay_list b On b.ORG_NM = a.ORG_NM
-            And b.FIN_OBJ_CD_NM = a.FIN_OBJ_CD_NM
-            And b.VENDOR_ID = a.VENDOR_ID
-            And julianday(b.PMT_DT) - julianday(a.PMT_DT) < 3
-            And julianday(b.PMT_DT) - julianday(a.PMT_DT) > 0
-            And cast(a.TOT_AMOUNT + b.TOT_AMOUNT As real) > 5000
-            And a.FIN_OBJ_CD_NM Not Like ('%TRAVEL%')
+    X001bac_Small_split_pay_select
+Group By
+    X001bac_Small_split_pay_select.ACC_COST_STRING,
+    X001bac_Small_split_pay_select.EDOC_A,
+    X001bac_Small_split_pay_select.AMOUNT_PD_A,
+    X001bac_Small_split_pay_select.EDOC_B,
+    X001bac_Small_split_pay_select.AMOUNT_PD_B
