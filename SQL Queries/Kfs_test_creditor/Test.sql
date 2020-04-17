44b686f23675b9afb186c3be7efbc6a19e4e3350
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-04-17 13:22:15 +0200

    Test scripts.

--- SQL Queries/Kfs_test_creditor/Test.sql
+++ SQL Queries/Kfs_test_creditor/Test.sql
@@ -1,15 +1,13 @@
 ﻿Select
-    X001bac_Small_split_pay_select.ACC_COST_STRING,
-    X001bac_Small_split_pay_select.EDOC_A,
-    X001bac_Small_split_pay_select.AMOUNT_PD_A,
-    X001bac_Small_split_pay_select.EDOC_B,
-    X001bac_Small_split_pay_select.AMOUNT_PD_B,
-    Count(X001bac_Small_split_pay_select.DAYS_AFTER) As Count_DAYS_AFTER
+    X001ba_a_vendor_small_split_payment.VENDOR_ID,
+    X001ba_a_vendor_small_split_payment.VNDR_TYP_CD,
+    X001ba_a_vendor_small_split_payment.PAYEE_NAME,
+    X001ba_a_vendor_small_split_payment.FIN_OBJ_CD_NM,
+    Count(X001ba_a_vendor_small_split_payment.EDOC) As Count_EDOC
 From
-    X001bac_Small_split_pay_select
+    X001ba_a_vendor_small_split_payment
 Group By
-    X001bac_Small_split_pay_select.ACC_COST_STRING,
-    X001bac_Small_split_pay_select.EDOC_A,
-    X001bac_Small_split_pay_select.AMOUNT_PD_A,
-    X001bac_Small_split_pay_select.EDOC_B,
-    X001bac_Small_split_pay_select.AMOUNT_PD_B
+    X001ba_a_vendor_small_split_payment.VENDOR_ID,
+    X001ba_a_vendor_small_split_payment.VNDR_TYP_CD,
+    X001ba_a_vendor_small_split_payment.PAYEE_NAME,
+    X001ba_a_vendor_small_split_payment.FIN_OBJ_CD_NM
