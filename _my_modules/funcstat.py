7fd787f6ee999121c923b804ddc358e60c049c3e
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-05-01 08:00:32 +0200

    Add list stat function. Add tuple stat function.

--- _my_modules/funcstat.py
+++ _my_modules/funcstat.py
@@ -77,3 +77,51 @@ def stat_highest_value(o_cursor, s_table, s_column, s_where=""):
     # print(s_sql)
     row = o_cursor.execute(s_sql).fetchone()
     return row[0]
+
+
+def stat_list(o_cursor, s_table, s_column, s_where=""):
+    """
+    Function to return the statistic mode for a column
+    :param o_cursor: Database cursor
+    :param s_table: Table in database
+    :param s_column: Column to return
+    :param s_where: Where clause
+    :return: List
+    """
+
+    # Convert the table column into a list and the calculate the mode
+    s_sql = "SELECT " + s_column
+    s_sql += " FROM " + s_table
+    s_sql += " WHERE " + s_column + " Is Not Null"
+    if s_where != "":
+        s_sql += " And " + s_where
+    s_sql += " ;"
+    # print(s_sql)
+    a_list = []
+    for row in o_cursor.execute(s_sql).fetchall():
+        a_list.append(row[0])
+    return a_list
+
+
+def stat_tuple(o_cursor, s_table, s_column, s_where=""):
+    """
+    Function to return the statistic mode for a column
+    :param o_cursor: Database cursor
+    :param s_table: Table in database
+    :param s_column: Column to return
+    :param s_where: Where clause
+    :return: List
+    """
+
+    # Convert the table column into a list and the calculate the mode
+    s_sql = "SELECT " + s_column
+    s_sql += " FROM " + s_table
+    s_sql += " WHERE " + s_column + " Is Not Null"
+    if s_where != "":
+        s_sql += " And " + s_where
+    s_sql += " ;"
+    # print(s_sql)
+    a_list = []
+    for row in o_cursor.execute(s_sql).fetchall():
+        a_list.append(row[0])
+    return tuple(a_list)
