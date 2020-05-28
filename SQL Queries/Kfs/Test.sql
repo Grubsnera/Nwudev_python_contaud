5233af309a408e98049e9deb39b1a3fb380aa916
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-05-28 11:07:52 +0200

    Test queries.

---
+++ SQL Queries/Kfs/Test.sql
@@ -0,0 +1,18 @@
+﻿Select
+    X000_GL_trans.CALC_COST_STRING,
+    X000_GL_trans.ORG_NM,
+    X000_GL_trans.ACCOUNT_NM,
+    X000_GL_trans.FIN_OBJ_CD_NM,
+    Max(X000_GL_trans.TRANSACTION_DT) As Max_TRANSACTION_DT,
+    Total(X000_GL_trans.CALC_AMOUNT) As Total_CALC_AMOUNT
+From
+    X000_GL_trans
+Group By
+    X000_GL_trans.CALC_COST_STRING,
+    X000_GL_trans.ORG_NM,
+    X000_GL_trans.ACCOUNT_NM,
+    X000_GL_trans.FIN_OBJ_CD_NM
+Order By
+    X000_GL_trans.ORG_NM,
+    X000_GL_trans.ACCOUNT_NM,
+    X000_GL_trans.FIN_OBJ_CD_NM
