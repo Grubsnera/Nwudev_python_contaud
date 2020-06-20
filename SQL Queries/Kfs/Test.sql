b46b16a2b1f7daed4a0d88a3d4c121078a079494
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-06-20 07:52:16 +0200

    Test scripts.

--- SQL Queries/Kfs/Test.sql
+++
@@ -1,8 +0,0 @@
-﻿Select
-    X000_GL_trans.FDOC_NBR,
-    Count(X000_GL_trans.CALC_AMOUNT) As AMT_COUNT,
-    Total(X000_GL_trans.CALC_AMOUNT) As AMT_TOTAL
-From
-    X000_GL_trans
-Group By
-    X000_GL_trans.FDOC_NBR
