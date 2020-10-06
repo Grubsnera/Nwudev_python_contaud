ab9e169a558cca7f7e11d43d7c410b4bc4784bea
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2020-10-06 09:11:53 +0200

    Add / change sql scripts.

---
+++ SQL Queries/Kfs/Test.sql
@@ -0,0 +1,9 @@
+﻿Select
+    X000_GL_trans.UNIV_FISCAL_YR,
+    X000_GL_trans.UNIV_FISCAL_PRD_CD,
+    Count(X000_GL_trans.FDOC_NBR) As Count_FDOC_NBR
+From
+    X000_GL_trans
+Group By
+    X000_GL_trans.UNIV_FISCAL_YR,
+    X000_GL_trans.UNIV_FISCAL_PRD_CD
---
+++ SQL Queries/Kfs_test_gl_transaction/Org_acc_proffee_summ.sql
@@ -0,0 +1,9 @@
+﻿Select
+    X001_gl_professional_fee_pay.ORG_NM,
+    X001_gl_professional_fee_pay.ACCOUNT_NM,
+    Total(X001_gl_professional_fee_pay.CALC_AMOUNT) As Total_CALC_AMOUNT
+From
+    X001_gl_professional_fee_pay
+Group By
+    X001_gl_professional_fee_pay.ORG_NM,
+    X001_gl_professional_fee_pay.ACCOUNT_NM
