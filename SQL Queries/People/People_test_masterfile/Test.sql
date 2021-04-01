962f7f374ba3934ec05367bbaf5e09b5ea56cf3e
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-04-01 10:45:49 +0200

    SQL Test queries.

--- SQL Queries/People/People_test_masterfile/Test.sql
+++
@@ -1,7 +0,0 @@
-﻿Select
-    X007dx_leavecode_invalid.Person_type,
-    Count(X007dx_leavecode_invalid.Workdays) As Count_Workdays
-From
-    X007dx_leavecode_invalid
-Group By
-    X007dx_leavecode_invalid.Person_type
--- SQL Queries/Vss_fee_test_bursary/Test.sql
+++ SQL Queries/Vss_fee_test_bursary/Test.sql
@@ -1,5 +1,15 @@
 ﻿Select
-    x000t.STUDENT
+    x001b.FINAIDCODE,
+    x001b.QUAL_TYPE,
+    Total(x001b.AMOUNT_TOTAL) As AMOUNT_TOTAL,
+    Count(x001b.TRAN_COUNT) As STUD_COUNT
 From
-    X000_Transaction x000t Inner Join
-    X000_Student x000s On x000s.KSTUDBUSENTID = x000t.STUDENT
+    X001aa_Bursary_student_value x001b
+Where
+    x001b.FINAIDCODE Is Not Null
+Group By
+    x001b.FINAIDCODE,
+    x001b.QUAL_TYPE
+Having
+    Total(x001b.AMOUNT_TOTAL) <> 0 And
+    Count(x001b.TRAN_COUNT) > 2
