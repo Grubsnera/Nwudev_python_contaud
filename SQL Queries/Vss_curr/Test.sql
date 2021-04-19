92f86535266e7d6381f0d42eb5990d9529935161
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-04-19 13:46:19 +0200

    SQL Queries

---
+++ SQL Queries/Vss_curr/Test.sql
@@ -0,0 +1,15 @@
+﻿Select
+    x010s.FBUSENTID,
+    x010s.AMOUNT,
+    x010s.TRANSCODE,
+    x010s.FDEBTCOLLECTIONSITE,
+    x010s.TRANSDATE
+From
+    X010_Studytrans x010s
+Where
+    (x010s.TRANSCODE = '214' And
+        x010s.FDEBTCOLLECTIONSITE = -1 And
+        x010s.TRANSDATE Like ('2021-01%')) Or
+    (x010s.TRANSCODE = '214' And
+        x010s.FDEBTCOLLECTIONSITE = -1 And
+        x010s.TRANSDATE Like ('2021-02%'))
