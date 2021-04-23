ba73affd00cde367c1b3d375f36a9f439f4badf3
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-04-23 07:30:45 +0200

    SQL Queries.

--- SQL Queries/Vss_fee_test_bursary/Test.sql
+++ SQL Queries/Vss_fee_test_bursary/Test.sql
@@ -1,15 +1,16 @@
 ﻿Select
+    x001b.STUDENT,
     x001b.FINAIDCODE,
+    x001b.FINAIDNAME,
     x001b.QUAL_TYPE,
-    Total(x001b.AMOUNT_TOTAL) As AMOUNT_TOTAL,
-    Count(x001b.TRAN_COUNT) As STUD_COUNT
+    x001b.AMOUNT_TOTAL,
+    x001b.TRAN_COUNT,
+    x001b1.AMOUNT
 From
-    X001aa_Bursary_student_value x001b
+    X001aa_Bursary_student_value x001b Left Join
+    X001ac_Bursary_mode x001b1 On x001b1.FINAIDCODE = x001b.FINAIDCODE
+            And x001b1.QUAL_TYPE = x001b.QUAL_TYPE
 Where
-    x001b.FINAIDCODE Is Not Null
-Group By
-    x001b.FINAIDCODE,
-    x001b.QUAL_TYPE
-Having
-    Total(x001b.AMOUNT_TOTAL) <> 0 And
-    Count(x001b.TRAN_COUNT) > 2
+    x001b.FINAIDCODE Is Not Null And
+    x001b.QUAL_TYPE Is Not Null And
+    x001b.AMOUNT_TOTAL <> 0
