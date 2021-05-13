e45c7a585ef728c035fde2f6d36a3e166838d83b
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-05-13 17:31:36 +0200

    Change to system opening balance.

--- A000_nwuiaca.py
+++ A000_nwuiaca.py
@@ -852,7 +852,8 @@ class RunTest(Thread):
                             try:
                                 # C200_report_studdeb_recon.report_studdeb_recon()
                                 # 2021 balances
-                                C200_report_studdeb_recon.report_studdeb_recon(65676774.13, 61655697.80, 41648563.00, "curr")
+                                C200_report_studdeb_recon.report_studdeb_recon(0, 0, 0, "curr")
+                                # C200_report_studdeb_recon.report_studdeb_recon(65676774.13, 61655697.80, 41648563.00, "curr")
                                 # 2020 balances
                                 # C200_report_studdeb_recon.report_studdeb_recon(48501952.09, -12454680.98, 49976048.39, "curr")
                                 # 2019 balances C200_report_studdeb_recon.report_studdeb_recon(66561452.48, -18340951.06, 39482933.18, "prev")
