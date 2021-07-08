2fb50fe6d97a26a3c149af2e2557522ed1b6b685
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-07-08 18:57:39 +0200

    Test queries.

---
+++ SQL Queries/People/Count_grade.sql
@@ -0,0 +1,13 @@
+﻿Select
+    p.assignment_category,
+    p.user_person_type,
+    p.employee_category,
+    p.grade,
+    Count(p.assignment_id) As COUNT_GRADE
+From
+    X000_PEOPLE p
+Group By
+    p.assignment_category,
+    p.user_person_type,
+    p.employee_category,
+    p.grade
