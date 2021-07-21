6aa0b65a395a28a9ae3b4c6711b3d18daab96d88
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-07-21 13:24:57 +0200

    Test queries.

--- SQL Queries/Vss_fee_test_bursary/Test.sql
+++ SQL Queries/Vss_fee_test_bursary/Test.sql
@@ -1,13 +1,15 @@
 ﻿Select
-    b.STUDENT,
-    b.BURS_COUNT,
-    b.AMOUNT_TOTAL,
-    b.TRAN_COUNT,
-    s.KSTUDBUSENTID As STUDENT_REGISTERED,
-    e.STUDENT As STUDENT_EMPLOYEE,
-    x000s.STUDENT As STUDENT_STAFF_DISC
+    a.STUDENT,
+    a.BURS_COUNT,
+    a.AMOUNT_TOTAL,
+    x001b.FINAIDCODE,
+    x001b.FINAIDNAME,
+    x001b.QUAL_TYPE,
+    x001b.AMOUNT_TOTAL As AMOUNT_BURSARY,
+    x001b.TRAN_COUNT
 From
-    X001aa_Bursary_student_value_total b Left Join
-    X000_Student s On s.KSTUDBUSENTID = b.STUDENT Left Join
-    X000_Student_employee e On e.STUDENT = b.STUDENT Left Join
-    X000_Student_staff x000s On x000s.STUDENT = b.STUDENT
+    X001aa_Bursary_student_value_total a Inner Join
+    X001aa_Bursary_student_value x001b On a.STUDENT = x001b.STUDENT
+Where
+    a.AMOUNT_TOTAL < -200000 And
+    x001b.AMOUNT_TOTAL != 0
