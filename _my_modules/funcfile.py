3a41ed85a29fc2b47e579f8d57e68abce5f0f1f5
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-07-21 13:23:52 +0200

    Add get field value function to obtain a field value from any database.

--- _my_modules/funcfile.py
+++ _my_modules/funcfile.py
@@ -11,11 +11,63 @@ import datetime
 from _my_modules import funcconf
 from _my_modules import funcsys
 
+# INDEX
+"""
+Function to obtain a field value from any table
+Function to create log file (writelog)
+Function to delete a file (file_delete)
+"""
+
+
+def get_field_value(o_cursor, s_table='', s_field='', s_where=''):
+    """
+    Function to obtain a field value from any table.
+
+    :param o_cursor: object: Database cursor
+    :param s_table: str: Table to query
+    :param s_field: str: Field value to return
+    :param s_where: str: Where clause
+    :return: str: Value of lookup field
+    """
+
+    # IMPORT OWN MODULES
+    from _my_modules import funcfile
+
+    # DECLARE VARIABLES
+    l_debug: bool = False
+
+    # SET PREVIOUS FINDINGS
+    if l_debug:
+        print("Field value lookup...")
+    s_sql = """
+        Select
+            t.%FIELD%
+        FROM
+            %TABLE% t
+        WHERE
+            t.%WHERE%
+        ;"""
+    s_sql = s_sql.replace("%TABLE%", s_table)
+    s_sql = s_sql.replace("%FIELD%", s_field)
+    s_sql = s_sql.replace("%WHERE%", s_where)
+    if l_debug:
+        print(s_sql)
+    t_return = o_cursor.execute(s_sql).fetchone()
+    print(t_return)
+    if t_return is not None:
+        s_return = str(t_return[0])
+    else:
+        s_return = "UNKNOWN "
+    funcfile.writelog("%t OBTAIN TABLE FIELD VALUE: " + s_table + " " + s_field + " " + s_where)
+
+    return s_return
+
 
 def writelog(s_entry="\n", s_path="S:/Logs/",
              s_file="Python_log_" + datetime.datetime.now().strftime("%Y%m%d") + ".txt"):
     """
     Function to create log file
+
     :param s_entry: Log file entry
     :param s_path: Log file path
     :param s_file: Log file name
@@ -57,6 +109,8 @@ def writelog(s_entry="\n", s_path="S:/Logs/",
 
 def file_delete(s_path: str = "", s_file: str = ""):
     """
+    Function to delete a file.
+
     :param s_path: Log file path
     :param s_file: Log file name
     :return: bool: True - deleted or False
