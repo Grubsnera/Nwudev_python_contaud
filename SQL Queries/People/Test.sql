0a43abb23b54ec9b02b3d37f8832b783321984dd
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-08-24 06:50:29 +0200

    Test queries.

--- SQL Queries/People/Test.sql
+++ SQL Queries/People/Test.sql
@@ -1,13 +1,5 @@
 ﻿Select
     x000p.employee_number,
-    x000p.name_full,
-    x000p.preferred_name,
-    SubStr(x000p.name_last, 1, 2) As name_last,
-    x000p.employee_age
+    x000p.organization
 From
     X000_PEOPLE x000p
-Where
-    SubStr(x000p.name_last, 1, 2) >= 'JA' And
-    SubStr(x000p.name_last, 1, 2) <= 'LI' And
-    x000p.employee_age >= 35 And
-    x000p.location Like ('POT%')
--- SQL Queries/Vss_fee_test_bursary/Test.sql
+++ SQL Queries/Vss_fee_test_bursary/Test.sql
@@ -1,15 +1,9 @@
 ﻿Select
-    a.STUDENT,
-    a.BURS_COUNT,
-    a.AMOUNT_TOTAL,
-    x001b.FINAIDCODE,
-    x001b.FINAIDNAME,
-    x001b.QUAL_TYPE,
-    x001b.AMOUNT_TOTAL As AMOUNT_BURSARY,
-    x001b.TRAN_COUNT
+    x000s.KRELATIONSHIPTYPECODEID,
+    x000s.REL_TYPE,
+    Count(x000s.KSTUDBUSENTID) As Count_KSTUDBUSENTID
 From
-    X001aa_Bursary_student_value_total a Inner Join
-    X001aa_Bursary_student_value x001b On a.STUDENT = x001b.STUDENT
-Where
-    a.AMOUNT_TOTAL < -200000 And
-    x001b.AMOUNT_TOTAL != 0
+    X000_Student_relationship x000s
+Group By
+    x000s.KRELATIONSHIPTYPECODEID,
+    x000s.REL_TYPE
