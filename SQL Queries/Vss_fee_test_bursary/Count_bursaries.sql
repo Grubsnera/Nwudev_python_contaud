d6f83e1720a8d92d96c3a6f020f8bb7854f12901
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-08-24 06:51:11 +0200

    Test queries.

---
+++ SQL Queries/Vss_fee_test_bursary/Count_bursaries.sql
@@ -0,0 +1,7 @@
+﻿Select
+    x000t.FINAIDCODE,
+    Total(x000t.AMOUNT) As Total_AMOUNT
+From
+    X000_Transaction x000t
+Group By
+    x000t.FINAIDCODE
---
+++ SQL Queries/Vss_fee_test_bursary/Count_bursaries_2.sql
@@ -0,0 +1,9 @@
+﻿Select
+    x000t.FINAIDCODE,
+    x000t.FINAIDNAME,
+    Total(x000t.AMOUNT) As Total_AMOUNT
+From
+    X000_Transaction x000t
+Group By
+    x000t.FINAIDCODE,
+    x000t.FINAIDNAME
