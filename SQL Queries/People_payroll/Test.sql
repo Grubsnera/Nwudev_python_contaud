4e629a1a59b14b52fd2bc2eb8515569869a73b2c
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2021-09-24 07:50:24 +0200

    Test query.

---
+++ SQL Queries/People_payroll/Test.sql
@@ -0,0 +1,9 @@
+﻿Select
+    X000aa_element_list_curr.REPORTING_NAME,
+    X000aa_element_list_curr.ELEMENT_NAME,
+    Count(X000aa_element_list_curr.ASSIGNMENT_ID) As Count_ASSIGNMENT_ID
+From
+    X000aa_element_list_curr
+Group By
+    X000aa_element_list_curr.REPORTING_NAME,
+    X000aa_element_list_curr.ELEMENT_NAME
