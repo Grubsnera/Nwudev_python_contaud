9c73eb1d43fa896e35deaeb15ac41325d79bac9b
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-01-27 14:40:45 +0200

    Add student debtor query.

---
+++ SQL Queries/Kfs_curr/Stud_debtors.sql
@@ -0,0 +1,12 @@
+﻿Select
+    X000_GL_trans.ACCOUNT_NBR,
+    X000_GL_trans.ACCOUNT_NM,
+    Total(X000_GL_trans.CALC_AMOUNT) As Total_CALC_AMOUNT
+From
+    X000_GL_trans
+Where
+    (X000_GL_trans.FIN_OBJECT_CD = '7551') Or
+    (X000_GL_trans.FIN_OBJECT_CD = '7552') Or
+    (X000_GL_trans.FIN_OBJECT_CD = '7553')
+Group By
+    X000_GL_trans.ACCOUNT_NBR
