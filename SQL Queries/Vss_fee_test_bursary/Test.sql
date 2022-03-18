11b6c5488c98d4e280eb7356583ed755c1c1ac13
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-03-18 08:42:06 +0200

    Test queries.

--- SQL Queries/Vss_fee_test_bursary/Test.sql
+++ SQL Queries/Vss_fee_test_bursary/Test.sql
@@ -1,9 +1,13 @@
-﻿Select
-    x000s.KRELATIONSHIPTYPECODEID,
-    x000s.REL_TYPE,
-    Count(x000s.KSTUDBUSENTID) As Count_KSTUDBUSENTID
+﻿    Select
+    BU.STUDENT,
+    ST.EMP_NAME_FULL,
+    ST.EMP_PERSON_TYPE,
+    BU.FINAIDCODE,
+    BU.FINAIDNAME,
+    BU.LEVY_CATEGORY,
+    BU.AMOUNT_TOTAL,
+    SD.TRAN_VALUE AS STAFF_DISC
 From
-    X000_Student_relationship x000s
-Group By
-    x000s.KRELATIONSHIPTYPECODEID,
-    x000s.REL_TYPE
+    X001_Bursary_student_value BU Inner Join
+    X001_Student_employee ST On ST.KSTUDBUSENTID = BU.STUDENT Left Join
+    X001_Bursary_student_value_staffdisc SD On SD.STUDENT = BU.STUDENT
