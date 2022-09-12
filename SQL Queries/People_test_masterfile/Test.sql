f2a7bf43d02604e103fa39ad9b1d782514e42b29
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-09-12 11:16:02 +0200

    Test queries.

--- SQL Queries/People_test_masterfile/Test.sql
+++ SQL Queries/People_test_masterfile/Test.sql
@@ -1,17 +1,29 @@
 ﻿Select
-    X007dab_employee_leave_code_invalid.ASSIGNMENT_CATEGORY,
-    X007dab_employee_leave_code_invalid.EMPLOYEE_CATEGORY,
-    X007dab_employee_leave_code_invalid.PERIOD,
-    X007dab_employee_leave_code_invalid.WORKDAYS,
-    X007dab_employee_leave_code_invalid.GRADE,
-    X007dab_employee_leave_code_invalid.LEAVE_CODE,
-    Count(X007dab_employee_leave_code_invalid.EMPLOYEE_NUMBER) As Total_EMPLOYEE_NUMBER
+    s.employee_number,
+    s.person_id,
+    s.name_address,
+    s.user_person_type,
+    s.test,
+    s.marital_status,
+    s.married,
+    s.date_started,
+    s.spouse_insurance_status,
+    s.spouse_age,
+    s.person_extra_info_id,
+    s.spouse_number,
+    s.spouse_address,
+    s.spouse_date_of_birth,
+    s.spouse_national_identifier,
+    s.spouse_passport,
+    s.spouse_start_date,
+    s.spouse_end_date,
+    s.spouse_create_date,
+    s.spouse_created_by,
+    s.spouse_update_date,
+    s.spouse_updated_by,
+    s.spouse_update_login
 From
-    X007dab_employee_leave_code_invalid
-Group By
-    X007dab_employee_leave_code_invalid.ASSIGNMENT_CATEGORY,
-    X007dab_employee_leave_code_invalid.EMPLOYEE_CATEGORY,
-    X007dab_employee_leave_code_invalid.PERIOD,
-    X007dab_employee_leave_code_invalid.WORKDAYS,
-    X007dab_employee_leave_code_invalid.GRADE,
-    X007dab_employee_leave_code_invalid.LEAVE_CODE
+    X009_people_spouse_all s
+Where
+    s.married Is Null And
+    s.spouse_insurance_status > 0
