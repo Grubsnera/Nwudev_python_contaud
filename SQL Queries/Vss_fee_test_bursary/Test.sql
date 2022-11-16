19e506d2d00f3f01178c66ed0a3c275b4f704329
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-11-16 11:46:49 +0200

    Update test queries.
    Update test queries.

--- SQL Queries/Vss_fee_test_bursary/Test.sql
+++ SQL Queries/Vss_fee_test_bursary/Test.sql
@@ -1,13 +1,15 @@
-﻿    Select
-    BU.STUDENT,
-    ST.EMP_NAME_FULL,
-    ST.EMP_PERSON_TYPE,
-    BU.FINAIDCODE,
-    BU.FINAIDNAME,
-    BU.LEVY_CATEGORY,
-    BU.AMOUNT_TOTAL,
-    SD.TRAN_VALUE AS STAFF_DISC
+﻿Select
+    tran.STUDENT As student,
+    Total(tran.AMOUNT) As total_burs,
+    Total(loan.AMOUNT) As total_loan,
+    Total(inte.AMOUNT) As total_internal,
+    Total(exte.AMOUNT) As total_external,
+    Total(trus.AMOUNT) As total_trust    
 From
-    X001_Bursary_student_value BU Inner Join
-    X001_Student_employee ST On ST.KSTUDBUSENTID = BU.STUDENT Left Join
-    X001_Bursary_student_value_staffdisc SD On SD.STUDENT = BU.STUDENT
+    X000_Transaction tran Left Join
+    X000_Transaction loan On loan.STUDENT = tran.STUDENT And loan.SOURCE = 'BURSARY-LOAN SCHEMA' Left Join
+    X000_Transaction inte On inte.STUDENT = tran.STUDENT And inte.SOURCE = 'UNIVERSITY FUND' Left Join
+    X000_Transaction exte On exte.STUDENT = tran.STUDENT And exte.SOURCE = 'EXTERNAL FUND' Left join
+    X000_Transaction trus On trus.STUDENT = tran.STUDENT And trus.SOURCE = 'DONATE/TRUST FUND'
+Group By
+    tran.STUDENT
