679e0538cac794fead5af92a2cfd4c25f81408f4
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-11-18 10:50:37 +0200

    Update B002_kfs_lists_xdev.py
    Move the importing of lookup tables to A005.

--- B002_kfs_lists_xdev.py
+++ B002_kfs_lists_xdev.py
@@ -56,30 +56,6 @@ funcfile.writelog("OPEN DATABASE: " + so_file)
 so_curs.execute("ATTACH DATABASE 'W:/People/People.sqlite' AS 'PEOPLE'")
 funcfile.writelog("%t ATTACH DATABASE: PEOPLE.SQLITE")
 
-"""*****************************************************************************
-DO NOT DELETE
-IMPORT OWN LOOKUPS
-DO NOT DELETE
-*****************************************************************************"""
-# IMPORT THE X000_OWN_KFS_LOOKUPS TABLE
-print("Import own lookups...")
-tb_name = "X000_Own_kfs_lookups"
-so_curs.execute("DROP TABLE IF EXISTS " + tb_name)
-so_curs.execute("CREATE TABLE " + tb_name + "(LOOKUP TEXT,LOOKUP_CODE TEXT,LOOKUP_DESCRIPTION TEXT)")
-s_cols = ""
-co = open(ed_path + "001_own_kfs_lookups.csv", newline=None)
-co_reader = csv.reader(co)
-for row in co_reader:
-    if row[0] == "LOOKUP":
-        continue
-    else:
-        s_cols = "INSERT INTO " + tb_name + " VALUES('" + row[0] + "','" + row[1] + "','" + row[2] + "')"
-        so_curs.execute(s_cols)
-so_conn.commit()
-# Close the imported data file
-co.close()
-funcfile.writelog("%t IMPORT TABLE: " + tb_name)
-
 """ ****************************************************************************
 BEGIN OF SCRIPT
 *****************************************************************************"""
