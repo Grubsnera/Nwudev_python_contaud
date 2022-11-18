a4424674951cf0295c1f93a8b9891eef4c627201
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-11-18 10:48:30 +0200

    SQL Test queries

---
+++ SQL Queries/Vss_fee_test_bursary/AR - Discontinue result.sql
@@ -0,0 +1,7 @@
+﻿Select
+    X000_Student.RESULT,
+    Count(X000_Student.KSTUDBUSENTID) As Count_KSTUDBUSENTID
+From
+    X000_Student
+Group By
+    X000_Student.RESULT
---
+++ SQL Queries/Vss_fee_test_bursary/X005aaa_list1.sql
@@ -0,0 +1,24 @@
+﻿Select
+    burs.student,
+    burs.total_burs,
+    burs.total_loan,
+    burs.total_external,
+    burs.total_internal,
+    burs.total_research,
+    burs.total_trust,
+    burs.total_other,
+    burs.staff_discount,
+    burs.active,
+    burs.levy_category,
+    burs.qualification,
+    burs.qualification_type,
+    burs.discontinue_date,
+    burs.discontinue_result,
+    burs.discontinue_reason
+From
+    X001_Bursary_summary_student burs
+Where
+    (burs.total_burs <> 0 And
+        burs.active = 'INACTIVE') Or
+    (burs.total_burs <> 0 And
+        burs.discontinue_date Is Not Null)
