5b8905c0df834c47c60e99e9a0a9762d15b95ac8
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2022-11-21 15:26:05 +0200

    SQL Test queries

---
+++ SQL Queries/Vss_fee_test_bursary/X005baa_list1.sql
@@ -0,0 +1,24 @@
+﻿Select
+    burs.student,
+    burs.total_burs,
+    burs.total_loan,
+    burs.staff_discount,
+    rela.EMP_NAME_FULL,
+    rela.EMP_PERSON_TYPE,
+    rela.KRELATEDBUSINESSENTITYID,
+    rela.REL_TYPE,
+    rela.REL_NAME_FULL,
+    rela.REL_PERSON_TYPE,
+    burs.active,
+    burs.levy_category,
+    burs.enrol_category,
+    burs.qualification,
+    burs.qualification_type,
+    burs.discontinue_date,
+    burs.discontinue_result,
+    burs.discontinue_reason
+From
+    X001_Bursary_summary_student burs Left Join
+    X000_Student_relationship rela On rela.KSTUDBUSENTID = burs.student
+Where
+    burs.staff_discount <> 0
