ec649e3ec4a3fd4c6507dd109ef2dbebd28b9cd5
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-02-17 14:06:11 +0200

    SQL Test scripts
    SQL Test scripts.

---
+++ SQL Queries/Oracle_hr/Payslip_part0_prr_prrv_piv.sql
@@ -0,0 +1,14 @@
+﻿Select
+    piv.UOM,
+    piv.NAME,
+    prr.RUN_RESULT_ID,
+    prrv.RESULT_VALUE
+From
+    HR.PAY_RUN_RESULTS prr,
+    HR.PAY_RUN_RESULT_VALUES prrv,
+    HR.PAY_INPUT_VALUES_F piv
+Where
+    prr.RUN_RESULT_ID = prrv.RUN_RESULT_ID And
+    piv.INPUT_VALUE_ID = prrv.INPUT_VALUE_ID And
+    piv.UOM = 'M' And
+    piv.NAME = 'Pay Value'
