90a70d41be80f148a3095df2a2a3982d3006edb9
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-02 11:08:23 +0200

    Delete old test queries
    Delete old test queries.

--- SQL Queries/Kfs_curr/Test.sql
+++
@@ -1,9 +0,0 @@
-﻿Select
-    X001aa_Report_payments.VENDOR_TYPE_CALC,
-    Total(X001aa_Report_payments.NET_PMT_AMT) As Total_NET_PMT_AMT
-From
-    X001aa_Report_payments
-Where
-    X001aa_Report_payments.NET_PMT_AMT <= 5000
-Group By
-    X001aa_Report_payments.VENDOR_TYPE_CALC
