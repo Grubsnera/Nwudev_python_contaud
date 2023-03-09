ffa40a124691b132ae71513b9069c11705523bba
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-09 13:59:22 +0200

    Update A000_nwuiaca.py
    Add 2023 opening balances.

--- A000_nwuiaca.py
+++ A000_nwuiaca.py
@@ -1025,8 +1025,11 @@ class RunTest(Thread):
                         if funcdate.today_dayname() in "MonTueWedThuFri":
                             try:
                                 # C200_report_studdeb_recon.report_studdeb_recon()
+                                # 2023 balances
+                                # C200_report_studdeb_recon.report_studdeb_recon(0, 0, 0, "curr")
+                                C200_report_studdeb_recon.report_studdeb_recon(43861754.51, 19675773.32, 14658226.87, "curr")
                                 # 2022 balances
-                                C200_report_studdeb_recon.report_studdeb_recon(0, 0, 0, "curr")
+                                # C200_report_studdeb_recon.report_studdeb_recon(0, 0, 0, "curr")
                                 # C200_report_studdeb_recon.report_studdeb_recon(40960505.33, 6573550.30, 29005168.76, "curr")
                                 # 2021 balances
                                 # C200_report_studdeb_recon.report_studdeb_recon(65676774.13, 61655697.80, 41648563.00, "curr")
