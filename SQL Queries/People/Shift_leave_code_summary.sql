172c315837777250368d1bc6525fe8d3c4ccb356
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-10 09:06:44 +0200

    SQL Test scripts
    SQL Test scripts.

---
+++ SQL Queries/People/Shift_leave_code_summary.sql
@@ -0,0 +1,9 @@
+﻿Select
+    X000_PEOPLE.type_of_shift,
+    X000_PEOPLE.leave_code,
+    Count(X000_PEOPLE.people_update_by) As Count_people
+From
+    X000_PEOPLE
+Group By
+    X000_PEOPLE.type_of_shift,
+    X000_PEOPLE.leave_code
