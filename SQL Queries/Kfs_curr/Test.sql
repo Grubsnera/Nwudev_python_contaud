968e718ec219a4ec586b60474937088a8e1cde46
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-10 15:13:59 +0200

    Test SQL queries.
    Test SQL queries.

---
+++ SQL Queries/Kfs_curr/Test.sql
@@ -0,0 +1,10 @@
+﻿Select
+    acc.VENDOR_ID,
+    acc.VENDOR_NAME,
+    Count(acc.EDOC) As Count_EDOC
+From
+    X001ad_Report_payments_accroute acc
+Where
+    acc.ACC_COST_STRING Like '%9641'
+Group By
+    acc.VENDOR_ID
