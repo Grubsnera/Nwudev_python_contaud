ad119fe0c7ee41806c774e3a6c6f582aefbef2cc
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-14 10:39:40 +0200

    Foreign payments
    Test SQL script to identify foreign payments from the payroll.

---
+++ SQL Queries/People_payroll/Foreign_payments_summary.sql
@@ -0,0 +1,10 @@
+﻿Select
+    pfp.EMPLOYEE_NUMBER,
+    Max(pfp.EFFECTIVE_DATE) As LAST_PAYMENT_DATE,
+    Count(pfp.RUN_RESULT_ID) As COUNT_PAYMENTS
+From
+    X000aa_payroll_history_curr pfp
+Where
+    pfp.ELEMENT_NAME Like 'NWU Foreign Payment%'
+Group By
+    pfp.EMPLOYEE_NUMBER
