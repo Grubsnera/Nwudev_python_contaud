22e342c248dce5bf745438e2f872295fb9290782
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-14 10:40:18 +0200

    Update C001_people_test_masterfile.py
    Add a foreign payments list of employees from the payroll.

--- C001_people_test_masterfile.py
+++ C001_people_test_masterfile.py
@@ -37,6 +37,7 @@ TEST ID NUMBER DUPLICATE (V1.0.8)
 TEST ADDRESS DUPLICATE (In development)
 
 PASSPORT NUMBER MASTER FILE
+FOREIGN PAYMENTS MASTER FILE
 TEST PASSPORT NUMBER BLANK (V1.0.8)
 TEST PASSPORT NUMBER DUPLICATE (V1.0.8)
 TEST FOREIGN EMPLOYEE WORK PERMIT EXPIRED (X003ex)(V2.0.5)
@@ -1823,6 +1824,39 @@ def people_test_masterfile():
     so_curs.execute(s_sql)
     so_conn.commit()
     funcfile.writelog("%t BUILD TABLE: "+sr_file)
+
+    """ ****************************************************************************
+    FOREIGN PAYMENTS MASTER FILE
+    *****************************************************************************"""
+
+    # BUILD TABLE WITH PAYROLL FOREIGN PAYMENTS FOR THE PREVIOUS MONTH
+    print("Obtain master list of foreign employee payments...")
+    sr_file = "X003_foreign_payments"
+    s_sql = "CREATE TABLE "+sr_file+" AS " + """
+    Select
+        pfp.EMPLOYEE_NUMBER,
+        Max(pfp.EFFECTIVE_DATE) As LAST_PAYMENT_DATE,
+        Count(pfp.RUN_RESULT_ID) As COUNT_PAYMENTS
+    From
+        PAYROLL.X000aa_payroll_history_%PERIOD% pfp
+    Where
+        pfp.ELEMENT_NAME Like 'NWU Foreign Payment%' And
+        pfp.EFFECTIVE_DATE Like '%PREVMONTH%%'
+    Group By
+        pfp.EMPLOYEE_NUMBER    
+    ;"""
+    so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
+    s_sql = s_sql.replace("%PREVMONTH%", funcdate.prev_monthend()[0:7])
+    if funcdate.prev_monthend()[0:4] == funcdate.prev_year:
+        s_sql = s_sql.replace("%PERIOD%", 'prev')
+    else:
+        s_sql = s_sql.replace("%PERIOD%", 'curr')
+    if l_debug:
+        print(s_sql)
+    so_curs.execute(s_sql)
+    funcfile.writelog("%t BUILD TABLE: "+sr_file)
+    if l_debug:
+        so_conn.commit()
 
     """ ****************************************************************************
     TEST PASSPORT NUMBER BLANK
