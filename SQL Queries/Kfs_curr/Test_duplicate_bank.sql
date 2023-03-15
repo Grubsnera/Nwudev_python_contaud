164d30d24290732f286a2298da9aaaa34b186ae7
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-15 12:34:07 +0200

    SQL Test query
    SQL Test query to list duplicate bank accounts.

---
+++ SQL Queries/Kfs_curr/Test_duplicate_bank.sql
@@ -0,0 +1,13 @@
+﻿Select
+    X001aa_Report_payments.VENDOR_BANK_NR,
+    X001aa_Report_payments.VENDOR_TYPE_CALC,
+    SubStr(X001aa_Report_payments.VENDOR_ID, 1, 8) As VENDOR_ID,
+    X001aa_Report_payments.VENDOR_NAME,
+    Count(X001aa_Report_payments.EDOC) As Count_EDOC
+From
+    X001aa_Report_payments
+Group By
+    X001aa_Report_payments.VENDOR_BANK_NR,
+    X001aa_Report_payments.VENDOR_TYPE_CALC,
+    SubStr(X001aa_Report_payments.VENDOR_ID, 1, 8),
+    X001aa_Report_payments.VENDOR_NAME
