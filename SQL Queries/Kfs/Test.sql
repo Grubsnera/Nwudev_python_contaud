b8ad8ed5ca82a6c13a41529d85afcd118cc510a9
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-15 12:33:35 +0200

    SQL Test queries
    SQL Test queries.

--- SQL Queries/Kfs/Test.sql
+++ SQL Queries/Kfs/Test.sql
@@ -1,7 +1,9 @@
 ﻿Select
-    X000_Vendor.VNDR_TYP_CD,
-    Count(X000_Vendor.VENDOR_ID) As Count_VENDOR_ID
+    X000_Vendor.VEND_BRANCH,
+    X000_Vendor.VEND_BANK,
+    Count(X000_Vendor.VNDR_INACTV_REAS_CD) As Count_VNDR_INACTV_REAS_CD
 From
     X000_Vendor
 Group By
-    X000_Vendor.VNDR_TYP_CD
+    X000_Vendor.VEND_BRANCH,
+    X000_Vendor.VEND_BANK
