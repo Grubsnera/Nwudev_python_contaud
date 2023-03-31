c3f2aff30bbcbca69a4fbf0f42635a3a9e14d124
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-31 11:42:27 +0200

    SQL Test queries.

---
+++ SQL Queries Yolandie/Sqlite_people_conflict/List of interests.sql
@@ -0,0 +1,10 @@
+﻿Select
+    X001_declarations_curr.DECLARATION_ID,
+    X001_declarations_curr.EMPLOYEE,
+    X001_declarations_curr.EMP_SURNAME,
+    X002_interests_curr.INTEREST_ID,
+    X002_interests_curr.EMP_SURNAME As EMP_SURNAME1,
+    X002_interests_curr.ENTITY_NAME
+From
+    X001_declarations_curr Inner Join
+    X002_interests_curr On X002_interests_curr.DECLARATION_ID = X001_declarations_curr.DECLARATION_ID
