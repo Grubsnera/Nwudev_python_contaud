5dc73ce507e83be7edc86654a6e3afcaee85ab8b
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-03-31 11:46:36 +0200

    SQL Queries to list local and foreign employees.

---
+++ SQL Queries/People/People_employee_foreign.sql
@@ -0,0 +1,14 @@
+﻿Select
+    pe.employee_number,
+    pe.name_full,
+    pe.is_foreign,
+    pe.nationality,
+    pe.nationality_passport,
+    pe.national_identifier,
+    pe.passport,
+    pe.permit,
+    pe.permit_expire
+From
+    X000_PEOPLE pe
+Where
+    pe.is_foreign = 'Y'
