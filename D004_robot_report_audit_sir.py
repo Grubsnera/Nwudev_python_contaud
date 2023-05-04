41849c6852c9d647bb4045af234509b943bed4bb
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-05-04 09:12:05 +0200

    Update D004_robot_report_audit_sir.py
    Deactivate debugging.

--- D004_robot_report_audit_sir.py
+++ D004_robot_report_audit_sir.py
@@ -44,7 +44,7 @@ def robot_report_audit_sir(s_year: str = "", s_type: str = "", s_name: str = "",
     from datetime import datetime
 
     # DECLARE VARIABLES
-    l_debug: bool = True
+    l_debug: bool = False
 
     """*************************************************************************
     ENVIRONMENT
