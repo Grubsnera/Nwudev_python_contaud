0320d994ccd9d602fe811edfd1fe0e43a7464c3c
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-05-24 11:32:02 +0200

    SQL test queries
    SQL test queries.

--- SQL Queries/Vss_general/test.sql
+++
@@ -1,8 +0,0 @@
-﻿Select
-    X001da_report_idlist.STUDENT
-From
-    X001da_report_idlist
-Where
-    (X001da_report_idlist.STUDENT = 28437500) Or
-    (X001da_report_idlist.STUDENT = 27239470) Or
-    (X001da_report_idlist.STUDENT = 24449989)
