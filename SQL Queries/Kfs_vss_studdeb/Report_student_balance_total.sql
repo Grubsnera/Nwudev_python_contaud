f7f37b8a18a8e34ef562fad5af5817a0f961f163
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-05-30 13:59:45 +0200

    SQL Test queries
    SQL Test queries.

---
+++ SQL Queries/Kfs_vss_studdeb/Report_student_balance_total.sql
@@ -0,0 +1,11 @@
+﻿Select
+    X002ab_vss_transort.STUDENT_VSS,
+    Total(X002ab_vss_transort.AMOUNT_VSS) As Total_AMOUNT_VSS,
+    Count(X002ab_vss_transort.BURSCODE_VSS) As Count_BURSCODE_VSS
+From
+    X002ab_vss_transort
+Where
+    X002ab_vss_transort.CAMPUS_VSS Like ("M%") And
+    X002ab_vss_transort.MONTH_VSS In ("00", "01")
+Group By
+    X002ab_vss_transort.STUDENT_VSS
---
+++ SQL Queries/Web_nwuia (server)/form_assignment_category_edit.sql
@@ -0,0 +1,13 @@
+﻿Select
+    acat.ia_assicate_auto,
+    acat.ia_assicate_name,
+    acat.ia_assicate_desc,
+    acat.ia_assicate_from,
+    acat.ia_assicate_to,
+    acat.ia_assicate_active,
+    acat.ia_assicate_private,
+    acat.ia_assicate_form
+From
+    ia_assignment_category acat
+Where
+    acat.ia_assicate_auto = 1
