fa95f273bba70289562e4a932fbe7f2aab1751d4
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-07-03 10:15:58 +0200

    Update C001_people_test_masterfile_xdev.py
    Minor changes.

--- C001_people_test_masterfile_xdev.py
+++ C001_people_test_masterfile_xdev.py
@@ -101,61 +101,6 @@ def people_test_masterfile_xdev():
     FOREIGN EMPLOYEES MASTER FILE
     *****************************************************************************"""
 
-    # BUILD SUMMARY TABLE WITH PAYROLL UIF PAYMENTS
-    print("Obtain master list of uif employee payments...")
-    sr_file = "X003_uif_payments"
-    s_sql = "CREATE TABLE "+sr_file+" AS " + """
-    Select
-        pfp.EMPLOYEE_NUMBER,
-        Max(pfp.EFFECTIVE_DATE) As LAST_PAYMENT_DATE,
-        Count(pfp.RUN_RESULT_ID) As COUNT_PAYMENTS
-    From
-        PAYROLL.X000aa_payroll_history_%PERIOD% pfp
-    Where
-        pfp.ELEMENT_NAME Like 'ZA_UIF_Employee_Contribution%'
-        -- pfp.PAYROLL_NAME Like 'UIF Employee%'
-    Group By
-        pfp.EMPLOYEE_NUMBER
-    ;"""
-    so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
-    if funcdate.prev_monthend()[0:4] == funcdate.prev_year:
-        s_sql = s_sql.replace("%PERIOD%", 'prev')
-    else:
-        s_sql = s_sql.replace("%PERIOD%", 'curr')
-    if l_debug:
-        print(s_sql)
-    so_curs.execute(s_sql)
-    funcfile.writelog("%t BUILD TABLE: "+sr_file)
-    if l_debug:
-        so_conn.commit()
-
-    # BUILD SUMMARY TABLE WITH PAYROLL FOREIGN PAYMENTS
-    print("Obtain master list of uif employee payments...")
-    sr_file = "X003_foreign_payments"
-    s_sql = "CREATE TABLE "+sr_file+" AS " + """
-    Select
-        pfp.EMPLOYEE_NUMBER,
-        Max(pfp.EFFECTIVE_DATE) As LAST_PAYMENT_DATE,
-        Count(pfp.RUN_RESULT_ID) As COUNT_PAYMENTS
-    From
-        PAYROLL.X000aa_payroll_history_%PERIOD% pfp
-    Where
-        pfp.ELEMENT_NAME Like 'NWU Foreign Payment%'
-    Group By
-        pfp.EMPLOYEE_NUMBER
-    ;"""
-    so_curs.execute("DROP TABLE IF EXISTS "+sr_file)
-    if funcdate.prev_monthend()[0:4] == funcdate.prev_year:
-        s_sql = s_sql.replace("%PERIOD%", 'prev')
-    else:
-        s_sql = s_sql.replace("%PERIOD%", 'curr')
-    if l_debug:
-        print(s_sql)
-    so_curs.execute(s_sql)
-    funcfile.writelog("%t BUILD TABLE: "+sr_file)
-    if l_debug:
-        so_conn.commit()
-
     # BUILD PEOPLE TO DETERMINE FOREIGN STATUS
     print("Obtain people master list with foreign status...")
     sr_file = "X003_foreigner_master"
@@ -276,7 +221,6 @@ def people_test_masterfile_xdev():
     Where
         CURR.PHONE_VALID = 0
     """
-
 
     # COUNT THE NUMBER OF FINDINGS
     i_finding_before: int = funcsys.tablerowcount(so_curs, sr_file)
@@ -530,6 +474,9 @@ def people_test_masterfile_xdev():
             FIND.AUD_SUP_MAIL AS Audit_Supervisor_Mail            
         From
             X008ah_detail FIND
+        Order By
+            Work_phone,
+            Employee    
         ;"""
         so_curs.execute("DROP TABLE IF EXISTS " + sr_file)
         so_curs.execute(s_sql)
