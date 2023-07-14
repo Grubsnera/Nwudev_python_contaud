c1974a3ac56cfa5c1feb21b85f8d77d21cfadb43
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-07-14 13:53:02 +0200

    Update C001_people_test_masterfile.py
    Add nationality information to the current employee data.

--- C001_people_test_masterfile.py
+++ C001_people_test_masterfile.py
@@ -171,15 +171,24 @@ def people_test_masterfile():
         pap.PERSON_ID As person_id,
         paa.ASSIGNMENT_ID As assignment_id,
         pap.FULL_NAME As name_full,
-        upper(hrl.LOCATION_CODE) As location,
+        Upper(hrl.LOCATION_CODE) As location,
         Max(pps.ACTUAL_TERMINATION_DATE) As service_end_date,
-        upper(ppt.USER_PERSON_TYPE) As user_person_type,
-        upper(cat.MEANING) As assignment_category,
+        Upper(ppt.USER_PERSON_TYPE) As user_person_type,
+        Upper(cat.MEANING) As assignment_category,
         Case
-            When paa.position_id = 0 Then upper(paa.EMPLOYEE_CATEGORY)
-            Else upper(hrp.acad_supp)
-        End as employee_category,
-        upper(hrp.position_name) position_name
+            When paa.POSITION_ID = 0
+            Then Upper(paa.EMPLOYEE_CATEGORY)
+            Else Upper(hrp.ACAD_SUPP)
+        End As employee_category,
+        Upper(hrp.POSITION_NAME) position_name,
+        Upper(nat.meaning) nationality,
+        Upper(pan.meaning) nationality_passport,      
+        pap.PER_INFORMATION9 As is_foreign,
+        pap.NATIONAL_IDENTIFIER As national_identifier,
+        Upper(pap.PER_INFORMATION2) passport,
+        Upper(pap.PER_INFORMATION3) permit,
+        Replace(Substr(pap.PER_INFORMATION8,1,10),'/','-') permit_expire,
+        acc.ORG_PAYMENT_METHOD_NAME As account_pay_method    
     From
         PER_ALL_PEOPLE_F pap Left Join
         PER_ALL_ASSIGNMENTS_F paa On paa.PERSON_ID = pap.PERSON_ID
@@ -194,7 +203,10 @@ def people_test_masterfile():
         HR_LOOKUPS cat On cat.LOOKUP_CODE = paa.EMPLOYMENT_CATEGORY
                 And cat.LOOKUP_TYPE = 'EMP_CAT' Left Join
         X000_POSITIONS hrp On hrp.POSITION_ID = paa.POSITION_ID
-                And paa.EFFECTIVE_END_DATE Between hrp.EFFECTIVE_START_DATE And hrp.EFFECTIVE_END_DATE    
+                And paa.EFFECTIVE_END_DATE Between hrp.EFFECTIVE_START_DATE And hrp.EFFECTIVE_END_DATE Left join
+        HR_LOOKUPS nat on nat.lookup_type = 'NATIONALITY' and nat.lookup_code = pap.nationality Left join
+        HR_LOOKUPS pan on pan.lookup_type = 'NATIONALITY' and pan.lookup_code = pap.per_information10 Left join
+        X000_pay_accounts_latest acc on acc.assignment_id = paa.assignment_id
     Where
         (paa.EFFECTIVE_END_DATE Between pps.DATE_START And pps.ACTUAL_TERMINATION_DATE And
             ppt.USER_PERSON_TYPE != 'Retiree') Or
