85eccc4bd7aa2f07b277e3fa1b0c2cfecc476a81
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-10-19 17:16:22 +0200

    Create tabulizer script.
    Create SQL Queries\Web_nwuia (server)\tabulizer_director.sql. Script used to display directors in the NWU web application.

---
+++ SQL Queries/Web_nwuia (server)/tabulizer_director.sql
@@ -0,0 +1,19 @@
+﻿Select
+    dir.nwu_number,
+    dir.employee_name,
+    dir.national_identifier,
+    dir.date_submitted,
+    dir.registration_number,
+    dir.company_name,
+    dir.enterprise_type,
+    dir.company_status,
+    dir.business_start_date,
+    dir.directorship_start_date
+From
+    ia_director dir
+Where
+    -- dir.customer = '{user_param_1:cmd}'
+    dir.customer = '1'
+Group By
+    dir.nwu_number,
+    dir.registration_number
