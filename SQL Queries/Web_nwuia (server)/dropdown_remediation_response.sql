a2ae819f7ce6e8b9c211cf9d3de105990a270119
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-10-31 11:44:48 +0200

    Create SQL Queries\Web_nwuia (server)\dropdown_remediation_response.sql.
    Create SQL Queries\Web_nwuia (server)\dropdown_remediation_response.sql. SQL script to obtain the remediation response dropdown list.

---
+++ SQL Queries/Web_nwuia (server)/dropdown_remediation_response.sql
@@ -0,0 +1,15 @@
+﻿Select
+    resp.ia_findresp_auto As value,
+    resp.ia_findresp_name As label
+From
+    ia_finding_response resp
+Where
+    resp.ia_findresp_active = 1 And
+    -- resp.ia_findresp_customer = ".$customer_id." And
+    resp.ia_findresp_customer = 1 And
+    -- resp.ia_findresp_from <= '".$create_date."' And
+    resp.ia_findresp_from <= Now() And
+    -- resp.ia_findresp_from >= '".$create_date."'
+    resp.ia_findresp_to >= Now()
+Order By
+    label
