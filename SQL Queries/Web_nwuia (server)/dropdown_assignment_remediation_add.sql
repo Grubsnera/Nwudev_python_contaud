0c1fbb2ba1cf5db64e42fc885befc218455c01df
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-10-31 10:17:21 +0200

    Create SQL Queries\Web_nwuia (server)\dropdown_assignment_remediation_add.sql.
    Create SQL Queries\Web_nwuia (server)\dropdown_assignment_remediation_add.sql. SQL script to display a dropdown list of assignments for the add action in the remediation form.

---
+++ SQL Queries/Web_nwuia (server)/dropdown_assignment_remediation_add.sql
@@ -0,0 +1,24 @@
+﻿Select
+    assi.ia_assi_auto as value,
+    Concat(cate.ia_assicate_name, ' (', assi.ia_assi_year, ') - ', assi.ia_assi_name, ' (', assi.ia_assi_auto, ') ', '(', type.ia_assitype_name, ')') As label
+From
+    ia_assignment assi Inner Join
+    ia_assignment_category cate On cate.ia_assicate_auto = assi.ia_assicate_auto Inner Join
+    ia_assignment_type type On type.ia_assitype_auto = assi.ia_assitype_auto Inner Join
+    ia_finding find On find.ia_assi_auto = assi.ia_assi_auto Inner Join
+    ia_finding_status On ia_finding_status.ia_findstat_auto = find.ia_findstat_auto
+Where
+    (-- assi.ia_user_sysid = ".$user_id." And
+    assi.ia_user_sysid = 855 And
+    assi.ia_assi_priority < 9 And
+    ia_finding_status.ia_findstat_name = 'Send for approval') Or
+    (-- assi.ia_user_sysid = ".$user_id." And
+    assi.ia_user_sysid = 855 And
+    assi.ia_assi_priority < 9 And
+    ia_finding_status.ia_findstat_name = 'Request remediation')
+Group By
+    assi.ia_assi_auto,
+    assi.ia_assi_name
+Order By
+    cate.ia_assicate_name,
+    assi.ia_assi_name
