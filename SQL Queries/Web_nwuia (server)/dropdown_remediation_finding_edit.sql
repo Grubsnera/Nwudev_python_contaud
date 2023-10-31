565503647d8753cafc20841585b66959ebde0c59
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-10-31 11:45:59 +0200

    Create SQL Queries\Web_nwuia (server)\dropdown_remediation_finding_edit.sql.
    Create SQL Queries\Web_nwuia (server)\dropdown_remediation_finding_edit.sql. SQL script to obtain the remediation finding list.

---
+++ SQL Queries/Web_nwuia (server)/dropdown_remediation_finding_edit.sql
@@ -0,0 +1,11 @@
+﻿Select
+    f.ia_find_auto As value,
+    Concat(f.ia_find_name, ' (', f.ia_find_auto, ')') As label
+From
+    ia_finding f Inner Join
+    ia_assignment a On a.ia_assi_auto = f.ia_assi_auto
+Where
+    -- a.ia_assi_auto = ".$assignment_id." And
+    a.ia_assi_auto = 89 And
+    -- f.ia_find_auto = ".$finding_id."
+    f.ia_find_auto = 928
