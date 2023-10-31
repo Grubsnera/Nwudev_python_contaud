55778e888d9b1d9f15d39869dd474aa5a3509f51
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-10-31 10:13:55 +0200

    Create SQL Queries\Web_nwuia (server)\tabulizer_director_super.sql.
    Create SQL Queries\Web_nwuia (server)\tabulizer_director_super.sql. SQL script to display a list of CIPC directors.

---
+++ SQL Queries/Web_nwuia (server)/tabulizer_director_super.sql
@@ -0,0 +1,22 @@
+﻿Select
+    concat(con.name, ' (', con.id, ')') as customer,
+    dir.nwu_number,
+    dir.employee_name,
+    dir.national_identifier,
+    dir.user_person_type,
+    dir.position_name,
+    dir.date_submitted,
+    dir.import_date,
+    dir.registration_number,
+    dir.company_name,
+    dir.enterprise_type,
+    dir.company_status,
+    dir.business_start_date,
+    dir.directorship_start_date
+From
+    ia_director dir Left Join
+    jm4_contact_details con On con.id = dir.customer
+Group By
+    con.name,
+    dir.nwu_number,
+    dir.registration_number
