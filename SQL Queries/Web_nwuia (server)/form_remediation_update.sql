3ba3a33087e7132e07b0a743051f6e7a8caaeb86
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-10-31 15:22:00 +0200

    Update SQL Queries\Web_nwuia (server)\form_remediation_update.sql.
    Update SQL Queries\Web_nwuia (server)\form_remediation_update.sql. Add remediation response type to the remediation finding update form.

--- SQL Queries/Web_nwuia (server)/form_remediation_update.sql
+++ SQL Queries/Web_nwuia (server)/form_remediation_update.sql
@@ -3,21 +3,23 @@
     reme.ia_find_auto,
     reme.ia_findreme_response,
     reme.ia_findrate_auto,
-    concat(frat.ia_findrate_name,' - ',frat.ia_findrate_desc) As ia_findrate_name,
+    Concat(frat.ia_findrate_name, ' - ', frat.ia_findrate_desc) As ia_findrate_name,
     reme.ia_findlike_auto,
-    concat(flik.ia_findlike_name,' - ',flik.ia_findlike_desc) As ia_findlike_name,
+    Concat(flik.ia_findlike_name, ' - ', flik.ia_findlike_desc) As ia_findlike_name,
     reme.ia_findcont_auto,
-    concat(fcon.ia_findcont_name,' - ',fcon.ia_findcont_desc) As ia_findcont_name,
+    Concat(fcon.ia_findcont_name, ' - ', fcon.ia_findcont_desc) As ia_findcont_name,
     find.ia_find_comment,
     reme.ia_findreme_name,
     reme.ia_findreme_date_submit,
-    reme.ia_findreme_mail_trigger
+    reme.ia_findreme_mail_trigger,
+    ia_finding_response.ia_findresp_name,
+    ia_finding_response.ia_findresp_desc
 From
     ia_finding_remediation reme Inner Join
     ia_finding find On find.ia_find_auto = reme.ia_find_auto Inner Join
     ia_finding_likelihood flik On flik.ia_findlike_auto = reme.ia_findlike_auto Inner Join
     ia_finding_rate frat On frat.ia_findrate_auto = reme.ia_findrate_auto Inner Join
-    ia_finding_control fcon On fcon.ia_findcont_auto = reme.ia_findcont_auto
+    ia_finding_control fcon On fcon.ia_findcont_auto = reme.ia_findcont_auto Inner Join
+    ia_finding_response On ia_finding_response.ia_findresp_auto = reme.ia_findresp_auto
 Where
-    -- reme.ia_findreme_auto = '".$id."'
     reme.ia_findreme_auto = 1214
