82c0c1db0f8827cd8d9c01acbe7b6aeba6c3c341
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-11-14 07:03:46 +0200

    Update test queries.
    Update test queries.

--- SQL Queries/People_conflict/Test.sql
+++ SQL Queries/People_conflict/Test.sql
@@ -1,26 +1,16 @@
 ﻿Select
-    r.nwu_number,
-    r.employee_name,
-    r.national_identifier,
-    Max(r.date_submitted) As Date_submitted,
-    r.registration_number,
-    r.company_name,
-    r.enterprise_type,
-    r.company_status,
-    r.history_date,
-    r.business_start_date,
-    r.directorship_status,
-    r.directorship_start_date,
-    r.directorship_end_date,
-    r.directorship_type,
-    r.directorship_interest,
-    r.nationality
+    v.nwu_number,
+    v.company_name,
+    v.company_registration_number,
+    v.vendor_id,
+    v.regno_director,
+    i.declaration_id,
+    i.interest_id,
+    i.entity_name,
+    i.entity_registration_number
 From
-    X004e_searchworks_results_import r
-Group By
-    r.nwu_number,
-    r.national_identifier,
-    r.registration_number
-Order By
-    r.nwu_number,
-    r.business_start_date
+    X200ab_employee_conflict_transaction v Left Join
+    X200ac_employee_conflict_transaction i On (i.employee_number = v.nwu_number
+                And SubStr(i.entity_registration_number, 1, 10) = v.regno_director)
+            Or (i.employee_number = v.nwu_number
+                And i.entity_name Like (v.company_name || '%'))
