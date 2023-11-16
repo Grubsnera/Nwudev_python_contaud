b1c2561c2d7fc368e1fee8e5f0d1a9ccbb226393
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-11-16 16:57:28 +0200

    Create SQL Queries\Kfs_curr\Vendor_owner_type_summary.sql.
    Create SQL Queries\Kfs_curr\Vendor_owner_type_summary.sql. Summary to highlight vendor and owner types.

---
+++ SQL Queries/Kfs_curr/Vendor_owner_type_summary.sql
@@ -0,0 +1,28 @@
+﻿Select
+    p.PAYEE_TYPE,
+    p.PAYEE_TYPE_DESC,
+    p.OWNER_TYPE,
+    p.OWNER_TYPE_DESC,
+    p.VENDOR_TYPE,
+    p.VENDOR_TYPE_DESC,
+    Count(p.VENDOR_ID) As Count_VENDOR_ID
+From
+    X002aa_Report_payments_summary p
+Where
+    (p.PAYEE_TYPE = 'V' And
+        p.PAYEE_NAME Like '%PTY%') Or
+    (p.PAYEE_TYPE = 'V' And
+        p.PAYEE_NAME Like '%LTD%') Or
+    (p.PAYEE_TYPE = 'V' And
+        p.PAYEE_NAME Like '%EDMS%') Or
+    (p.PAYEE_TYPE = 'V' And
+        p.PAYEE_NAME Like '%BPK%') Or
+    (p.PAYEE_TYPE = 'V' And
+        p.PAYEE_NAME Like '% CC')
+Group By
+    p.PAYEE_TYPE,
+    p.PAYEE_TYPE_DESC,
+    p.OWNER_TYPE,
+    p.OWNER_TYPE_DESC,
+    p.VENDOR_TYPE,
+    p.VENDOR_TYPE_DESC
