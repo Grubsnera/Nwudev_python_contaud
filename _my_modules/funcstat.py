63c384d23f34d3574187f732fccbdc3823900995
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-11-19 08:07:48 +0200

    Update funcstat.py
    Update _my_modules\funcstat.py. PEP8 changes and improve code efficiency.

--- _my_modules/funcstat.py
+++ _my_modules/funcstat.py
@@ -8,126 +8,112 @@ Author: Albert J van Rensburg (NWU:21162395)
 import statistics
 
 
-def stat_mode(o_cursor, s_table, s_column, s_where=""):
+def stat_mode(sqlite_cursor, source_table, source_column, where_condition=""):
     """
     Function to return the statistic mode for a column
-    :param o_cursor: Database cursor
-    :param s_table: Table in database
-    :param s_column: Column to calculate
-    :param s_where: Where clause
+    :param sqlite_cursor: Database cursor
+    :param source_table: Table in database
+    :param source_column: Column to calculate
+    :param where_condition: Where clause
     :return: Mode statistic
     """
 
     # Convert the table column into a list and the calculate the mode
-    s_sql = "SELECT " + s_column
-    s_sql += " FROM " + s_table
-    s_sql += " WHERE " + s_column + " Is Not Null"
-    if s_where != "":
-        s_sql += " And " + s_where
+    s_sql = "SELECT " + source_column
+    s_sql += " FROM " + source_table
+    s_sql += " WHERE " + source_column + " Is Not Null"
+    if where_condition != "":
+        s_sql += " And " + where_condition
     s_sql += " ;"
     # print(s_sql)
     a_list = []
-    for row in o_cursor.execute(s_sql).fetchall():
+    for row in sqlite_cursor.execute(s_sql).fetchall():
         a_list.append(row[0])
     return statistics.mode(a_list)
 
 
-def stat_pstdev(o_cursor, s_table, s_column, s_where=""):
+def stat_pstdev(sqlite_cursor, source_table, source_column, where_condition=""):
     """
     Function to return the statistic mode for a column
-    :param o_cursor: Database cursor
-    :param s_table: Table in database
-    :param s_column: Column to calculate
-    :param s_where: Where clause
+    :param sqlite_cursor: Database cursor
+    :param source_table: Table in database
+    :param source_column: Column to calculate
+    :param where_condition: Where clause
     :return: Population standard deviation statistic
     """
 
     # Convert the table column into a list and the calculate the mode
-    s_sql = "SELECT " + s_column
-    s_sql += " FROM " + s_table
-    s_sql += " WHERE " + s_column + " Is Not Null"
-    if s_where != "":
-        s_sql += " And " + s_where
+    s_sql = "SELECT " + source_column
+    s_sql += " FROM " + source_table
+    s_sql += " WHERE " + source_column + " Is Not Null"
+    if where_condition != "":
+        s_sql += " And " + where_condition
     s_sql += " ;"
     # print(s_sql)
     a_list = []
-    for row in o_cursor.execute(s_sql).fetchall():
+    for row in sqlite_cursor.execute(s_sql).fetchall():
         a_list.append(row[0])
     return statistics.pstdev(a_list)
 
 
-def stat_highest_value(o_cursor, s_table, s_column, s_where=""):
+def stat_highest_value(sqlite_cursor, source_table, source_column, where_condition=""):
     """
     Function to return the statistic highest value for a column
-    :param o_cursor: Database cursor
-    :param s_table: Table in database
-    :param s_column: Column to calculate
-    :param s_where: Where clause
+    :param sqlite_cursor: Database cursor
+    :param source_table: Table in database
+    :param source_column: Column to calculate
+    :param where_condition: Where clause
     :return: Mode statistic
     """
 
     # Convert the table column into a list and the calculate the mode
-    s_sql = "SELECT " + s_column
-    s_sql += " FROM " + s_table
-    s_sql += " WHERE " + s_column + " Is Not Null"
-    if s_where != "":
-        s_sql += " And " + s_where
-    s_sql += " ORDER BY " + s_column + " DESC "
+    s_sql = "SELECT " + source_column
+    s_sql += " FROM " + source_table
+    s_sql += " WHERE " + source_column + " Is Not Null"
+    if where_condition != "":
+        s_sql += " And " + where_condition
+    s_sql += " ORDER BY " + source_column + " DESC "
     s_sql += " ;"
     # print(s_sql)
-    row = o_cursor.execute(s_sql).fetchone()
+    row = sqlite_cursor.execute(s_sql).fetchone()
     return row[0]
 
 
-def stat_list(o_cursor, s_table, s_column, s_where=""):
+def stat_list(sqlite_cursor, source_table, source_column, where_condition=""):
     """
-    Function to return the statistic mode for a column
-    :param o_cursor: Database cursor
-    :param s_table: Table in database
-    :param s_column: Column to return
-    :param s_where: Where clause
-    :return: List
-    """
-
-    # DECLARE VARIABLES
-    l_debug: bool = False
-
-    # Convert the table column into a list and the calculate the mode
-    s_sql = "SELECT " + s_column
-    s_sql += " FROM " + s_table
-    s_sql += " WHERE " + s_column + " Is Not Null"
-    if s_where != "":
-        s_sql += " And " + s_where
-    s_sql += " ;"
-    # print(s_sql)
-    a_list = []
-    for row in o_cursor.execute(s_sql).fetchall():
-        a_list.append(row[0])
-    if l_debug:
-        for i in a_list:
-            print(i)
-    return a_list
-
-
-def stat_tuple(o_cursor, s_table, s_column, s_where=""):
-    """
-    Function to return the statistic mode for a column
-    :param o_cursor: Database cursor
-    :param s_table: Table in database
-    :param s_column: Column to return
-    :param s_where: Where clause
-    :return: List
+    Function to return a list of non-null values for a given column from the specified table.
+    :param sqlite_cursor: Database cursor
+    :param source_table: Table in database
+    :param source_column: Column to return
+    :param where_condition: Where clause
+    :return: List of non-null values
     """
 
-    # Convert the table column into a list and the calculate the mode
-    s_sql = "SELECT " + s_column
-    s_sql += " FROM " + s_table
-    s_sql += " WHERE " + s_column + " Is Not Null"
-    if s_where != "":
-        s_sql += " And " + s_where
-    s_sql += " ;"
-    # print(s_sql)
-    a_list = []
-    for row in o_cursor.execute(s_sql).fetchall():
-        a_list.append(row[0])
-    return tuple(a_list)
+    # Build the SQL query
+    s_sql = f"SELECT {source_column} FROM {source_table} WHERE {source_column} IS NOT NULL"
+    if where_condition:
+        s_sql += f" AND ({where_condition})"
+
+    # Execute the query and return the results directly as a list
+    sqlite_cursor.execute(s_sql)
+    return [row[0] for row in sqlite_cursor.fetchall()]
+
+
+def stat_tuple(sqlite_cursor, source_table, source_column, where_condition=""):
+    """
+    Function to return the tuple of non-null values for a column
+    :param sqlite_cursor: Database cursor
+    :param source_table: Table in database
+    :param source_column: Column to return
+    :param where_condition: Where clause
+    :return: Tuple
+    """
+
+    s_sql = f"SELECT {source_column} FROM {source_table} WHERE {source_column} IS NOT NULL"
+    if where_condition:
+        s_sql += f" AND ({where_condition})"
+    s_sql += ";"
+
+    # Execute the query and directly convert the result to a tuple
+    sqlite_cursor.execute(s_sql)
+    return tuple(row[0] for row in sqlite_cursor.fetchall())
