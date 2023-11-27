38292693bab6e2c0c375cacf8c4400dd5d2e20ae
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2023-11-27 11:22:50 +0200

    Create C203_test_special_investigation.py
    Create C203_test_special_investigation.py. Initial script to test the viability of a CA test to identify one vendor used by one department approved by one approver for special investigations.

---
+++ C203_test_special_investigation.py
@@ -0,0 +1,285 @@
+"""
+Script various special investigation related tests
+Created on: 24 November 2023
+Author: Albert J v Rensburg (NWU21162395)
+"""
+
+# IMPORT PYTHON MODULES
+import csv
+import sqlite3
+# from fuzzywuzzy import fuzz
+
+# IMPORT OWN MODULES
+from _my_modules import funccsv
+from _my_modules import funcdate
+from _my_modules import funcfile
+from _my_modules import funcsms
+from _my_modules import funcsys
+from _my_modules import functest
+from _my_modules import funcstr
+from _my_modules import funcstat
+
+# INDEX
+"""
+ENVIRONMENT
+OPEN THE DATABASES
+BEGIN OF SCRIPT
+END OF SCRIPT
+"""
+
+"""*****************************************************************************
+ENVIRONMENT
+*****************************************************************************"""
+
+# DECLARE VARIABLES
+source_database_path: str = "W:/Kfs/"  # Source database path
+source_database_name: str = "Kfs_test_si.sqlite"  # Source database
+source_database: str = source_database_path + source_database_name
+external_data_path: str = "S:/_external_data/"  # external data path
+results_path: str = "R:/Kfs/" + funcdate.cur_year() + "/"  # Results path
+s_sql: str = ""  # SQL statements
+l_debug: bool = True
+l_export: bool = False
+l_mess: bool = False
+l_mail: bool = False
+l_record: bool = False
+
+# OPEN THE SCRIPT LOG FILE
+if l_debug:
+    print("----------------")
+    print("C203_KFS_TEST_SI")
+    print("----------------")
+funcfile.writelog("Now")
+funcfile.writelog("SCRIPT: C203_KFS_TEST_SI")
+funcfile.writelog("------------------------")
+
+if l_mess:
+    funcsms.send_telegram('', 'administrator', 'Testing <b>special investigations</b>.')
+
+"""*****************************************************************************
+OPEN THE DATABASES
+*****************************************************************************"""
+if l_debug:
+    print("OPEN THE DATABASES")
+funcfile.writelog("OPEN THE DATABASES")
+
+# OPEN THE WORKING DATABASE
+with sqlite3.connect(source_database) as sqlite_connection:
+    sqlite_cursor = sqlite_connection.cursor()
+funcfile.writelog("OPEN DATABASE: " + source_database)
+
+# ATTACH DATA SOURCES
+sqlite_cursor.execute("ATTACH DATABASE 'W:/People/People.sqlite' AS 'PEOPLE'")
+funcfile.writelog("%t ATTACH DATABASE: PEOPLE.SQLITE")
+sqlite_cursor.execute("ATTACH DATABASE 'W:/People_payroll/People_payroll.sqlite' AS 'PAYROLL'")
+funcfile.writelog("%t ATTACH DATABASE: PAYROLL.SQLITE")
+sqlite_cursor.execute("ATTACH DATABASE 'W:/Kfs/Kfs.sqlite' AS 'KFS'")
+funcfile.writelog("%t ATTACH DATABASE: KFS.SQLITE")
+sqlite_cursor.execute("ATTACH DATABASE 'W:/Kfs/Kfs_curr.sqlite' AS 'KFSCURR'")
+funcfile.writelog("%t ATTACH DATABASE: KFS_CURR.SQLITE")
+sqlite_cursor.execute("ATTACH DATABASE 'W:/Vss/Vss_curr.sqlite' AS 'VSSCURR'")
+funcfile.writelog("%t ATTACH DATABASE: VSS_CURR.SQLITE")
+
+"""*****************************************************************************
+BEGIN OF SCRIPT
+*****************************************************************************"""
+if l_debug:
+    print("BEGIN OF SCRIPT")
+funcfile.writelog("BEGIN OF SCRIPT")
+
+"""*****************************************************************************
+TEST ACTIVE CIPC DIRECTOR ACTIVE VENDOR NO DECLARATION
+*****************************************************************************"""
+
+""" DESCRIPTION
+"""
+
+""" INDEX
+"""
+
+"""" TABLES USED IN TEST
+"""
+
+# DECLARE TEST VARIABLES
+count_findings_after: int = 0
+test_description = "Single supplier single approver"
+test_file_name: str = "single_supplier_single_user"
+test_file_prefix: str = "X001a"
+test_finding: str = "SINGLE SUPPLIER SINGE USER"
+test_report_file: str = "304_reported.txt"
+
+# OBTAIN TEST RUN FLAG
+if not functest.get_test_flag(sqlite_cursor, "KFS", f"TEST {test_finding}", "RUN"):
+
+    if l_debug:
+        print('TEST DISABLED')
+    funcfile.writelog("TEST " + test_finding + " DISABLED")
+
+else:
+
+    # Open log
+    if l_debug:
+        print("TEST " + test_finding)
+    funcfile.writelog("TEST " + test_finding)
+
+    # Obtain a list of purchase and payment employees to exclude
+    exclude_pp_employee = funcstat.stat_tuple(sqlite_cursor, "X000_PEOPLE", "employee_number", "organization = 'NWU PURCHASE AND PAYMENTS' Or organization = 'NWU FINANCIAL PLANNING'")
+    if l_debug:
+        print(exclude_pp_employee)
+
+    # Fetch initial data from the master table
+    if l_debug:
+        print("Fetch initial data from the master table...")
+    table_name = test_file_prefix + f"a_{test_file_name}"
+    s_sql = f"CREATE TABLE {table_name} As " + f"""
+    Select
+        v.EDOC,
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        v.PMT_DT,
+        v.NET_PMT_AMT,
+        v.APPROVE_EMP_NO,
+        v.APPROVE_EMP_NAME,
+        p.oe_code,
+        v.A_COUNT,
+        v.ACC_COST_STRING,
+        SubStr(v.ACC_COST_STRING, 4, 7) As ORG_ID,
+        a.ORG_NM
+    From
+        KFSCURR.X001ac_Report_payments_approve v Left Join
+        PEOPLE.X000_PEOPLE p On p.employee_number = v.APPROVE_EMP_NO Left Join
+        KFS.X000_Account a On a.ACCOUNT_NBR = SubStr(v.ACC_COST_STRING, 4, 7) 
+        
+    Where
+        v.VENDOR_TYPE In ('DV', 'PO') And
+        v.PMT_STAT_CD = 'EXTR' And
+        v.APPROVE_EMP_NO Not In {exclude_pp_employee} And
+        SubStr(v.ACC_COST_STRING, 4, 7) <> ''
+    ;"""
+    if l_debug:
+        # print(s_sql)
+        pass
+    sqlite_cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    sqlite_cursor.execute(s_sql)
+    sqlite_connection.commit()
+    funcfile.writelog(f"%t BUILD TABLE: {table_name}")
+
+    # Identify the approvers per account per vendor
+    if l_debug:
+        print("Fetch initial data from the master table...")
+    table_name = test_file_prefix + f"b_{test_file_name}"
+    s_sql = f"CREATE TABLE {table_name} As " + f"""
+    Select
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        v.ORG_ID,
+        v.ORG_NM,
+        v.APPROVE_EMP_NO,
+        v.APPROVE_EMP_NAME
+    From
+        X001aa_single_supplier_single_user v
+    Group By
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        v.ORG_ID,
+        v.ORG_NM,
+        v.APPROVE_EMP_NO,
+        v.APPROVE_EMP_NAME
+    ;"""
+    if l_debug:
+        # print(s_sql)
+        pass
+    sqlite_cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    sqlite_cursor.execute(s_sql)
+    sqlite_connection.commit()
+    funcfile.writelog(f"%t BUILD TABLE: {table_name}")
+
+    # Count the approvers per account per vendor
+    if l_debug:
+        print("Fetch initial data from the master table...")
+    table_name = test_file_prefix + f"c_{test_file_name}"
+    s_sql = f"CREATE TABLE {table_name} As " + f"""
+    Select
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        v.ORG_ID,
+        v.ORG_NM,
+        Count('count') As count_approver
+    From
+        X001ab_single_supplier_single_user v
+    Group By
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        v.ORG_ID,
+        v.ORG_NM
+    ;"""
+    if l_debug:
+        # print(s_sql)
+        pass
+    sqlite_cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    sqlite_cursor.execute(s_sql)
+    sqlite_connection.commit()
+    funcfile.writelog(f"%t BUILD TABLE: {table_name}")
+
+    # Count the approvers per account per vendor
+    if l_debug:
+        print("Fetch initial data from the master table...")
+    table_name = test_file_prefix + f"d_{test_file_name}"
+    s_sql = f"CREATE TABLE {table_name} As " + f"""
+    Select
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        Count('count') As count_organization,
+        Sum(v.count_approver) As count_approver
+    From
+        X001ac_single_supplier_single_user v
+    Group By
+        v.VENDOR_ID,
+        v.VENDOR_NAME    ;"""
+    if l_debug:
+        # print(s_sql)
+        pass
+    sqlite_cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    sqlite_cursor.execute(s_sql)
+    sqlite_connection.commit()
+    funcfile.writelog(f"%t BUILD TABLE: {table_name}")
+
+    # Add the number of transactions and value
+    if l_debug:
+        print("Fetch initial data from the master table...")
+    table_name = test_file_prefix + f"e_{test_file_name}"
+    s_sql = f"CREATE TABLE {table_name} As " + f"""
+    Select
+        v.VENDOR_ID,
+        v.VENDOR_NAME,
+        v.count_organization,
+        v.count_approver,
+        p.LAST_PMT_DT,
+        p.TRAN_COUNT,
+        p.NET_PMT_AMT
+    From
+        X001ad_single_supplier_single_user v Left Join
+        KFSCURR.X002aa_Report_payments_summary p On p.VENDOR_ID = v.VENDOR_ID    
+    ;"""
+    if l_debug:
+        # print(s_sql)
+        pass
+    sqlite_cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    sqlite_cursor.execute(s_sql)
+    sqlite_connection.commit()
+    funcfile.writelog(f"%t BUILD TABLE: {table_name}")
+
+
+"""*****************************************************************************
+END OF SCRIPT
+*****************************************************************************"""
+if l_debug:
+    print("END OF SCRIPT")
+funcfile.writelog("END OF SCRIPT")
+
+# CLOSE THE DATABASE CONNECTION
+sqlite_connection.close()
+
+# CLOSE THE LOG WRITER
+funcfile.writelog("----------------------------------------")
+funcfile.writelog("COMPLETED: C002_PEOPLE_TEST_CONFLICT_DEV")
