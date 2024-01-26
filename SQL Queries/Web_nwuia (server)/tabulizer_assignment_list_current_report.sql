f171e197b3f30ebbc3d8caf3680713e936ac0b7f
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2024-01-26 12:00:37 +0200

    Update SQL Queries\Web_nwuia (server)\tabulizer_assignment_list_current_report.sql
    Update SQL Queries\Web_nwuia (server)\tabulizer_assignment_list_current_report.sql. Add the assignment last update date to the assignment report.

--- SQL Queries/Web_nwuia (server)/tabulizer_assignment_list_current_report.sql
+++ SQL Queries/Web_nwuia (server)/tabulizer_assignment_list_current_report.sql
@@ -26,7 +26,8 @@
     assi.ia_assi_offi As Notes_Official,
     assi.ia_assi_desc As Notes_Own,
     assi.ia_assi_auto As Aid,
-    Concat(assi.ia_assi_year, '.', assi.ia_assi_file) As File_Reference
+    Concat(assi.ia_assi_year, '.', assi.ia_assi_file) As File_Reference,
+    assi.ia_assi_editdate
 From
     ia_assignment assi Left Join
     ia_finding find On find.ia_assi_auto = assi.ia_assi_auto Left Join
@@ -35,42 +36,25 @@ From
     ia_assignment_status stat On stat.ia_assistat_auto = assi.ia_assistat_auto Left Join
     ia_user user On user.ia_user_sysid = assi.ia_user_sysid
 Where
-    (Case
-    When Month(Date(Now())) In ('10', '11', '12')
-    Then
-    -- (assi.ia_user_sysid = {user_id} And
-    (assi.ia_user_sysid = 855 And
-    assi.ia_assi_year = Year(Date_add(Now(), Interval 1 Year)) And
-    user.ia_user_active = '1' And
-    cate.ia_assicate_private = '0') Or
-    -- (assi.ia_user_sysid = {user_id} And
-    (assi.ia_user_sysid = 855 And
-    assi.ia_assi_year < Year(Date_add(Now(), Interval 1 Year)) And
-    user.ia_user_active = '1' And
-    assi.ia_assi_priority < 9 And
-    cate.ia_assicate_private = '0') Or
-    -- (assi.ia_user_sysid = {user_id} And
-    (assi.ia_user_sysid = 855 And
-    assi.ia_assi_finishdate between Concat(Year(Date(Now())), '1001') And Concat(Year(Date_add(Now(), Interval 1 Year)), '0930') And
-    user.ia_user_active = '1' and
-    cate.ia_assicate_private = '0')
-    Else
-    (assi.ia_user_sysid = 855 And
-    assi.ia_assi_year = Year(Date(Now())) And
-    user.ia_user_active = '1' and
-    cate.ia_assicate_private = '0') Or
-    (assi.ia_user_sysid = 855 And
-    assi.ia_assi_year < Year(Date(Now())) And
-    user.ia_user_active = '1' And
-    assi.ia_assi_priority < 9 And
-    cate.ia_assicate_private = '0') Or
-    (assi.ia_user_sysid = 855 And
-    assi.ia_assi_finishdate between Concat(Year(Date_add(Now(), Interval -1 Year)), '1001') And Concat(Year(Date(Now())), '0930') And
-    user.ia_user_active = '1' and
-    cate.ia_assicate_private = '0')
-    End)
+    Case
+        When Month(Date(Now())) In ('10', '11', '12')
+        Then (assi.ia_user_sysid = 855 And assi.ia_assi_year = Year(Date_Add(Now(), Interval 1 Year)) And
+            user.ia_user_active = '1' And cate.ia_assicate_private >= '0') Or
+            (assi.ia_user_sysid = 855 And assi.ia_assi_year < Year(Date_Add(Now(), Interval 1 Year)) And
+            user.ia_user_active = '1' And assi.ia_assi_priority < 9 And cate.ia_assicate_private >= '0') Or
+            (assi.ia_user_sysid = 855 And assi.ia_assi_finishdate Between Concat(Year(Date(Now())), '1001') And
+            Concat(Year(Date_Add(Now(), Interval 1 Year)), '0930') And user.ia_user_active = '1' And
+            cate.ia_assicate_private >= '0')
+        Else (assi.ia_user_sysid = 855 And assi.ia_assi_year = Year(Date(Now())) And user.ia_user_active = '1'
+            And cate.ia_assicate_private >= '0') Or
+            (assi.ia_user_sysid = 855 And assi.ia_assi_year < Year(Date(Now())) And user.ia_user_active = '1' And
+            assi.ia_assi_priority < 9 And cate.ia_assicate_private >= '0') Or
+            (assi.ia_user_sysid = 855 And assi.ia_assi_finishdate Between Concat(Year(Date_Add(Now(), Interval
+            -1 Year)), '1001') And Concat(Year(Date(Now())), '0930') And user.ia_user_active = '1' And
+            cate.ia_assicate_private >= '0')
+    End
 Group By
     cate.ia_assicate_name,
     type.ia_assitype_name,
-    assi.ia_assi_name,
-    assi.ia_assi_auto
+    assi.ia_assi_auto,
+    assi.ia_assi_name
