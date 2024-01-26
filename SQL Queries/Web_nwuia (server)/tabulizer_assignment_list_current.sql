39a2abbf7d94f7d320d3f9e0966f6d8401a60c28
grubsnera <44517335+Grubsnera@users.noreply.github.com>
2024-01-26 11:59:27 +0200

    Update SQL Queries\Web_nwuia (server)\tabulizer_assignment_list_current.sql
    Update SQL Queries\Web_nwuia (server)\tabulizer_assignment_list_current.sql. Add the last edit date to the assignment table.

--- SQL Queries/Web_nwuia (server)/tabulizer_assignment_list_current.sql
+++ SQL Queries/Web_nwuia (server)/tabulizer_assignment_list_current.sql
@@ -2,8 +2,11 @@
     cate.ia_assicate_name As Category,
     type.ia_assitype_name As Type,
     Concat(assi.ia_assi_year, '.', assi.ia_assi_file) As FileRef,
-    ia_user.ia_user_name As Owner,
-    Concat('<a title="Assignment edit" href="index.php?option=com_rsform&view=rsform&formId=', assi.ia_assi_formedit, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token, '&action=edit&category=', assi.ia_assicate_auto, '" target="_blank" rel="noopener nofollow noreferrer">', Concat(assi.ia_assi_name, ' (', assi.ia_assi_auto, ')'), '</a>') As Assignment,
+    user.ia_user_name As Owner,
+    Concat('<a title="Assignment edit" href="index.php?option=com_rsform&view=rsform&formId=', assi.ia_assi_formedit,
+    '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token, '&action=edit&category=',
+    assi.ia_assicate_auto, '" target="_blank" rel="noopener nofollow noreferrer">', Concat(assi.ia_assi_name, ' (',
+    assi.ia_assi_auto, ')'), '</a>') As Assignment,
     Case
         When assi.ia_assi_priority = 1
         Then 'Low'
@@ -20,14 +23,26 @@
         Else 'Inactive'
     End As Priority,
     stat.ia_assistat_name As Status,
+    date_format(assi.ia_assi_editdate,'%y%m%d') As Edited,    
     Concat(Date(assi.ia_assi_completedate), ' ', Substr(MonthName(assi.ia_assi_completedate), 1, 3)) As Due,
     Case
-        -- When assi.ia_assi_permission = {user_id}
         When assi.ia_assi_permission = 855
-        Then
-            Concat('<a title="Assignment report" href = "index.php?option=com_content&view=article&id=', assi.ia_assi_formview, '&hash=', assi.ia_assi_token, '" target="_blank" rel="noopener noreferrer nofollow">', 'Report', '</a>')
-        Else
-            Concat('<a title="Assignment copy" href = "index.php?option=com_rsform&view=rsform&formId=', assi.ia_assi_formedit, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token, '&action=copy', '&category=', assi.ia_assicate_auto, '" target="_blank" rel="noopener noreferrer nofollow">', 'Copy', '</a>', ' | ', '<a title="Assignment delete" href = "index.php?option=com_rsform&view=rsform&formId=', assi.ia_assi_formedit, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token, '&action=delete', '&category=', assi.ia_assicate_auto, '" target="_blank" rel="noopener noreferrer nofollow">', 'Delete', '</a>', ' | ', '<a title="Assignment wip" href = "index.php?option=com_rsform&view=rsform&formId=', assi.ia_assi_formdelete, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token, '&action=edit', '&category=', assi.ia_assicate_auto, '" target="_blank" rel="noopener noreferrer nofollow">', 'WIP', '</a>', ' | ', '<a title="Assignment report" href = "index.php?option=com_content&view=article&id=', assi.ia_assi_formview, '&hash=', assi.ia_assi_token, '" target="_blank" rel="noopener noreferrer nofollow">', 'Report', '</a>')
+        Then Concat('<a title="Assignment report" href = "index.php?option=com_content&view=article&id=',
+            assi.ia_assi_formview, '&hash=', assi.ia_assi_token,
+            '" target="_blank" rel="noopener noreferrer nofollow">', 'Report', '</a>')
+        Else Concat('<a title="Assignment copy" href = "index.php?option=com_rsform&view=rsform&formId=',
+            assi.ia_assi_formedit, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token, '&action=copy',
+            '&category=', assi.ia_assicate_auto, '" target="_blank" rel="noopener noreferrer nofollow">', 'Copy',
+            '</a>', ' | ', '<a title="Assignment delete" href = "index.php?option=com_rsform&view=rsform&formId=',
+            assi.ia_assi_formedit, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token,
+            '&action=delete', '&category=', assi.ia_assicate_auto,
+            '" target="_blank" rel="noopener noreferrer nofollow">', 'Delete', '</a>', ' | ',
+            '<a title="Assignment wip" href = "index.php?option=com_rsform&view=rsform&formId=',
+            assi.ia_assi_formdelete, '&recordId=', assi.ia_assi_auto, '&recordHash=', assi.ia_assi_token,
+            '&action=edit', '&category=', assi.ia_assicate_auto,
+            '" target="_blank" rel="noopener noreferrer nofollow">', 'WIP', '</a>', ' | ',
+            '<a title="Assignment report" href = "index.php?option=com_content&view=article&id=', assi.ia_assi_formview,
+            '&hash=', assi.ia_assi_token, '" target="_blank" rel="noopener noreferrer nofollow">', 'Report', '</a>')
     End As Actions,
     Case
         When Count(find.ia_find_auto) > 1
@@ -45,12 +60,12 @@ From
     ia_assignment_category cate On cate.ia_assicate_auto = assi.ia_assicate_auto Left Join
     ia_assignment_type type On type.ia_assitype_auto = assi.ia_assitype_auto Left Join
     ia_assignment_status stat On stat.ia_assistat_auto = assi.ia_assistat_auto Left Join
-    ia_user On user.ia_user_sysid = assi.ia_user_sysid
+    ia_user user On user.ia_user_sysid = assi.ia_user_sysid
 Where
-    -- (assi.ia_user_sysid = {user_id} And assi.ia_assi_priority < 9) Or
-    (assi.ia_user_sysid = 855 And assi.ia_assi_priority < 9) Or
-    -- (assi.ia_assi_permission = {user_id} And assi.ia_assi_priority < 9)
-    (assi.ia_assi_permission = 855 And assi.ia_assi_priority < 9)
+    (assi.ia_user_sysid = 855 And
+        assi.ia_assi_priority < 9) Or
+    (assi.ia_assi_priority < 9 And
+        assi.ia_assi_permission = 855)
 Group By
     cate.ia_assicate_name,
     type.ia_assitype_name,
